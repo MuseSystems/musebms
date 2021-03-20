40926697f2426858de24af1692568562beccb511
Steven C. Buttgereit <scb@musesystems.com>
2021-03-20 09:46:18 -0700

    Add function get_exception_details
    Returns the pretty-printed text of our standard exception details
    object.

---
+++ msbms/database/msbms_syst_priv/functions/get_exception_details.sql
@@ -0,0 +1,79 @@
+-- File:        get_exception_details.sql
+-- Location:    msbms/database/msbms_syst_priv/functions/get_exception_details.sql
+-- Project:     Muse Systems Business Management System
+--
+-- Licensed to Lima Buttgereit Holdings LLC (d/b/a Muse Systems) under one or
+-- more agreements.  Muse Systems licenses this file to you under the terms and
+-- conditions of your Muse Systems Master Services Agreement or governing
+-- Statement of Work.
+--
+-- See the NOTICE file in the project root for copyright ownership information.
+--
+-- muse.information@musesystems.com  :: https://muse.systems
+
+--
+-- Returns exception details based on the passed parameters represented as a pretty-printed JSON
+-- object.  The returned value is intended to standardize the details related to RAISEd exceptions
+-- and be suitable for use in setting the RAISE DETAILS variable.
+--
+
+CREATE OR REPLACE FUNCTION
+    msbms_syst_priv.get_exception_details(p_proc_schema    text
+                                         ,p_proc_name      text
+                                         ,p_exception_name text
+                                         ,p_errcode        text
+                                         ,p_param_data     jsonb
+                                         ,p_context_data   jsonb)
+RETURNS text AS
+$BODY$
+    SELECT
+        jsonb_pretty(
+            jsonb_build_object(
+                 'procedure_schema',      p_proc_schema
+                ,'procedure_name',        p_proc_name
+                ,'exception_name',        p_exception_name
+                ,'sqlstate',              p_errcode
+                ,'parameters',            p_param_data
+                ,'context',               p_context_data
+                ,'transaction_timestamp', now()
+                ,'wallclock_timestamp',   clock_timestamp()));
+$BODY$
+LANGUAGE sql VOLATILE;
+
+ALTER FUNCTION msbms_syst_priv.get_exception_details(p_proc_schema    text
+                                                    ,p_proc_name      text
+                                                    ,p_exception_name text
+                                                    ,p_errcode        text
+                                                    ,p_param_data     jsonb
+                                                    ,p_context_data   jsonb)
+    OWNER TO msbms_owner;
+
+REVOKE EXECUTE ON FUNCTION
+    msbms_syst_priv.get_exception_details(p_proc_schema    text
+                                          ,p_proc_name      text
+                                          ,p_exception_name text
+                                          ,p_errcode        text
+                                          ,p_param_data     jsonb
+                                          ,p_context_data   jsonb)
+    FROM public;
+
+GRANT EXECUTE ON FUNCTION
+    msbms_syst_priv.get_exception_details( p_proc_schema    text
+                                          ,p_proc_name      text
+                                          ,p_exception_name text
+                                          ,p_errcode        text
+                                          ,p_param_data     jsonb
+                                          ,p_context_data   jsonb)
+    TO msbms_owner;
+
+
+COMMENT ON FUNCTION
+    msbms_syst_priv.get_exception_details(p_proc_schema    text
+                                          ,p_proc_name      text
+                                          ,p_exception_name text
+                                          ,p_errcode        text
+                                          ,p_param_data     jsonb
+                                          ,p_context_data   jsonb) IS
+$DOC$Returns exception details based on the passed parameters represented as a pretty-printed JSON
+object.  The returned value is intended to standardize the details related to RAISEd exceptions and
+be suitable for use in setting the RAISE DETAILS variable. $DOC$;
