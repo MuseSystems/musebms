eefddc9e36e125497b4783e06d83ddbfd15ca5a7
Steven C. Buttgereit <scb@musesystems.com>
2021-04-04 07:09:32 -0700

    Refactor table names to conform to standards
    A number of tables were originally designed before we had naming
    conventions which reflected where in the ERP information architecture
    they belonged.
    
    This commit corrects that error.

--- msbms/database/msbms_syst_data/tables/syst_feature_types.sql
+++ msbms/database/msbms_syst_data/tables/syst_feature_types.sql
@@ -1,5 +1,5 @@
--- File:        app_feature_types.sql
--- Location:    msbms/database/msbms_syst_data/tables/app_feature_types.sql
+-- File:        syst_feature_types.sql
+-- Location:    msbms/database/msbms_syst_data/tables/syst_feature_types.sql
 -- Project:     Muse Systems Business Management System
 --
 -- Licensed to Lima Buttgereit Holdings LLC (d/b/a Muse Systems) under one or
@@ -11,14 +11,14 @@
 --
 -- muse.information@musesystems.com  :: https://muse.systems
 
-CREATE TABLE msbms_syst_data.app_feature_types
+CREATE TABLE msbms_syst_data.syst_feature_types
 (
      id                      uuid        DEFAULT uuid_generate_v1( ) NOT NULL
-        CONSTRAINT app_feature_types_pk PRIMARY KEY
+        CONSTRAINT syst_feature_types_pk PRIMARY KEY
     ,internal_name           text                                    NOT NULL
-        CONSTRAINT app_feature_types_internal_name_udx UNIQUE
+        CONSTRAINT syst_feature_types_internal_name_udx UNIQUE
     ,display_name            text                                    NOT NULL
-        CONSTRAINT app_feature_types_display_name_udx UNIQUE
+        CONSTRAINT syst_feature_types_display_name_udx UNIQUE
     ,description             text                                    NOT NULL
     ,feature_group           text                                    NOT NULL
     ,functional_type         text                                    NOT NULL
@@ -35,78 +35,78 @@ CREATE TABLE msbms_syst_data.app_feature_types
         CHECK (feature_group IN ('document', 'operation', 'relation'))
 );
 
-ALTER TABLE msbms_syst_data.app_feature_types OWNER TO msbms_owner;
+ALTER TABLE msbms_syst_data.syst_feature_types OWNER TO msbms_owner;
 
-REVOKE ALL ON TABLE msbms_syst_data.app_feature_types FROM public;
-GRANT ALL ON TABLE msbms_syst_data.app_feature_types TO msbms_owner;
+REVOKE ALL ON TABLE msbms_syst_data.syst_feature_types FROM public;
+GRANT ALL ON TABLE msbms_syst_data.syst_feature_types TO msbms_owner;
 
 CREATE TRIGGER z99_trig_b_iu_set_diagnostic_columns
-    BEFORE INSERT OR UPDATE ON msbms_syst_data.app_feature_types
+    BEFORE INSERT OR UPDATE ON msbms_syst_data.syst_feature_types
     FOR EACH ROW EXECUTE PROCEDURE msbms_syst_priv.trig_b_iu_set_diagnostic_columns();
 
 COMMENT ON
-    TABLE msbms_syst_data.app_feature_types IS
+    TABLE msbms_syst_data.syst_feature_types IS
 $DOC$A list of the various types of forms, processes, and data managed by the ERP
 system.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.app_feature_types.id IS
+    COLUMN msbms_syst_data.syst_feature_types.id IS
 $DOC$The record's primary key.  The definitive identifier of the record in the
 system.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.app_feature_types.internal_name IS
+    COLUMN msbms_syst_data.syst_feature_types.internal_name IS
 $DOC$A candidate key useful for programmatic references to individual records.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.app_feature_types.display_name IS
+    COLUMN msbms_syst_data.syst_feature_types.display_name IS
 $DOC$A friendly name and candidate key for the record, suitable for use in user
 app_functionals.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.app_feature_types.description IS
+    COLUMN msbms_syst_data.syst_feature_types.description IS
 $DOC$A text describing the meaning and use of the specific record that may be
 visible to users of the record.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.app_feature_types.feature_group IS
+    COLUMN msbms_syst_data.syst_feature_types.feature_group IS
 $DOC$Identifies the nature of the feature.  For example, a data records stored in the
 database are considered part of the 'relation' feature groups.  This field is
 used primarily for grouping like features together.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.app_feature_types.functional_type IS
+    COLUMN msbms_syst_data.syst_feature_types.functional_type IS
 $DOC$Establishes the meaning of the record in relation to functionality implemented
 in the system.  The system will base processing decisions upon the value in this
 field.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.app_feature_types.diag_timestamp_created IS
+    COLUMN msbms_syst_data.syst_feature_types.diag_timestamp_created IS
 $DOC$The database server date/time when the transaction which created the record
 started.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.app_feature_types.diag_role_created IS
+    COLUMN msbms_syst_data.syst_feature_types.diag_role_created IS
 $DOC$The database role which created the record.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.app_feature_types.diag_timestamp_modified IS
+    COLUMN msbms_syst_data.syst_feature_types.diag_timestamp_modified IS
 $DOC$The database server date/time when the transaction which modified the record
 started.  This field will be the same as diag_timestamp_created for inserted
 records.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.app_feature_types.diag_wallclock_modified IS
+    COLUMN msbms_syst_data.syst_feature_types.diag_wallclock_modified IS
 $DOC$The database server date/time at the moment the record was actually modified.
 For long running transactions this time may be significantly later than the
 value of diag_timestamp_modified.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.app_feature_types.diag_role_modified IS
+    COLUMN msbms_syst_data.syst_feature_types.diag_role_modified IS
 $DOC$The database role which modified the record.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.app_feature_types.diag_row_version IS
+    COLUMN msbms_syst_data.syst_feature_types.diag_row_version IS
 $DOC$The current version of the row.  The value here indicates how many actual
 data changes have been made to the row.  If an update of the row leaves all data
 fields the same, disregarding the updates to the diag_* columns, the row version
@@ -114,7 +114,7 @@ is not updated, nor are any updates made to the other diag_* columns other than
 diag_update_count.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.app_feature_types.diag_update_count IS
+    COLUMN msbms_syst_data.syst_feature_types.diag_update_count IS
 $DOC$Records the number of times the record has been updated regardless as to if
 the update actually changed any data.  In this way needless or redundant record
 updates can be found.  This row starts at 0 and therefore may be the same as the
@@ -122,7 +122,7 @@ diag_row_version - 1.$DOC$;
 
 COMMENT ON
     CONSTRAINT functional_type_chk
-    ON msbms_syst_data.app_feature_types IS
+    ON msbms_syst_data.syst_feature_types IS
 $DOC$Defines the system recognized types which can alter processing.
 
       * master:       Identifies definitional data records which always represent the current state
@@ -161,7 +161,7 @@ $DOC$Defines the system recognized types which can alter processing.
 
 COMMENT ON
     CONSTRAINT feature_group_chk
-    ON msbms_syst_data.app_feature_types IS
+    ON msbms_syst_data.syst_feature_types IS
 $DOC$Defines the currently recognized feature groups in the system:
 
     * document:  These are typically the forms with which the user interacts while using the
