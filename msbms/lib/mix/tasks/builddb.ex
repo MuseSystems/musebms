080ae6bf457127e8219999ce9187a1cef9394d41
Steven C. Buttgereit <scb@musesystems.com>
2021-04-10 18:51:22 -0700

    Refactor db_build
    Some smallish changes to a smallish file.
    
      - Rename the command and file to builddb.  It follows the verb/noun
        arrangement that we typically use and feels better under the fingers
        (my fingers at any rate)
    
      - Add DO block start/end parts around the build file.  For as long as
        we can let's make the load transactional... it will make failure
        modes easier to detect and deal with.  Also, less cleanup in a
        failure.

--- msbms/lib/mix/tasks/builddb.ex
+++ msbms/lib/mix/tasks/builddb.ex
@@ -1,4 +1,4 @@
-defmodule Mix.Tasks.DbBuild do
+defmodule Mix.Tasks.Builddb do
   use Mix.Task
 
   @shortdoc "Builds the current starting database into priv/starting_database.sql"
@@ -8,6 +8,14 @@ defmodule Mix.Tasks.DbBuild do
     File.touch("priv/starting_database.sql")
     target = File.open!("priv/starting_database.sql", [:append, {:delayed_write, 1024, 20}])
 
+    IO.binwrite(target,
+    """
+    DO
+    $BUILDSCRIPT$
+    BEGIN
+
+    """)
+
     File.stream!("database/manifest.toml")
     |> Toml.decode_stream!()
     |> Map.get("load_files")
@@ -15,6 +23,12 @@ defmodule Mix.Tasks.DbBuild do
       sql = File.read!(Path.join("database", file))
       IO.binwrite(target, sql)
     end)
+
+    IO.binwrite(target,
+    """
+    END;
+    $BUILDSCRIPT$;
+    """)
 
     File.close(target)
   end
