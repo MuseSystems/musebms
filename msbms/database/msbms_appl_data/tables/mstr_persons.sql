58abeccea3cc71f587410e92ad2b6eadb99ad9c9
Steven C. Buttgereit <scb@musesystems.com>
2021-04-16 19:01:58 -0700

    Add table msbms_appl_data.mstr_persons

---
+++ msbms/database/msbms_appl_data/tables/mstr_persons.sql
@@ -0,0 +1,109 @@
+-- File:        mstr_persons.sql
+-- Location:    msbms/database/msbms_appl_data/tables/mstr_persons.sql
+-- Project:     Muse Systems Business Management System
+--
+-- Licensed to Lima Buttgereit Holdings LLC (d/b/a Muse Systems) under one or
+-- more agreements.  Muse Systems licenses this file to you under the terms and
+-- conditions of your Muse Systems Master Services Agreement or governing
+-- Statement of Work.
+--
+-- See the NOTICE file in the project root for copyright ownership information.
+--
+-- muse.information@musesystems.com  :: https://muse.systems
+
+CREATE TABLE msbms_appl_data.mstr_persons
+(
+     id                      uuid        DEFAULT uuid_generate_v1( ) NOT NULL
+        CONSTRAINT mstr_persons_pk PRIMARY KEY
+    ,internal_name           text                                    NOT NULL
+        CONSTRAINT mstr_persons_internal_name_udx UNIQUE
+    ,display_name            text
+        CONSTRAINT mstr_persons_display_name_udx UNIQUE
+    ,external_name           text                                    NOT NULL
+    ,person_type_id          uuid                                    NOT NULL
+        CONSTRAINT mstr_persons_person_types_fk
+        REFERENCES msbms_appl_data.enum_person_types (id)
+    ,person_state_id         uuid
+        CONSTRAINT mstr_persons_person_state_fk
+        REFERENCES msbms_appl_data.enum_person_states (id)
+    ,diag_timestamp_created  timestamptz DEFAULT now( )              NOT NULL
+    ,diag_role_created       text                                    NOT NULL
+    ,diag_timestamp_modified timestamptz DEFAULT now( )              NOT NULL
+    ,diag_wallclock_modified timestamptz DEFAULT clock_timestamp( )  NOT NULL
+    ,diag_role_modified      text                                    NOT NULL
+    ,diag_row_version        bigint      DEFAULT 1                   NOT NULL
+    ,diag_update_count       bigint      DEFAULT 0                   NOT NULL
+);
+
+ALTER TABLE msbms_appl_data.mstr_persons OWNER TO msbms_owner;
+
+REVOKE ALL ON TABLE msbms_appl_data.mstr_persons FROM public;
+GRANT ALL ON TABLE msbms_appl_data.mstr_persons TO msbms_owner;
+
+CREATE TRIGGER z99_trig_b_iu_set_diagnostic_columns
+    BEFORE INSERT OR UPDATE ON msbms_appl_data.mstr_persons
+    FOR EACH ROW EXECUTE PROCEDURE msbms_syst_priv.trig_b_iu_set_diagnostic_columns();
+
+COMMENT ON
+    TABLE msbms_appl_data.mstr_persons IS
+$DOC$Represents real people in the world.  Ideally, there is one record in this table
+for each person the various entities interact with.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_appl_data.mstr_persons.id IS
+$DOC$The record's primary key.  The definitive identifier of the record in the
+system.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_appl_data.mstr_persons.internal_name IS
+$DOC$A candidate key useful for programmatic references to individual records.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_appl_data.mstr_persons.display_name IS
+$DOC$A friendly name and candidate key for the record, suitable for use in user
+interfaces.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_appl_data.mstr_persons.external_name IS
+$DOC$A friendly name for externally facing uses such as in communications with the
+person.  Note that this is not a key value and has no UNIQUE enforcement.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_appl_data.mstr_persons.diag_timestamp_created IS
+$DOC$The database server date/time when the transaction which created the record
+started.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_appl_data.mstr_persons.diag_role_created IS
+$DOC$The database role which created the record.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_appl_data.mstr_persons.diag_timestamp_modified IS
+$DOC$The database server date/time when the transaction which modified the record
+started.  This field will be the same as diag_timestamp_created for inserted
+records.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_appl_data.mstr_persons.diag_wallclock_modified IS
+$DOC$The database server date/time at the moment the record was actually modified.
+For long running transactions this time may be significantly later than the
+value of diag_timestamp_modified.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_appl_data.mstr_persons.diag_role_modified IS
+$DOC$The database role which modified the record.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_appl_data.mstr_persons.diag_row_version IS
+$DOC$The current version of the row.  The value here indicates how many actual
+data changes have been made to the row.  If an update of the row leaves all data
+fields the same, disregarding the updates to the diag_* columns, the row version
+is not updated, nor are any updates made to the other diag_* columns other than
+diag_update_count.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_appl_data.mstr_persons.diag_update_count IS
+$DOC$Records the number of times the record has been updated regardless as to if
+the update actually changed any data.  In this way needless or redundant record
+updates can be found.  This row starts at 0 and therefore may be the same as the
+diag_row_version - 1.$DOC$;
