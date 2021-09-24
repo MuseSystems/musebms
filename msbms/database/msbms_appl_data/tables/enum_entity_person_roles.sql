767c4d382f07d30b8e98bf6da44bbe9535a3d4c2
Steven C. Buttgereit <scb@musesystems.com>
2021-09-24 16:41:28 -0700

    Fix spelling error in column comment
    Copy and pasted into many files.  Fix all of them here.

--- msbms/database/msbms_appl_data/tables/enum_entity_person_roles.sql
+++ msbms/database/msbms_appl_data/tables/enum_entity_person_roles.sql
@@ -74,7 +74,7 @@ person: the person record that is represented by the entity will be assigned the
 
 COMMENT ON
     COLUMN msbms_appl_data.enum_entity_person_roles.options IS
-$DOC$A JSON representation of various behaviorial options that the application may 
+$DOC$A JSON representation of various behavioral options that the application may
 make when the enum is assigned to the record it is qualifying.  Options may 
 include flags, rules to test, and other such arbitrary behaviors as required by 
 the specific record to which the enum is assigned.$DOC$;
--- msbms/database/msbms_appl_data/tables/enum_entity_place_roles.sql
+++ msbms/database/msbms_appl_data/tables/enum_entity_place_roles.sql
@@ -66,7 +66,7 @@ visible to users of the record.$DOC$;
 
 COMMENT ON
     COLUMN msbms_appl_data.enum_entity_place_roles.options IS
-$DOC$A JSON representation of various behaviorial options that the application may 
+$DOC$A JSON representation of various behavioral options that the application may
 make when the enum is assigned to the record it is qualifying.  Options may 
 include flags, rules to test, and other such arbitrary behaviors as required by 
 the specific record to which the enum is assigned.$DOC$;
