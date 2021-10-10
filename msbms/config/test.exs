4384da7e8470c901080c31cbe641ef8afa6b50ac
Steven C. Buttgereit <scb@musesystems.com>
2021-10-10 07:56:26 -0700

    Update environment specific configuration files
    Add a production file, clean out unneeded stuff from the others.
    
    These are almost certainly not needed yet.  Most of our configuration
    concerns will actually be runtime configuration concerns, so we
    shouldn't worry ourselves yet with the content of these files.
    
    Open question: do we even need dev/test/prod config files?  I'll look
    into it, but I think I got an error when running mix for prod and I
    didn't have a prod.exs (which is why it exists for this commit).

--- msbms/config/test.exs
+++ msbms/config/test.exs
@@ -1,12 +1,4 @@
 import Config
-
-config :msbms, Msbms.Repo, [
-  database: "msbms_#{Mix.env}",
-  username: "msbms_installer",
-  password: "msbms_pass",
-  hostname: "localhost"
-]
-
 config :logger,
   backends: [:console],
   level: :warn,
