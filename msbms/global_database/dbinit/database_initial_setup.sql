6f4d09f236e250cb72b77e0633348377a654059c
Steven C. Buttgereit <scb@musesystems.com>
2021-10-19 13:25:24 -0700

    Add instance management database
    This commit add the concept of instance management to the database.  The
    gist is that there is a system operations database which tells the
    application where instance databases exist so that repos can be built
    at application runtime.  This is the starting point for multi-tenancy
    support, though I think it will be useful in other scenarios as well,
    production vs. sandboxes for example.
    
      - Update tools/database_script files for a bootstraping change.
        Before this commit, there was a single application database which
        was responsible for both application data and system operations
        configuration.  Now, the system will bootstrap from a dedicated
        system operations configuration database, but that database must
        have some initial setup prior to running the application server.
        This changes the prior reset_dev_database script to be not just a
        dev tool and also separates the tear-down operations (only useful
        for dev) from the bootstrap portion of the code (useful in dev and
        to bootstrap a running application server).
    
      - Add a database definition for system operations configuration.  This
        is just a start, but I expect to be able to get some distance with
        just this.

---
+++ msbms/global_database/dbinit/database_initial_setup.sql
@@ -0,0 +1,15 @@
+
+-- Source File: database_initial_setup.sql
+-- Location:    msbms/global_database/dbinit/database_initial_setup.sql
+-- Project:     musebms
+--
+-- Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+-- This file may include content copyrighted and licensed from thrid parties.
+--
+-- See the LICENSE file in the project root for license terms and conditions.
+-- See the NOTICE file in the project root for copyright ownership information.
+--
+-- muse.information@musesystems.com  :: https://muse.systems
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
