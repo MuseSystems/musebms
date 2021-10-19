073d9b2effe1cd1a62cd96995937a93eb29aa030
Steven C. Buttgereit <scb@musesystems.com>
2021-10-19 13:01:09 -0700

    Add Msbms app entry point & initial supervisors
    This commit adds the Elixir application entry point for Msbms, the
    application server, and initial parts of the supervisor tree which
    handle the data access management pieces of the system.
    
    The general idea is that there is a data supervisor which manages a
    registry process to find instance specific Repo processes and then a
    child supervisor which manages the individual instances.  I do this
    because if the registry is not available or the Repo supervisor is not
    available then the data supervisor must either restart them or die (and
    if the data supervisor itself dies, the app is dead at that point).  The
    repo supervisor will try to restart individual repos, but it won't take
    down it's sibling repos if the failure is fatal.
    
    -- Add msbms/application.ex file
    -- Add msbms/system/data_access_supervisor.ex file
    -- Update mix.exs to start the Msbms application via calls to the
       msbms/application.ex module functions as appropriate for the
       Application behaviour.

---
+++ msbms/lib/msbms/system/data_access_supervisor.ex
@@ -0,0 +1,31 @@
+# Source File: data_access_supervisor.ex
+# Location:    musebms/lib/msbms/system/data_access_supervisor.ex
+# Project:     musebms
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from thrid parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com  : : https: //muse.systems
+defmodule Msbms.System.DataAccessSupervisor do
+  @moduledoc false
+  use Supervisor
+
+  @spec start_link(any) :: :ignore | {:error, any} | {:ok, pid}
+  def start_link(init_arg) do
+    Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+  end
+
+  @impl true
+  @spec init(any) :: {:ok, {%{intensity: any, period: any, strategy: any}, list}}
+  def init(_init_arg) do
+    children = [
+      {Registry, keys: :unique, name: Msbms.System.RepoRegistry},
+      {DynamicSupervisor, name: Msbms.System.RepoSupervisor, strategy: :one_for_one}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_all)
+  end
+end
