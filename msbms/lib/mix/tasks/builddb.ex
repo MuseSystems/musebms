2373d1cfbecebebeded7a9854b3e0f2dd8a2368d
Steven C. Buttgereit <scb@musesystems.com>
2021-10-22 06:04:20 -0700

    Refactor builddb mix task for structure, clean-up
    The following changes have been made to the task:
    
      - Introduce the global and instance database concepts.  Previously
        there was just the simple application database
    
      - Add a required command line option to identify which database type
        is being built
    
      - Simplify a bit (not so many calls to Path for example)

--- msbms/lib/mix/tasks/builddb.ex
+++ msbms/lib/mix/tasks/builddb.ex
@@ -1,34 +1,70 @@
 defmodule Mix.Tasks.Builddb do
   use Mix.Task
 
-  @shortdoc "Builds the current starting database into priv/starting_database.sql"
+  @shortdoc "Builds the current database sources into their respective starter databases and migrations."
   @spec run(any) :: any
-  def run(_) do
-    File.rm("priv/starting_database.sql")
-    File.touch("priv/starting_database.sql")
-    target = File.open!("priv/starting_database.sql", [:append, {:delayed_write, 1024, 20}])
+  def run(args) do
+    args
+    |> OptionParser.parse!(strict: [dbtype: :string])
+    |> build_starting_migration
 
-    IO.binwrite(target,
-    """
-    DO
-    $BUILDSCRIPT$
-    BEGIN
+    {:ok}
+  end
 
-    """)
+  defp build_starting_migration({[dbtype: "global"], _}) do
+    source_builder("manifest_global.toml", "global")
+  end
 
-    File.stream!("database/manifest.toml")
+  defp build_starting_migration({[dbtype: "instance"], _}) do
+    source_builder("manifest_instance.toml", "instance")
+  end
+
+  defp build_starting_migration({[dbtype: bad_arg], _}) do
+    IO.puts(
+      "Bad --dbtype argument '#{bad_arg}'.  Please set --dbtype to either 'global' or 'instance'."
+    )
+  end
+
+  defp source_builder(manifest_file, target_path) do
+    qualified_target_path = Path.join(["priv", "database", target_path, "starting_database.sql"])
+    qualified_manifest_path = Path.join(["database", manifest_file])
+
+    File.rm(qualified_target_path)
+    File.touch(qualified_target_path)
+
+    target =
+      File.open!(qualified_target_path, [
+        :append,
+        {:delayed_write, 1024, 20}
+      ])
+
+    IO.binwrite(
+      target,
+      """
+      -- Built from manifest: #{qualified_manifest_path}
+      -- Built on: #{DateTime.utc_now()}
+
+      DO
+      $BUILDSCRIPT$
+      BEGIN
+      """
+    )
+
+    File.stream!(qualified_manifest_path)
     |> Toml.decode_stream!()
     |> Map.get("load_files")
     |> Enum.each(fn file ->
-      sql = File.read!(Path.join("database", file))
+      sql = File.read!(Path.join(["database", file]))
       IO.binwrite(target, sql)
     end)
 
-    IO.binwrite(target,
-    """
-    END;
-    $BUILDSCRIPT$;
-    """)
+    IO.binwrite(
+      target,
+      """
+      END;
+      $BUILDSCRIPT$;
+      """
+    )
 
     File.close(target)
   end
