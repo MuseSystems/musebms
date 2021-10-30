86818ee90ef84d06a60ca164ea45afb00983019c
Steven C. Buttgereit <scb@musesystems.com>
2021-10-30 04:43:35 -0700

    Fix spelling error in header comments
    Apparently the templates for file headers have misspelled third, which
    means all files have the misspelling.
    
    This fixes the already created files with these comments.

--- msbms/database/common/msbms_syst_priv/functions/get_exception_details.eex.sql
+++ msbms/database/common/msbms_syst_priv/functions/get_exception_details.eex.sql
@@ -19,7 +19,7 @@ $BODY$
 -- Project:     Muse Systems Business Management System
 --
 -- Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
--- This file may include content copyrighted and licensed from thrid parties.
+-- This file may include content copyrighted and licensed from third parties.
 --
 -- See the LICENSE file in the project root for license terms and conditions.
 -- See the NOTICE file in the project root for copyright ownership information.
--- msbms/database/instance/msbms_appl_data/tables/mstr_entity_person_roles.eex.sql
+++ msbms/database/instance/msbms_appl_data/tables/mstr_entity_person_roles.eex.sql
@@ -3,7 +3,7 @@
 -- Project:     musebms
 --
 -- Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
--- This file may include content copyrighted and licensed from thrid parties.
+-- This file may include content copyrighted and licensed from third parties.
 --
 -- See the LICENSE file in the project root for license terms and conditions.
 -- See the NOTICE file in the project root for copyright ownership information.
