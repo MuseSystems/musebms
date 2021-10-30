ec9a4218c16bc55c13cc4991cfa1d9efec662ee9
Steven C. Buttgereit <scb@musesystems.com>
2021-10-30 04:45:37 -0700

    Add common/misc/no_changes.eex.sql file
    Right now the plan is to have a placeholder migration for any update
    that doesn't have database changes.  This way we get a positive
    indication that the database was considered during any update and we
    see all updates in the database_migrations table; we aren't wondering if
    any skips are because there was a missing migration or because there was
    just no update.

---
+++ msbms/database/common/misc/no_changes.eex.sql
@@ -0,0 +1,15 @@
+
+-- Source File: no_changes.eex.sql
+-- Location:    msbms/database/common/misc/no_changes.eex.sql
+-- Project:     musebms
+--
+-- Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+-- This file may include content copyrighted and licensed from third parties.
+--
+-- See the LICENSE file in the project root for license terms and conditions.
+-- See the NOTICE file in the project root for copyright ownership information.
+--
+-- muse.information@musesystems.com  :: https://muse.systems
+
+-- Make this PERFORM since there will always be a DO script wrapper
+PERFORM '*** No changes for this release/version/update.';
