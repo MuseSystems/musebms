ce626501325d2055e5286993f1aee832e254fbb8
Steven C. Buttgereit <scb@musesystems.com>
2021-11-14 08:30:40 -0800

    WIP Add stub files for various purposes
    These are mostly stub files and interface definition files.  None of
    them have had more than speculative additions and none contain any real
    logic.
    
      - system.ex: this file is intended to be the public API for accessing
        system functions.  Some functions have been added (commented out)
        but it's unclear whether these are the right functions yet.
    
      - actions.ex:  This file defines the actions that the system module
        may take.  The idea is that this is an internal API of the actions
        available from the System module, and that an actions file added
        to other modules in the application would similarly implement other
        application user action code.  Very unclear whether or not this
        pattern is the right one, but it offers a starting place.
    
      - actions/global.ex: The actual implementation of actions for the
        global repository.
    
      - data/utils.ex:  implementation for data related utility type code.
        For example, the actual implementation of the migrations code may
        happen here since the same process for migrating both the global
        and instance databases could be generalized in a purpose agnostic
        way... thereby making migrations appear utility like.  In fact most
        low level database actions the application will need to make will
        generally not be specific to either the global or instance
        databases, just details like which database to connect to or which
        specific name to use for something will change... and that could
        just be parameterized.

---
+++ msbms/lib/msbms/system/data/utils.ex
@@ -0,0 +1,15 @@
+# Source File: utils.ex
+# Location:    musebms/lib/msbms/system/data/utils.ex
+# Project:     musebms
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https: //muse.systems
+
+defmodule Msbms.System.Data.Utils do
+
+end
