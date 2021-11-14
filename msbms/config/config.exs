0579d9dd2a5f7dfef29bf0089e8af4fdfc348042
Steven C. Buttgereit <scb@musesystems.com>
2021-11-14 06:59:14 -0800

    Refactor database connection basics into config
    Previously, these database connectivity related configuration points
    were stored as module attributes in an as yet to be committed Elixir
    module.
    
    Move these to the configuration since they may be used in different
    modules at different times.

--- msbms/config/config.exs
+++ msbms/config/config.exs
@@ -1,6 +1,15 @@
 import Config
 
-config :msbms, ecto_repos: [Msbms.Repo]
+config :msbms,
+  ecto_repos: [Msbms.Repo],
+  system_db_name: :msbms_global,
+  system_db_login: "msbms_sys_dba",
+  db_owner: "msbms_##dbindent##_owner",
+  db_app_user: "msbms_##dbindent##_app_user",
+  db_app_admin: "msbms_##dbindent##_app_admin",
+  db_api_user: "msbms_##dbindent##_api_user",
+  db_api_admin: "msbms_##dbindent##_api_admin"
+
 
 if Mix.env == :dev do
   config :mix_test_watch,
