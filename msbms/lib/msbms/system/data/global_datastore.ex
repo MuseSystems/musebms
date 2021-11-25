b8a5512452fe8da21f5464eaf284245c3a34d211
Steven C. Buttgereit <scb@musesystems.com>
2021-11-25 07:02:08 -0800

    Refactor repos into datastores
    Abstract away the Ecto "Repo" into the idea of "Datastores" (described
    in earlier commits).  Add one datastore module for each kind of
    datastore the system will deal with (privileged, global, instance).
    
    The datastore abstraction is based on the idea that the application will
    access the database via a set of security context aware database
    connections and for certain general purposes.  The collection of
    connections for a purpose is called a "datastore" and in the application
    there are three purposes:
    
      - Privileged: This is the DBA function.  The privileged datastore is
        always a single connection to the "postgres" default database by a
        PostgreSQL role that has both CREATEDB and CREATEROLE PostgreSQL
        privileges.  This datastore is used to create the other kinds of
        databases, PostgreSQL roles, and datastores.  This connection is not
        held open for access as needed but rather is opened each time access
        is required.  Very limited actions are expected on this connection
        and misuse should be proactively monitored in an operating
        environment.
    
      - Global: The global datastore is a single database which is used to
        configure the application overall behavior as well as record what
        instances (explained next) exist.  There are connections for four
        security contexts: UI user access, UI administrative access, API
        user access, and API administrative access.  These are broad,
        database server level security contexts and are represented by
        individual PostgreSQL roles which privileges restricted to their own
        context.  This means that we (currently) open an Ecto repo for each
        context and that each context operates in its own pool of
        connections.  All of the contextually driven repos connecting to the
        global database are considered part of the same MSBMS Datastore.
    
      - Instance: Instance datastores are how the application accesses user
        business data.  Instance datastore connections work fundamentally
        the same as the Global datastore, except that many instance
        datastores may be operating concurrently, one for each application
        environment or tenant, whereas the global datastore only ever has
        one instance operating at a time.
    
    Key changes in this commit:
    
      - Rename the Msbms.System.Data.Global and Msbms.System.Data.Instance
        modules to GlobalDatastore and InstanceDatastore respectively.  This
        is make clear that these are abstractions for database access (above
        Ecto Repo for now).  In all likelihood, the Global and Instance
        modules will come back, but with a different purpose than managing
        connections to the database.
    
      - Add Msbms.System.Data.PrivilegedDatastore for managing privileged/
        DBA connections.
    
      - Get to a good place with the code for actually performing the
        datastore functions for each type.

---
+++ msbms/lib/msbms/system/data/global_datastore.ex
@@ -0,0 +1,96 @@
+# Source File: global_datastore.ex
+# Location:    musebms/lib/msbms/system/data/global_datastore.ex
+# Project:     musebms
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https: //muse.systems
+
+defmodule Msbms.System.Data.GlobalDatastore do
+  use Ecto.Repo,
+    otp_app: :msbms,
+    adapter: Ecto.Adapters.Postgres
+
+  alias Msbms.System.Types.DatastoreOptions
+  alias Msbms.System.Types.DbServer
+  alias Msbms.System.Types.GlobalAdminSettings
+  alias Msbms.System.Constants
+
+  @spec get_datastore_options :: DatastoreOptions.t()
+  def get_datastore_options() do
+    global_database_name =
+      Constants.get(:db_name)
+      |> String.replace("##dbtype##", "glbl")
+      |> String.replace("##dbident##", "0000MS")
+      |> String.downcase()
+
+    %DatastoreOptions{
+      database_name: global_database_name,
+      database_owner: String.replace(Constants.get(:db_owner), "##dbident##", "0000MS"),
+      datastores: [
+        appusr:
+          String.to_atom(
+            String.downcase(String.replace(Constants.get(:db_app_usr), "##dbident##", "0000MS"))
+          ),
+        apiusr:
+          String.to_atom(
+            String.downcase(String.replace(Constants.get(:db_api_usr), "##dbident##", "0000MS"))
+          ),
+        appadm:
+          String.to_atom(
+            String.downcase(String.replace(Constants.get(:db_app_admin), "##dbident##", "0000MS"))
+          ),
+        apiadm:
+          String.to_atom(
+            String.downcase(String.replace(Constants.get(:db_api_admin), "##dbident##", "0000MS"))
+          )
+      ]
+    }
+  end
+
+  @spec start_datastores(DbServer.t(), DatastoreOptions.t()) :: [{:ok, pid()} | {:error, term()}]
+  def start_datastores(%DbServer{} = dbserver, %DatastoreOptions{} = options) do
+    Enum.reduce(options.datastores, [], fn datastore, acc ->
+      [
+        case start_link(
+               name: elem(datastore, 1),
+               database: options.database_name,
+               hostname: dbserver.db_host,
+               port: dbserver.db_port,
+               username: Atom.to_string(elem(datastore, 1)),
+               password: dbserver.instance_salt <> Atom.to_string(elem(datastore, 1)),
+               show_sensitive_data_on_connection_error: dbserver.db_show_sensitive,
+               pool_size:
+                 case elem(datastore, 0) do
+                   :appusr -> dbserver.db_default_app_user_pool_size
+                   :apiusr -> dbserver.db_default_api_user_pool_size
+                   :appadm -> dbserver.db_default_app_admin_pool_size
+                   :apiadm -> dbserver.db_default_api_admin_pool_size
+                   _ -> 1
+                 end
+             ) do
+          {:error, {:already_started, ds_pid}} -> {:ok, ds_pid}
+          return_value -> return_value
+        end
+        | acc
+      ]
+    end)
+  end
+
+  @spec stop_datastores(DatastoreOptions.t()) :: :ok
+  def stop_datastores(%DatastoreOptions{datastores: datastores}) do
+    Enum.each(
+      datastores,
+      fn datastore_id ->
+        put_dynamic_repo(datastore_id)
+        stop()
+      end
+    )
+
+    :ok
+  end
+end
