dd0a12586192f5d6127a6c22a308448122d51848
Steven C. Buttgereit <scb@musesystems.com>
2021-11-25 06:01:24 -0800

    Add type Msbms.System.Types.DatastoreOptions
    Datastores are an abstraction in the application which define the basics
    of connecting to a database.  This includes the name of the database,
    the actual database connections to be started to operate the datastore,
    and default pool sizes for standard database connections.  This commit
    creates a type in the application to handle this data.
    
    I can already see the next item to refactor which is to bring the
    connection pool information into the specific database connections as
    well as making clear that the datastore abstraction includes all
    database connections needed for the application to interact with a
    specific database on the database server.

---
+++ msbms/lib/msbms/system/types/datastore_options.ex
@@ -0,0 +1,30 @@
+# Source File: datastore_options.ex
+# Location:    musebms/lib/msbms/system/types/datastore_options.ex
+# Project:     musebms
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https: //muse.systems
+
+defmodule Msbms.System.Types.DatastoreOptions do
+  @type t() :: %__MODULE__{
+          database_name: binary(),
+          database_owner: binary(),
+          app_user_pool: integer(),
+          app_admin_pool: integer(),
+          api_user_pool: integer(),
+          api_admin_pool: integer(),
+          datastores: Keyword.t()
+        }
+  defstruct database_name: nil,
+            database_owner: nil,
+            app_user_pool: nil,
+            app_admin_pool: nil,
+            api_user_pool: nil,
+            api_admin_pool: nil,
+            datastores: nil
+end
