b3198f0a2ab07f09aba77faec9968d07b7b7c2d8
Steven C. Buttgereit <github.com@musesystems.com>
2021-12-17 05:56:05 -0800

    Remove role creation and SET ROLE/AUTHORIZATION
    Rather than managing role creation in the migration files, do that
    explicitly in the application as part of the data management routines.
    It will be more flexible, easier to ,maintain and should require fewer
    privileges in the role performing the migrations.

--- msbms/database/instance/dbinit/database_initial_setup.eex.sql
+++ msbms/database/instance/dbinit/database_initial_setup.eex.sql
@@ -31,87 +31,8 @@ DO
 $SCRIPT$
 BEGIN
 
-    --
-    -- Database Cluster Preparation & Application Roles
-    --
-
-    IF
-        NOT EXISTS(SELECT true FROM pg_roles WHERE rolname = '<%= msbms_owner %>')
-    THEN
-
-        CREATE ROLE <%= msbms_owner %>;
-
-        COMMENT ON ROLE <%= msbms_owner %> IS
-        $DOC$MSBMS Maximum Privilege System Account$DOC$;
-
-    END IF;
-
-    IF
-        NOT EXISTS(SELECT true FROM pg_roles WHERE rolname = '<%= msbms_app_admin %>')
-    THEN
-
-        CREATE ROLE <%= msbms_app_admin %> LOGIN;
-
-        COMMENT ON ROLE <%= msbms_app_admin %> IS
-        $DOC$MSBMS Administrative Access Login Account$DOC$;
-
-    END IF;
-
-    IF
-        NOT EXISTS(SELECT true FROM pg_roles WHERE rolname = '<%= msbms_app_user %>')
-    THEN
-
-        CREATE ROLE <%= msbms_app_user %> LOGIN;
-
-        COMMENT ON ROLE <%= msbms_app_user %> IS
-        $DOC$MSBMS Normal Application Access Login Account$DOC$;
-
-    END IF;
-
-    IF
-        NOT EXISTS(SELECT true FROM pg_roles WHERE rolname = '<%= msbms_api_admin %>')
-    THEN
-
-        CREATE ROLE <%= msbms_api_admin %> LOGIN;
-
-        COMMENT ON ROLE <%= msbms_api_admin %> IS
-        $DOC$MSBMS Administrative API Access Login Account$DOC$;
-
-    END IF;
-
-    IF
-        NOT EXISTS(SELECT true FROM pg_roles WHERE rolname = '<%= msbms_api_user %>')
-    THEN
-
-        CREATE ROLE <%= msbms_api_user %> LOGIN;
-
-        COMMENT ON ROLE <%= msbms_app_user %> IS
-        $DOC$MSBMS Normal API Access Login Account$DOC$;
-
-    END IF;
-
-    --
-    -- Application Database Preparation
-    --
-
-    IF
-        current_database()::text IN ('postgres'
-                                    ,'template1'
-                                    ,'template0')
-    THEN
-        RAISE EXCEPTION
-            USING MESSAGE =
-                'You may not use this script while connected to one ' ||
-                'of the system databases.';
-    END IF;
-
-
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
     CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-    EXECUTE format('ALTER DATABASE %1$I OWNER TO <%= msbms_owner %>'
-                   ,current_database());
-
-    SET SESSION AUTHORIZATION <%= msbms_owner %>;
 END;
 $SCRIPT$;
