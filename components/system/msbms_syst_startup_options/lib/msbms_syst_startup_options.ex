bd1e42b8a023d59a98c70cf45902b47184cb6e72
Steven C. Buttgereit <github.com@musesystems.com>
2022-01-13 06:25:55 -0800

    Add components/system/msbms_syst_startup_options
    This new elixir project encapsulates handling of the
    msbms_startup_options.toml file and makes its options available to other
    components in the application.
    
    At this commit it's just the elixir project skeleton.

---
+++ components/system/msbms_syst_startup_options/lib/msbms_syst_startup_options.ex
@@ -0,0 +1,18 @@
+defmodule MsbmsSystStartupOptions do
+  @moduledoc """
+  Documentation for `MsbmsSystStartupOptions`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MsbmsSystStartupOptions.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ components/system/msbms_syst_startup_options/test/msbms_syst_startup_options_test.exs
@@ -0,0 +1,8 @@
+defmodule MsbmsSystStartupOptionsTest do
+  use ExUnit.Case
+  doctest MsbmsSystStartupOptions
+
+  test "greets the world" do
+    assert MsbmsSystStartupOptions.hello() == :world
+  end
+end
