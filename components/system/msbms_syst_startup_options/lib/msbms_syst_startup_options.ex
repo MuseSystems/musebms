7fb1576f0c0cc4cf20c030501a9e877d62c9029a
Steven C. Buttgereit <github.com@musesystems.com>
2022-01-14 06:36:04 -0800

    Add starting public API to MsbmsSystStartupOptions
    This is the starting point of the public API definition and the
    implementation logic.  Currently, this is just stubbed out with more to
    come.

--- components/system/msbms_syst_startup_options/lib/msbms_syst_startup_options.ex
+++ components/system/msbms_syst_startup_options/lib/msbms_syst_startup_options.ex
@@ -1,18 +1,18 @@
 defmodule MsbmsSystStartupOptions do
+
+  alias MsbmsSystStartupOptions.Impl.StartupOptions
+
   @moduledoc """
-  Documentation for `MsbmsSystStartupOptions`.
+  API for working with the msbms_startup_options.toml file.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MsbmsSystStartupOptions.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  defdelegate get_global_dbserver_name(),                             to: StartupOptions
+  defdelegate get_global_dbserver_name(startup_options_file_path),    to: StartupOptions
+  defdelegate get_global_dbserver(),                                  to: StartupOptions
+  defdelegate get_global_dbserver(startup_options_file_path),         to: StartupOptions
+  defdelegate get_dbserver(dbserver_name),                            to: StartupOptions
+  defdelegate get_dbserver(startup_options_file_path, dbserver_name), to: StartupOptions
+  defdelegate get_dbserver_list(),                                    to: StartupOptions
+  defdelegate get_dbserver_list(startup_options_file_path),           to: StartupOptions
+  defdelegate validate_startup_options,                               to: StartupOptions
+  defdelegate validate_dbserver_options(dbserver_name),               to: StartupOptions
 end
