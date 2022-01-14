5e1c5c86ffefeedc431c48af2545490dc5edcb89
Steven C. Buttgereit <github.com@musesystems.com>
2022-01-14 06:36:04 -0800

    Add starting public API to MsbmsSystStartupOptions
    This is the starting point of the public API definition and the
    implementation logic.  Currently, this is just stubbed out with more to
    come.

---
+++ components/system/msbms_syst_startup_options/lib/impl/startup_options.ex
@@ -0,0 +1,50 @@
+# Source File: startup_options.ex
+# Location:    components/system/msbms_syst_startup_options/lib/impl/startup_options.ex
+# Project:     msbms
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https: //muse.systems
+
+defmodule MsbmsSystStartupOptions.Impl.StartupOptions do
+
+  @moduledoc false
+
+  def get_global_dbserver_name do
+
+  end
+
+  def get_global_dbserver_name do
+
+  end
+
+  def get_global_dbserver do
+
+  end
+
+  def get_global_dbserver do
+
+  end
+
+  def get_dbserver do
+
+  end
+
+  def get_dbserver do
+
+  end
+
+  def get_dbserver_list do
+
+  end
+
+  def get_dbserver_list do
+
+  end
+
+
+end
