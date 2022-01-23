3d76f433012607a1fd7b0fdb3e6fd6d1bcb49a22
Steven C. Buttgereit <github.com@musesystems.com>
2022-01-23 08:13:39 -0800

    Remove component msbms_syst_startup_options
    This component will be replaced by msbms_syst_options which is a more
    generalized and narrowly focused version of some of the same
    functionality.  Other functionality, such as dealing with specific
    options values, will be moved to other modules more specifically
    involved with the definition of those values.

--- components/system/msbms_syst_startup_options/test/msbms_syst_startup_options_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule MsbmsSystStartupOptionsTest do
-  use ExUnit.Case
-  doctest MsbmsSystStartupOptions
-
-  test "greets the world" do
-    assert MsbmsSystStartupOptions.hello() == :world
-  end
-end
