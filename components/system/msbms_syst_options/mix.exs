98052633ec499e56a1325ba90d26089af0e2d703
Steven C. Buttgereit <github.com@musesystems.com>
2022-01-23 08:23:10 -0800

    Add component msbms_syst_options
    The functionality here comes from the now removed
    msbms_syst_startup_options component.  msbms_syst_options is a more
    generalized way to access TOML encoded options files in the file system
    and forward a map of those options to the callers.  This also means this
    functionality is no longer necessarily for only the application
    startup options, but for any options that might be encoded into a file
    system based TOML file.
    
    With this generalization, none of the value specific functionality, such
    as dbserver handling, has been brought into this component.  All such
    value specific handling should now happen in the modules that will make
    use of the values retrieved from the TOML file.

---
+++ components/system/msbms_syst_options/mix.exs
@@ -0,0 +1,40 @@
+defmodule MsbmsSystOptions.MixProject do
+ use Mix.Project
+
+  @name :msbms_syst_options
+  @version "0.1.0"
+
+  @deps [
+    {:credo,            "~> 1.6",  only: [:dev, :test], runtime: false},
+    {:dialyxir,         "~> 1.0",  only: [:dev], runtime: false},
+    {:ecto,             ">= 0.0.0"},
+    {:ex_doc,           "~> 0.27", only: :dev, runtime: false},
+    {:gettext,          "~> 0.19.0"},
+    {:toml,             ">= 0.0.0"},
+
+    {:msbms_syst_error, path: "../msbms_syst_error"}
+  ]
+
+  # ------------------------------------------------------------
+
+  def project do
+    in_production = Mix.env() == :prod
+
+    [
+      app:             @name,
+      version:         @version,
+      elixir:          "~> 1.13",
+      deps:            @deps,
+      build_embedded:  in_production,
+      start_permanent: in_production
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [
+        :logger
+      ]
+    ]
+  end
+end
