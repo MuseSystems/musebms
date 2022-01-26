2eb841740b2e577d9f6fb3c45b7136d26284cf20
Steven C. Buttgereit <github.com@musesystems.com>
2022-01-26 04:42:21 -0800

    Refactor get_options in msbms_syst_options
    The "with" expression didn't feel right.  Refactor maybe_file_decode to
    expect an error tuple from maybe_file_read and just pass it on if found.
    This allows us to replace the with block in get_options with a simple
    pipeline instead.
    
    This change makes maybe_file_decode a bit more brittle in its use cases
    as it's really tuned to the output of maybe_file_read now, but I think
    that's OK since this is a small amount of functionality and we're
    working with private functions.

--- components/system/msbms_syst_options/lib/impl/options_file.ex
+++ components/system/msbms_syst_options/lib/impl/options_file.ex
@@ -14,6 +14,45 @@ defmodule MsbmsSystOptions.Impl.OptionsFile do
 
   @moduledoc false
 
+  @spec get_options(options_file_path :: String.t()) :: {:ok, map()} | {:error, MsbmsSystError.t()}
+  def get_options(options_file_path) when is_binary(options_file_path) do
+    options_file_path
+    |> maybe_file_read()
+    |> maybe_file_decode()
+  end
+
+  @spec maybe_file_read(String.t()) :: {:ok, binary()} | {:error, MsbmsSystError.t()}
+  defp maybe_file_read(options_file_path) do
+    with error = {:error, _reason} <- File.read(options_file_path) do
+      {
+        :error,
+        %MsbmsSystError{
+          code:    :file_error,
+          message: "Problem reading options file '#{options_file_path}'.",
+          cause:   error
+        }
+      }
+    end
+  end
+
+  @spec maybe_file_decode({:ok, binary()} | {:error, MsbmsSystError.t()}) ::
+    {:ok , map()} | {:error, MsbmsSystError.t()}
+  defp maybe_file_decode({:ok, file_contents}) do
+    with error = {:error, _reason} <- Toml.decode(file_contents) do
+      {
+        :error,
+        %MsbmsSystError{
+          code:    :invalid_data,
+          message: "Problem decoding the options file.",
+          cause:   error
+        }
+      }
+    end
+  end
+
+  defp maybe_file_decode(error = {:error, _reason}), do: error
+
+
   @spec get_options!(String.t()) :: map()
   def get_options!(options_file_path) when is_binary(options_file_path) do
 
@@ -33,40 +72,6 @@ defmodule MsbmsSystOptions.Impl.OptionsFile do
       message: msbms_error.message,
       code:    msbms_error.code,
       cause:   msbms_error.cause
-  end
-
-  @spec get_options(options_file_path :: String.t()) :: {:ok, map()} | {:error, MsbmsSystError.t()}
-  def get_options(options_file_path) when is_binary(options_file_path) do
-    with {:ok, file_contents} <- maybe_file_read(options_file_path) do
-      maybe_file_decode(file_contents)
-    end
-  end
-
-  @spec maybe_file_read(String.t()) :: {:ok, binary()} | {:error, MsbmsSystError.t()}
-  defp maybe_file_read(options_file_path) do
-    with error = {:error, _reason} <- File.read(options_file_path) do
-      {
-        :error,
-        %MsbmsSystError{
-          code:    :file_error,
-          message: "Problem reading options file '#{options_file_path}'.",
-          cause:   error
-        }
-      }
-    end
-  end
-
-  defp maybe_file_decode(file_contents) do
-    with error = {:error, _reason} <- Toml.decode(file_contents) do
-      {
-        :error,
-        %MsbmsSystError{
-          code:    :invalid_data,
-          message: "Problem decoding the options file.",
-          cause:   error
-        }
-      }
-    end
   end
 
 end
