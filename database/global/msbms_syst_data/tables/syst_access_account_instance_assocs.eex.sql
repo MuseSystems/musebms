f012aad4179d602af69988c0f27bb2e86c203350
Steven C. Buttgereit <github.com@musesystems.com>
2022-03-09 14:51:20 -0800

    Refactor table: rename and add timestamps columns
    Rename syst_access_account_instance_assoc to
    syst_access_account_instance_assocs which is the correct name.  Most of
    the code already was referencing the correct table table name.
    
    Add timestamp fields to the syst_access_account_instance_assocs table
    which monitor and facilitate processing of instance connection
    invitations issued by instance owners to third party access account
    holders that are not owned by the instance owner.

--- database/global/msbms_syst_data/tables/syst_access_account_instance_assocs.eex.sql
+++ database/global/msbms_syst_data/tables/syst_access_account_instance_assocs.eex.sql
@@ -1,5 +1,5 @@
--- File:        syst_access_account_instance_assoc.eex.sql
--- Location:    database\global\msbms_syst_data\tables\syst_access_account_instance_assoc.eex.sql
+-- File:        syst_access_account_instance_assocs.eex.sql
+-- Location:    database\global\msbms_syst_data\tables\syst_access_account_instance_assocs.eex.sql
 -- Project:     Muse Business Management System
 --
 -- Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
@@ -25,6 +25,10 @@ CREATE TABLE msbms_syst_data.syst_access_account_instance_assocs
         CONSTRAINT syst_access_account_instance_assocs_instances_fk
         REFERENCES msbms_syst_data.syst_instances (id)
             ON DELETE CASCADE
+    ,access_granted          timestamptz
+    ,invitation_issued       timestamptz
+    ,invitation_expires      timestamptz
+    ,invitation_declined     timestamptz
     ,diag_timestamp_created  timestamptz DEFAULT now( )              NOT NULL
     ,diag_role_created       text                                    NOT NULL
     ,diag_timestamp_modified timestamptz DEFAULT now( )              NOT NULL
@@ -45,16 +49,58 @@ CREATE TRIGGER z99_trig_b_iu_set_diagnostic_columns
 
 COMMENT ON
     TABLE msbms_syst_data.syst_access_account_instance_assocs IS
-$DOC$Associates access accounts with the instances for which they are allowed to authenticate to.  Note
-that being able to authenticate to an instance is not the same as having authorized rights within
-the instance; authorization is handled by the instance directly.$DOC$;
+$DOC$Associates access accounts with the instances for which they are allowed to
+authenticate to.  Note that being able to authenticate to an instance is not the
+same as having authorized rights within the instance; authorization is handled
+by the instance directly.$DOC$;
 
 COMMENT ON
     COLUMN msbms_syst_data.syst_access_account_instance_assocs.id IS
 $DOC$The record's primary key.  The definitive identifier of the record in the
 system.$DOC$;
 
+COMMENT ON
+    COLUMN msbms_syst_data.syst_access_account_instance_assocs.access_account_id IS
+$DOC$The access account which is being granted authentication rights to the given
+instance.$DOC$;
 
+COMMENT ON
+    COLUMN msbms_syst_data.syst_access_account_instance_assocs.credential_type_id IS
+$DOC$Allows a specific kind of credential to be used to authenticate the access
+account to the instance.  Typically this is to control the ability to connect as
+a user or using API tokens.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_syst_data.syst_access_account_instance_assocs.instance_id IS
+$DOC$The identity of the instance to which authentication rights is being granted.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_syst_data.syst_access_account_instance_assocs.access_granted IS
+$DOC$The timestamp at which access to the instance was granted and active.  If
+the access did not require the access invitation process, this value will
+typically reflect the creation timestamp of the record.  If the invitation was
+required, it will reflect the time when the access account holder actually
+accepted the invitation to access the instance.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_syst_data.syst_access_account_instance_assocs.invitation_issued IS
+$DOC$When inviting unowned, independent access accounts such as might be used by an
+external bookkeeper, the grant of access by the instance owner is
+not immediately effective but must also be approved by the access account holder
+being granted access.  The timestamp in this column indicates when the
+invitation to connect to the instance was issued.
+
+If the value in this column is null, the assumption is that no invitation was
+required to grant the access to the access account.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_syst_data.syst_access_account_instance_assocs.invitation_expires IS
+$DOC$The timestamp at which the invitation to access a given instance expires.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_syst_data.syst_access_account_instance_assocs.invitation_declined IS
+$DOC$The timestamp at which the access account holder explicitly declined the
+invitation to access the given instance.$DOC$;
 
 COMMENT ON
     COLUMN msbms_syst_data.syst_access_account_instance_assocs.diag_timestamp_created IS
