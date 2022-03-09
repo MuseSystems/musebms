d2e7d685d8f3aa83f7049af6758f13ee480eb710
Steven C. Buttgereit <github.com@musesystems.com>
2022-03-09 14:27:10 -0800

    Fix table spelling in enum_identity_types.eex.sql

--- database/global/msbms_syst_data/tables/enum_identity_types.eex.sql
+++ database/global/msbms_syst_data/tables/enum_identity_types.eex.sql
@@ -9,14 +9,14 @@
 -- See the NOTICE file in the project root for copyright ownership information.
 --
 -- muse.information@musesystems.com  :: https://muse.systems
-CREATE TABLE msbms_syst_data.enum_indentity_types
+CREATE TABLE msbms_syst_data.enum_identity_types
 (
      id                      uuid        DEFAULT uuid_generate_v1( ) NOT NULL
-        CONSTRAINT enum_indentity_types_pk PRIMARY KEY
+        CONSTRAINT enum_identity_types_pk PRIMARY KEY
     ,internal_name           text                                    NOT NULL
-        CONSTRAINT enum_indentity_types_internal_name_udx UNIQUE
+        CONSTRAINT enum_identity_types_internal_name_udx UNIQUE
     ,display_name            text                                    NOT NULL
-        CONSTRAINT enum_indentity_types_display_name_udx UNIQUE
+        CONSTRAINT enum_identity_types_display_name_udx UNIQUE
     ,functional_type         text                                    NOT NULL
         CONSTRAINT enum_identity_types_functional_type_chk
         CHECK (functional_type IN (  'user_name'
@@ -34,35 +34,35 @@ CREATE TABLE msbms_syst_data.enum_indentity_types
     ,diag_update_count       bigint      DEFAULT 0                   NOT NULL
 );
 
-ALTER TABLE msbms_syst_data.enum_indentity_types OWNER TO <%= msbms_owner %>;
+ALTER TABLE msbms_syst_data.enum_identity_types OWNER TO <%= msbms_owner %>;
 
-REVOKE ALL ON TABLE msbms_syst_data.enum_indentity_types FROM public;
-GRANT ALL ON TABLE msbms_syst_data.enum_indentity_types TO <%= msbms_owner %>;
+REVOKE ALL ON TABLE msbms_syst_data.enum_identity_types FROM public;
+GRANT ALL ON TABLE msbms_syst_data.enum_identity_types TO <%= msbms_owner %>;
 
 CREATE TRIGGER z99_trig_b_iu_set_diagnostic_columns
-    BEFORE INSERT OR UPDATE ON msbms_syst_data.enum_indentity_types
+    BEFORE INSERT OR UPDATE ON msbms_syst_data.enum_identity_types
     FOR EACH ROW EXECUTE PROCEDURE msbms_syst_priv.trig_b_iu_set_diagnostic_columns();
 
 COMMENT ON
-    TABLE msbms_syst_data.enum_indentity_types IS
+    TABLE msbms_syst_data.enum_identity_types IS
 $DOC$The various kinds of identity that can be used to identify a user to the system.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.enum_indentity_types.id IS
+    COLUMN msbms_syst_data.enum_identity_types.id IS
 $DOC$The record's primary key.  The definitive identifier of the record in the
 system.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.enum_indentity_types.internal_name IS
+    COLUMN msbms_syst_data.enum_identity_types.internal_name IS
 $DOC$A candidate key useful for programmatic references to individual records.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.enum_indentity_types.display_name IS
+    COLUMN msbms_syst_data.enum_identity_types.display_name IS
 $DOC$A friendly name and candidate key for the record, suitable for use in user
-indentitys.$DOC$;
+identitys.$DOC$;
 
 COMMENT ON
-    COLUMN  msbms_syst_data.enum_indentity_types.functional_type IS
+    COLUMN  msbms_syst_data.enum_identity_types.functional_type IS
 $DOC$Establishes what system recognized type the type represents.  Functional types
 may determine how the system behaves in certain circumstances.  Functional types
 recognized by this application:
@@ -90,49 +90,49 @@ recognized by this application:
                   credential types.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.enum_indentity_types.description IS
+    COLUMN msbms_syst_data.enum_identity_types.description IS
 $DOC$A text describing the meaning and use of the specific record that may be
 visible to users of the record.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.enum_indentity_types.options IS
+    COLUMN msbms_syst_data.enum_identity_types.options IS
 $DOC$A JSON representation of various behaviorial options that the application may
 make when the enum is assigned to the record it is qualifying.  Options may
 include flags, rules to test, and other such arbitrary behaviors as required by
 the specific record to which the enum is assigned.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.enum_indentity_types.user_options IS
+    COLUMN msbms_syst_data.enum_identity_types.user_options IS
 $DOC$Allows for user defined options related to the type similar to the way the
 options field is envisioned.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.enum_indentity_types.diag_timestamp_created IS
+    COLUMN msbms_syst_data.enum_identity_types.diag_timestamp_created IS
 $DOC$The database server date/time when the transaction which created the record
 started.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.enum_indentity_types.diag_role_created IS
+    COLUMN msbms_syst_data.enum_identity_types.diag_role_created IS
 $DOC$The database role which created the record.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.enum_indentity_types.diag_timestamp_modified IS
+    COLUMN msbms_syst_data.enum_identity_types.diag_timestamp_modified IS
 $DOC$The database server date/time when the transaction which modified the record
 started.  This field will be the same as diag_timestamp_created for inserted
 records.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.enum_indentity_types.diag_wallclock_modified IS
+    COLUMN msbms_syst_data.enum_identity_types.diag_wallclock_modified IS
 $DOC$The database server date/time at the moment the record was actually modified.
 For long running transactions this time may be significantly later than the
 value of diag_timestamp_modified.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.enum_indentity_types.diag_role_modified IS
+    COLUMN msbms_syst_data.enum_identity_types.diag_role_modified IS
 $DOC$The database role which modified the record.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.enum_indentity_types.diag_row_version IS
+    COLUMN msbms_syst_data.enum_identity_types.diag_row_version IS
 $DOC$The current version of the row.  The value here indicates how many actual
 data changes have been made to the row.  If an update of the row leaves all data
 fields the same, disregarding the updates to the diag_* columns, the row version
@@ -140,7 +140,7 @@ is not updated, nor are any updates made to the other diag_* columns other than
 diag_update_count.$DOC$;
 
 COMMENT ON
-    COLUMN msbms_syst_data.enum_indentity_types.diag_update_count IS
+    COLUMN msbms_syst_data.enum_identity_types.diag_update_count IS
 $DOC$Records the number of times the record has been updated regardless as to if
 the update actually changed any data.  In this way needless or redundant record
 updates can be found.  This row starts at 0 and therefore may be the same as the
