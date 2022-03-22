614a6e50c10caa364e19c0573827530285e7ce18
Steven C. Buttgereit <github@musesystems.com>
2022-03-22 22:32:26 +0000

    Add syst_feature_types syst_description values

--- database/common/seed_data/seed_syst_feature_types.eex.sql
+++ database/common/seed_data/seed_syst_feature_types.eex.sql
@@ -15,90 +15,94 @@ INSERT INTO msbms_syst_data.syst_feature_types
 VALUES
     ( 'master_document'
     , 'Master Data Forms'
-    , ''
+    , 'User interface forms targeted at maintaining master data.'
     , 'master'
     , 'document' )
      ,
     ( 'master_job'
     , 'Master Data Jobs'
-    , ''
+    , 'Scheduled jobs & batch processing related to master data.'
     , 'master'
     , 'job' )
     ,
     ( 'master_relation'
     , 'Master Data Tables'
-    , ''
+    , 'Tables related to master data retention.'
     , 'master'
     , 'relation' )
     ,
     ( 'master_enumeration'
     , 'Master Data Lists of Values'
-    , ''
+    , 'Lists of values used in master data forms and referenced by master data records.'
     , 'master'
     , 'enumeration' )
     ,
     ( 'master_setting'
     , 'Master Data Settings'
-    , ''
+    , 'Configuration settings which influence the behavior of master data maintenance and ' ||
+      'interpretation.'
     , 'master'
     , 'setting' )
     ,
     ( 'support_document'
     , 'Supporting Data Forms'
-    , ''
+    , 'User interface forms targeted at maintaining supporting data.'
     , 'support'
     , 'document' )
      ,
     ( 'support_job'
     , 'Supporting Data Jobs'
-    , ''
+    , 'Scheduled jobs & batch processing related to supporting data.'
     , 'support'
     , 'job' )
     ,
     ( 'support_relation'
     , 'Supporting Data Tables'
-    , ''
+    , 'Tables related to supporting data retention.'
     , 'support'
     , 'relation' )
     ,
     ( 'support_enumeration'
     , 'Supporting Data Lists of Values'
-    , ''
+    , 'Lists of values used in supporting data forms and referenced by supporting data records.'
     , 'support'
     , 'enumeration' )
     ,
     ( 'support_setting'
     , 'Supporting Data Settings'
-    , ''
+    , 'Configuration settings which influence the behavior of supporting data maintenance and ' ||
+      'interpretation.'
     , 'support'
     , 'setting' )
     ,
     ( 'maintenance_document'
     , 'System Admin Forms'
-    , ''
+    , 'User interface forms targeted at maintaining systems administration data.'
     , 'maintenance'
     , 'document' )
      ,
     ( 'maintenance_job'
     , 'System Admin Jobs'
-    , ''
+    , 'Scheduled jobs & batch processing related to systems administration data.'
     , 'maintenance'
     , 'job' )
     ,
     ( 'maintenance_relation'
     , 'System Admin Tables'
-    , ''
+    , 'Tables related to systems administration data retention.'
     , 'maintenance'
     , 'relation' )
     ,
     ( 'maintenance_enumeration'
     , 'System Admin Lists of Values'
-    , ''
+    , 'Lists of values used in systems administration data forms and referenced by systems ' ||
+      'administration data records.'
     , 'maintenance'
     , 'enumeration' )
     ,
     ( 'maintenance_setting'
     , 'System Admin Settings'
-    , ''
+    , 'Configuration settings which influence the behavior of systems administration data ' ||
+      'maintenance and interpretation.'
     , 'maintenance'
     , 'setting' );
--- database/instance/seed_data/seed_syst_feature_types.eex.sql
+++ database/instance/seed_data/seed_syst_feature_types.eex.sql
@@ -15,90 +15,96 @@ INSERT INTO msbms_syst_data.syst_feature_types
 VALUES
     ( 'nonbooking_document'
     , 'Non-booking Transaction Forms'
-    , ''
+    , 'User interface forms target at maintaining non-booking transaction data.'
     , 'nonbooking'
     , 'document' )
      ,
     ( 'nonbooking_job'
     , 'Non-booking Transaction Jobs'
-    , ''
+    , 'Scheduled jobs & batch processing related to non-booking transaction data.'
     , 'nonbooking'
     , 'job' )
     ,
     ( 'nonbooking_relation'
     , 'Non-booking Transaction Tables'
-    , ''
+    , 'Tables related to non-booking transaction data retention.'
     , 'nonbooking'
     , 'relation' )
     ,
     ( 'nonbooking_enumeration'
     , 'Non-booking Transaction Lists of Values'
-    , ''
+    , 'Lists of values used in non-booking transaction data forms and referenced by non-' ||
+      'booking transaction data records.'
     , 'nonbooking'
     , 'enumeration' )
     ,
     ( 'nonbooking_setting'
     , 'Non-booking Transaction Settings'
-    , ''
+    , 'Configuration settings which influence the behavior of non-booking transaction data ' ||
+      'maintenance and interpretation.'
     , 'nonbooking'
     , 'setting' )
     ,
     ( 'booking_document'
     , 'Booking Transaction Forms'
-    , ''
+    , 'User interface forms target at maintaining booking transaction data.'
     , 'booking'
     , 'document' )
      ,
     ( 'booking_job'
     , 'Booking Transaction Jobs'
-    , ''
+    , 'Scheduled jobs & batch processing related to booking transaction data.'
     , 'booking'
     , 'job' )
     ,
     ( 'booking_relation'
     , 'Booking Transaction Tables'
-    , ''
+    , 'Tables related to booking transaction data retention.'
     , 'booking'
     , 'relation' )
     ,
     ( 'booking_enumeration'
     , 'Booking Transaction Lists of Values'
-    , ''
+    , 'Lists of values used in booking transaction data forms and referenced by booking ' ||
+      'transaction data records.'
     , 'booking'
     , 'enumeration' )
     ,
     ( 'booking_setting'
     , 'Booking Transaction Settings'
-    , ''
+    , 'Configuration settings which influence the behavior of booking transaction data ' ||
+      'maintenance and interpretation.'
     , 'booking'
     , 'setting' )
     ,
     ( 'tasking_document'
     , 'Tasking Transaction Forms'
-    , ''
+    , 'User interface forms target at maintaining tasking transaction data.'
     , 'tasking'
     , 'document' )
      ,
     ( 'tasking_job'
     , 'Tasking Transaction Jobs'
-    , ''
+    , 'Scheduled jobs & batch processing related to tasking transaction data.'
     , 'tasking'
     , 'job' )
     ,
     ( 'tasking_relation'
     , 'Tasking Transaction Tables'
-    , ''
+    , 'Tables related to tasking transaction data retention.'
     , 'tasking'
     , 'relation' )
     ,
     ( 'tasking_enumeration'
     , 'Tasking Transaction Lists of Values'
-    , ''
+    , 'Lists of values used in tasking transaction data forms and referenced by tasking ' ||
+      'transaction data records.'
     , 'tasking'
     , 'enumeration' )
     ,
     ( 'tasking_setting'
     , 'Tasking Transaction Settings'
-    , ''
+    , 'Configuration settings which influence the behavior of tasking transaction data ' ||
+      'maintenance and interpretation.'
     , 'tasking'
     , 'setting' );
