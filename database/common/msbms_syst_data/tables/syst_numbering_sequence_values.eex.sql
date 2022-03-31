11844cd1e4eecc51d8ffbd69163fb5d894c2a24e
Steven C. Buttgereit <github.com@musesystems.com>
2022-03-31 07:16:09 -0700

    Add document numberings support tables/functions

---
+++ database/common/msbms_syst_data/tables/syst_numbering_sequence_values.eex.sql
@@ -0,0 +1,56 @@
+-- File:        syst_numbering_sequence_values.eex.sql
+-- Location:    database\common\msbms_syst_data\tables\syst_numbering_sequence_values.eex.sql
+-- Project:     Muse Business Management System
+--
+-- Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+-- This file may include content copyrighted and licensed from third parties.
+--
+-- See the LICENSE file in the project root for license terms and conditions.
+-- See the NOTICE file in the project root for copyright ownership information.
+--
+-- muse.information@musesystems.com  :: https://muse.systems
+
+CREATE TABLE msbms_syst_data.syst_numbering_sequence_values
+(
+     id
+        uuid
+        CONSTRAINT syst_numbering_sequence_values_pk PRIMARY KEY
+        CONSTRAINT syst_numbering_sequence_values_numbering_sequences_fk
+            REFERENCES msbms_syst_data.syst_numbering_sequences (id)
+    ,next_value
+        bigint
+);
+
+ALTER TABLE msbms_syst_data.syst_numbering_sequence_values OWNER TO <%= msbms_owner %>;
+
+REVOKE ALL ON TABLE msbms_syst_data.syst_numbering_sequence_values FROM public;
+GRANT ALL ON TABLE msbms_syst_data.syst_numbering_sequence_values TO <%= msbms_owner %>;
+
+COMMENT ON
+    TABLE msbms_syst_data.syst_numbering_sequence_values IS
+$DOC$Contains the next value for a parent numbering sequence.
+
+This is a quantitative table which is, one record for one record, related to the
+qualitative data table msbms_syst_data.syst_numbering_sequences. As such,
+records in this table are created automatically when a new
+syst_numbering_sequences record is inserted and the primary key in this table is
+set to be the same as its parent syst_numbering_sequences record's primary key
+value.
+
+Values are consumed from a numbering sequence by calling the function
+msbms_syst_priv.get_next_sequence_value and can be set to a specific desired
+value using function msbms_syst_priv.set_next_sequence_value.  These functions
+ensure that the correct updating, locking, and validation are applied to the
+sequence value.  Direct updating of these records is discouraged.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_syst_data.syst_numbering_sequence_values.id IS
+$DOC$The record's primary key.  The definitive identifier of the record in the
+system.$DOC$;
+
+COMMENT ON
+    COLUMN msbms_syst_data.syst_numbering_sequence_values.next_value IS
+$DOC$The next value to be returned to callers requesting a number from the
+sequence.  Note that consuming a sequence should typically be handled using a
+SELECT FOR UPDATE or similar row locking strategy; the assumption being that any
+sequence may be supporting a gap-less numbering need in the application.$DOC$;
---
+++ database/instance/seed_data/system/initialize_numbering_segment_types.eex.sql
@@ -0,0 +1,36 @@
+-- File:        initialize_numbering_segment_types.eex.sql
+-- Location:    database\instance\seed_data\system\initialize_numbering_segment_types.eex.sql
+-- Project:     Muse Business Management System
+--
+-- Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+-- This file may include content copyrighted and licensed from third parties.
+--
+-- See the LICENSE file in the project root for license terms and conditions.
+-- See the NOTICE file in the project root for copyright ownership information.
+--
+-- muse.information@musesystems.com  :: https://muse.systems
+
+INSERT INTO msbms_syst_data.syst_numbering_segment_types
+    ( internal_name, display_name, syst_description )
+VALUES
+    ( 'enumeration'
+    , 'List of Values'
+    , 'References a configured enumeration which provides choices from which the segment values are drawn.' )
+     ,
+    ( 'sequence'
+    , 'Sequential Number'
+    , 'A sequential numbering.  Supports increment/decrement, skip sequencing, and gap-less sequencing.' )
+     ,
+    ( 'fixed_text'
+    , 'Fixed Text'
+    , 'A fixed text segment which can be used to define separators, prefixes, suffixes, or other static elements.' )
+     ,
+    ( 'free_text'
+    , 'Free Text'
+    , 'Allows users to enter arbitrary text, subject to a pattern matched validation.' )
+     ,
+    ( 'random', 'Random Number', 'Generates random numbers.' )
+     ,
+    ( 'sequence_in_prior'
+    , 'Sequence In Prior'
+    , 'Returns an incrementing sequential number within a grouping defined by the prior segments.' );
