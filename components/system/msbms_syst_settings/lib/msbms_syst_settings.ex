7b1f7472b3f49aa3d6ddae49656f7538e3efc798
Steven C. Buttgereit <github.com@musesystems.com>
2022-03-31 10:15:18 -0700

    Add msbms_syst_settings skeleton project
    This is just 'mix new' and updating mix.exs for expected dependencies.

---
+++ components/system/msbms_syst_settings/lib/msbms_syst_settings.ex
@@ -0,0 +1,18 @@
+defmodule MsbmsSystSettings do
+  @moduledoc """
+  Documentation for `MsbmsSystSettings`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MsbmsSystSettings.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ components/system/msbms_syst_settings/test/msbms_syst_settings_test.exs
@@ -0,0 +1,8 @@
+defmodule MsbmsSystSettingsTest do
+  use ExUnit.Case
+  doctest MsbmsSystSettings
+
+  test "greets the world" do
+    assert MsbmsSystSettings.hello() == :world
+  end
+end
