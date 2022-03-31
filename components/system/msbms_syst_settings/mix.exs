5fd82cfed5c66ea667d17e7b57906457e644b127
Steven C. Buttgereit <github.com@musesystems.com>
2022-03-31 10:15:18 -0700

    Add msbms_syst_settings skeleton project
    This is just 'mix new' and updating mix.exs for expected dependencies.

---
+++ components/system/msbms_syst_settings/mix.exs
@@ -0,0 +1,46 @@
+defmodule MsbmsSystSettings.MixProject do
+  use Mix.Project
+
+  @name :msbms_syst_settings
+  @version "0.1.0"
+
+  @deps [
+    # Third Party Dependencies
+    {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
+    {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+    {:ecto, "~> 3.0"},
+    {:ex_doc, "~> 0.20", only: :dev, runtime: false},
+
+    # Muse Systems Business Management System Components
+    {:msbms_syst_error, path: "../msbms_syst_error"}
+  ]
+
+  @dialyzer_opts [
+    flags: ["-Wunmatched_returns", :error_handling, :underspecs],
+    plt_add_apps: [:mix]
+  ]
+
+  # ------------------------------------------------------------
+
+  def project do
+    in_production = Mix.env() == :prod
+
+    [
+      app: @name,
+      version: @version,
+      elixir: "~> 1.13",
+      deps: @deps,
+      build_embedded: in_production,
+      start_permanent: in_production,
+      dialyzer: @dialyzer_opts
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [
+        :logger
+      ]
+    ]
+  end
+end
