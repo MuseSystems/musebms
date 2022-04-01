5ae49185a44c6f28d919c50da653e405763b5d69
Steven C. Buttgereit <github.com@musesystems.com>
2022-04-01 08:54:42 -0700

    Add generic Ecto schema to msbms_syst_settings
    Our Ecto schemas are almost always going to include a database generated
    UUID primary key (as a surrogate key) and foreign keys will themselves
    by UUIDs.  So just create a __using__ implementing module to make that
    so.  One-offs can use Ecto.Schema as necessary.

---
+++ components/system/msbms_syst_settings/lib/impl/schema.ex
@@ -0,0 +1,21 @@
+# Source File: schema.ex
+# Location:    /home/scb/source/products/musebms/components/system/msbms_syst_settings/lib/impl/schema.ex
+# Project:     msbms
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https: //muse.systems
+
+defmodule MsbmsSystSettings.Impl.Schema do
+  defmacro __using__(_) do
+    quote do
+      use Ecto.Schema
+      @primary_key {:id, :binary_id, autogenerate: true}
+      @foreign_key_type :binary_id
+    end
+  end
+end
