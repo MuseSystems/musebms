4528feb69c12ab57a274ca96f548cfc7f22e48c2
Steven C. Buttgereit <github.com@musesystems.com>
2022-04-07 04:24:44 -0700

    Add starter msbms_syst_features module
    This addition will ultimately be completed in a different feature
    branch.  What is committed here is done because it started life as
    msbms_syst_settings code prior to when the dependency between features
    and settings was moved to features.

---
+++ database/cmp_msbms_syst_features/msbms_syst/api_views/syst_feature_setting_assigns/trig_i_i_syst_feature_setting_assigns.eex.sql
@@ -0,0 +1,80 @@
+DO
+$SYS_SETTINGS_OPTION$
+    BEGIN
+        IF
+            exists( SELECT TRUE
+                    FROM information_schema.tables
+                    WHERE table_schema = 'msbms_syst_data'
+                      AND table_name = 'syst_settings')
+        THEN
+
+CREATE OR REPLACE FUNCTION msbms_syst.trig_i_i_syst_feature_setting_assigns()
+RETURNS trigger AS
+$BODY$
+
+-- File:        trig_i_i_syst_feature_setting_assigns.eex.sql
+-- Location:    database\cmp_msbms_syst_features\msbms_syst\api_views\syst_feature_setting_assigns\trig_i_i_syst_feature_setting_assigns.eex.sql
+-- Project:     Muse Business Management System
+--
+-- Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+-- This file may include content copyrighted and licensed from third parties.
+--
+-- See the LICENSE file in the project root for license terms and conditions.
+-- See the NOTICE file in the project root for copyright ownership information.
+--
+-- muse.information@musesystems.com  :: https://muse.systems
+
+BEGIN
+
+    IF
+        (
+            SELECT syst_defined
+            FROM   msbms_syst_data.syst_settings
+            WHERE  id = new.setting_id
+        )
+    THEN
+        RAISE EXCEPTION
+            USING
+                MESSAGE = 'You cannot create a new feature/setting mapping for system defined ' ||
+                          'settings using this API view.',
+                DETAIL = msbms_syst_priv.get_exception_details(
+                             p_proc_schema    => 'msbms_syst'
+                            ,p_proc_name      => 'trig_i_i_syst_feature_setting_assigns'
+                            ,p_exception_name => 'invalid_api_view_call'
+                            ,p_errcode        => 'PM008'
+                            ,p_param_data     => to_jsonb(new)
+                            ,p_context_data   =>
+                                jsonb_build_object(
+                                     'tg_op',         tg_op
+                                    ,'tg_when',       tg_when
+                                    ,'tg_schema',     tg_table_schema
+                                    ,'tg_table_name', tg_table_name)),
+                ERRCODE = 'PM008',
+                SCHEMA = tg_table_schema,
+                TABLE = tg_table_name;
+    END IF;
+
+    INSERT INTO msbms_syst_data.syst_feature_setting_assigns
+        ( feature_map_id, setting_id )
+    VALUES
+        ( new.feature_map_id, new.setting_id );
+
+    RETURN new;
+
+END;
+$BODY$ LANGUAGE plpgsql VOLATILE SECURITY DEFINER;
+
+ALTER FUNCTION msbms_syst.trig_i_i_syst_feature_setting_assigns() OWNER TO <%= msbms_owner %>;
+
+REVOKE EXECUTE ON FUNCTION msbms_syst.trig_i_i_syst_feature_setting_assigns() FROM PUBLIC;
+GRANT EXECUTE ON FUNCTION msbms_syst.trig_i_i_syst_feature_setting_assigns() TO <%= msbms_appusr %>;
+GRANT EXECUTE ON FUNCTION msbms_syst.trig_i_i_syst_feature_setting_assigns() TO <%= msbms_apiusr %>;
+
+COMMENT ON
+    FUNCTION msbms_syst.trig_i_i_syst_feature_setting_assigns() IS
+$DOC$An INSTEAD OF trigger function which applies business rules when using the
+syst_settings API View for INSERT operations.$DOC$;
+
+        END IF;
+    END;
+$SYS_SETTINGS_OPTION$;
---
+++ database/cmp_msbms_syst_features/testing_support/test_data.sql
@@ -0,0 +1,109 @@
+-- File:        test_data.sql
+-- Location:    database\cmp_msbms_syst_features\testing_support\test_data.sql
+-- Project:     Muse Business Management System
+--
+-- Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+-- This file may include content copyrighted and licensed from third parties.
+--
+-- See the LICENSE file in the project root for license terms and conditions.
+-- See the NOTICE file in the project root for copyright ownership information.
+--
+-- muse.information@musesystems.com  :: https://muse.systems
+
+--------------------------------------------------------------------------------
+--  Preliminary Initialization -- Feature Mapping
+--------------------------------------------------------------------------------
+
+INSERT INTO msbms_syst_data.syst_feature_map_levels
+    ( internal_name
+    , display_name
+    , functional_type
+    , syst_description
+    , system_defined
+    , user_maintainable )
+VALUES
+    ( 'syst_settings_test_module'
+    , 'Modules'
+    , 'nonassignable'
+    , 'Broad, top level feature grouping.'
+    , TRUE
+    , FALSE )
+     ,
+    ( 'syst_settings_test_kind'
+    , 'Kinds'
+    , 'assignable'
+    , 'Identifies different kinds of mappable features such as forms, settings, or enumerations.'
+    , TRUE
+    , FALSE );
+
+INSERT INTO msbms_syst_data.syst_feature_map
+    ( internal_name
+    , display_name
+    , external_name
+    , feature_map_level_id
+    , parent_feature_map_id
+    , system_defined
+    , user_maintainable
+    , displayable
+    , syst_description
+    , sort_order )
+VALUES
+    ( 'test_module'
+    , 'Test Module'
+    , 'Test Module'
+    , ( SELECT
+            id
+        FROM msbms_syst_data.syst_feature_map_levels
+        WHERE internal_name = 'syst_settings_test_module' )
+    , NULL
+    , TRUE
+    , FALSE
+    , TRUE
+    , 'Testing at the Module level.'
+    , 1 );
+
+INSERT INTO msbms_syst_data.syst_feature_map
+    ( internal_name
+    , display_name
+    , external_name
+    , feature_map_level_id
+    , parent_feature_map_id
+    , system_defined
+    , user_maintainable
+    , displayable
+    , syst_description
+    , sort_order )
+VALUES
+    ( 'test_settings'
+    , 'Test Settings'
+    , 'Test Settings'
+    , ( SELECT
+            id
+        FROM msbms_syst_data.syst_feature_map_levels
+        WHERE internal_name = 'syst_settings_test_kind' )
+    , ( SELECT id FROM msbms_syst_data.syst_feature_map WHERE internal_name = 'test_module' )
+    , TRUE
+    , FALSE
+    , TRUE
+    , 'Testing at the Kind level.'
+    , 1 );
+
+--------------------------------------------------------------------------------
+--  Finalization -- Settings / Feature Map Assignments
+--------------------------------------------------------------------------------
+
+INSERT INTO msbms_syst_data.syst_feature_setting_assigns
+    ( feature_map_id, setting_id )
+VALUES
+    ( ( SELECT id FROM msbms_syst_data.syst_feature_map WHERE internal_name = 'test_settings' )
+    , ( SELECT id FROM msbms_syst_data.syst_settings WHERE internal_name = 'test_setting_one' ) )
+     ,
+    ( ( SELECT id FROM msbms_syst_data.syst_feature_map WHERE internal_name = 'test_settings' )
+    , ( SELECT id FROM msbms_syst_data.syst_settings WHERE internal_name = 'test_setting_two' ) )
+     ,
+    ( ( SELECT id FROM msbms_syst_data.syst_feature_map WHERE internal_name = 'test_settings' )
+    , ( SELECT id FROM msbms_syst_data.syst_settings WHERE internal_name = 'test_setting_three' ) );
+
+--------------------------------------------------------------------------------
+--  Grant all privileges to test user
+--------------------------------------------------------------------------------
