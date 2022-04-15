b2a76884a580da4976f5d5a7acb85e89aa4b1ff6
Steven C. Buttgereit <github.com@musesystems.com>
2022-04-14 20:35:14 -0700

    Add header comments to msbms_syst_settings

--- components/system/msbms_syst_settings/lib/msbms_syst_settings.ex
+++ components/system/msbms_syst_settings/lib/msbms_syst_settings.ex
@@ -1,3 +1,15 @@
+# Source File: msbms_syst_settings.ex
+# Location:    components/system/msbms_syst_settings/lib/msbms_syst_settings.ex
+# Project:     msbms
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https: //muse.systems
+
 defmodule MsbmsSystSettings do
   alias MsbmsSystSettings.Impl.Settings
   alias MsbmsSystSettings.Runtime.Server
