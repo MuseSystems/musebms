fb7a873990bf43c62727ee52e2248b72e0aad486
Steven C. Buttgereit <github.com@musesystems.com>
2022-04-15 07:22:35 -0700

    Add component msbms_syst_enums starting structure

---
+++ database/cmp_msbms_syst_enums/testing_support/test_data.eex.sql
@@ -0,0 +1,17 @@
+-- File:        test_data.eex.sql
+-- Location:    database\cmp_msbms_syst_settings\testing_support\test_data.eex.sql
+-- Project:     Muse Business Management System
+--
+-- Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+-- This file may include content copyrighted and licensed from third parties.
+--
+-- See the LICENSE file in the project root for license terms and conditions.
+-- See the NOTICE file in the project root for copyright ownership information.
+--
+-- muse.information@musesystems.com  :: https://muse.systems
+
+
+--------------------------------------------------------------------------------
+--  Primary Initialization -- Settings
+--------------------------------------------------------------------------------
+
