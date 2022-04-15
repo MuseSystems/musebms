772270c5c2004f5b6258cb7ebe51184f927ef81d
Steven C. Buttgereit <github.com@musesystems.com>
2022-04-15 07:22:35 -0700

    Add component msbms_syst_enums starting structure

---
+++ components/system/msbms_syst_enums/lib/msbms_syst_enums.ex
@@ -0,0 +1,18 @@
+defmodule MsbmsSystEnums do
+  @moduledoc """
+  Documentation for `MsbmsSystEnums`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MsbmsSystEnums.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ components/system/msbms_syst_enums/test/msbms_syst_enums_test.exs
@@ -0,0 +1,8 @@
+defmodule MsbmsSystEnumsTest do
+  use ExUnit.Case
+  doctest MsbmsSystEnums
+
+  test "greets the world" do
+    assert MsbmsSystEnums.hello() == :world
+  end
+end
