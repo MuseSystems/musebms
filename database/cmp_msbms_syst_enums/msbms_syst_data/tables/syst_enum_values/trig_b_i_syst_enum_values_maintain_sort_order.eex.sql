a09ec21e5f6af984f6a188b17fa24496c2d62859
Steven C. Buttgereit <github.com@musesystems.com>
2022-04-17 08:24:12 -0700

    Refactor database table/trigger directory struct.
    Prior to this commit, there was a separation of table and trigger
    database object definitions into specific directories.  While working
    with the API views, which include the INSTEAD OF trigger definitions in
    the same directory as the view definitions, it's become apparent that
    having table specific trigger files kept alongside the table definition
    is a more clear/intuitive way to keep these files than the previous
    method.
    
    What we're really saying is that triggers are more an aspect of table
    definition than just a functional construct.  This changes if the
    trigger is general purpose (applying to many tables), and we've not
    changed the definition of those triggers other than to bump them out of
    a "multi-table" sub-directory.  This change in thinking about table
    specific triggers has also meant that they should be in the same schema
    as the table to which they're attached.
    
    This commit updates this everywhere and tests insofar as is feasible
    (they global & instance build plans successfully build migrations).

---
+++ database/cmp_msbms_syst_enums/msbms_syst_data/tables/syst_enum_values/trig_b_i_syst_enum_values_maintain_sort_order.eex.sql
@@ -0,0 +1,45 @@
+CREATE OR REPLACE FUNCTION msbms_syst_data.trig_b_i_syst_enum_values_maintain_sort_order()
+RETURNS trigger AS
+$BODY$
+
+-- File:        trig_b_i_syst_enum_values_maintain_sort_order.eex.sql
+-- Location:    database\cmp_msbms_syst_enums\msbms_syst_data\tables\syst_enum_values\trig_b_i_syst_enum_values_maintain_sort_order.eex.sql
+-- Project:     Muse Business Management System
+--
+-- Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+-- This file may include content copyrighted and licensed from third parties.
+--
+-- See the LICENSE file in the project root for license terms and conditions.
+-- See the NOTICE file in the project root for copyright ownership information.
+--
+-- muse.information@musesystems.com  :: https://muse.systems
+
+BEGIN
+
+    new.sort_order :=
+        coalesce(
+            new.sort_order,
+            ( SELECT max( sort_order ) + 1
+              FROM msbms_syst_data.syst_enum_values
+              WHERE enum_id = new.enum_id ),
+            1 );
+
+    RETURN new;
+
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE;
+
+ALTER FUNCTION msbms_syst_data.trig_b_i_syst_enum_values_maintain_sort_order()
+    OWNER TO <%= msbms_owner %>;
+
+REVOKE EXECUTE ON FUNCTION msbms_syst_data.trig_b_i_syst_enum_values_maintain_sort_order() FROM public;
+GRANT EXECUTE ON FUNCTION msbms_syst_data.trig_b_i_syst_enum_values_maintain_sort_order() TO <%= msbms_owner %>;
+
+
+COMMENT ON FUNCTION msbms_syst_data.trig_b_i_syst_enum_values_maintain_sort_order() IS
+$DOC$For INSERTed records with a null sort_order value, this trigger will assign a
+default value assuming the new record should be inserted at the end of the sort.
+
+If the inserted record was already assigned a sort_order value, the inserted
+value is respected.$DOC$;
