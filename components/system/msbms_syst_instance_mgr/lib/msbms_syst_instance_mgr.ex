7bd898624172c4f039c34e4c2859d7e3e69a1e68
Steven C. Buttgereit <github.com@musesystems.com>
2022-05-02 14:20:27 -0700

    Add msbms_syst_instance_mgr Elixir skeleton
    With updated mix.exs and mix.lock

---
+++ components/system/msbms_syst_instance_mgr/lib/msbms_syst_instance_mgr.ex
@@ -0,0 +1,18 @@
+defmodule MsbmsSystInstanceMgr do
+  @moduledoc """
+  Documentation for `MsbmsSystInstanceMgr`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MsbmsSystInstanceMgr.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ components/system/msbms_syst_instance_mgr/mix.exs
@@ -0,0 +1,47 @@
+defmodule MsbmsSystInstanceMgr.MixProject do
+  use Mix.Project
+
+  @name :msbms_syst_instance_mgr
+  @version "0.1.0"
+
+  @deps [
+    # Third Party Dependencies
+    {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
+    {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+    {:ex_doc, "~> 0.20", only: :dev, runtime: false},
+
+    # Muse Systems Business Management System Components
+    {:msbms_syst_error, path: "../msbms_syst_error"},
+    {:msbms_syst_datastore, path: "../msbms_syst_datastore"},
+    {:msbms_syst_enums, path: "../msbms_syst_enums"}
+  ]
+
+  @dialyzer_opts [
+    flags: ["-Wunmatched_returns", :error_handling, :race_conditions, :underspecs],
+    plt_add_apps: [:mix]
+  ]
+
+  # ------------------------------------------------------------
+
+  def project do
+    in_production = Mix.env() == :prod
+
+    [
+      app: @name,
+      version: @version,
+      elixir: "~> 1.13",
+      deps: @deps,
+      build_embedded: in_production,
+      start_permanent: in_production,
+      dialyzer: @dialyzer_opts
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [
+        :logger
+      ]
+    ]
+  end
+end
