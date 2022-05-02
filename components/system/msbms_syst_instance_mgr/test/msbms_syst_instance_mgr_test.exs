97a71a41dfd56d8bccd5b0675d7718db11b3c428
Steven C. Buttgereit <github.com@musesystems.com>
2022-05-02 14:20:27 -0700

    Add msbms_syst_instance_mgr Elixir skeleton
    With updated mix.exs and mix.lock

---
+++ components/system/msbms_syst_instance_mgr/test/msbms_syst_instance_mgr_test.exs
@@ -0,0 +1,8 @@
+defmodule MsbmsSystInstanceMgrTest do
+  use ExUnit.Case
+  doctest MsbmsSystInstanceMgr
+
+  test "greets the world" do
+    assert MsbmsSystInstanceMgr.hello() == :world
+  end
+end
