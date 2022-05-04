665cb915773a65dd1e91e122801389b758fc9769
Steven C. Buttgereit <github.com@musesystems.com>
2022-05-04 05:08:14 -0700

    Add owners test data; rearrange some ordering
    In msbms_syst_instance_mgr test_data, add support for the syst_owners
    table.
    
    Also, reorder the syst_applications insert values.   While it may or may
    not make a difference, we want to try and disrupt "database order" so
    that sorting in the application as -maybe- a little more chance to
    fail.

--- database/cmp_msbms_syst_instance_mgr/testing_support/test_data.eex.sql
+++ database/cmp_msbms_syst_instance_mgr/testing_support/test_data.eex.sql
@@ -13,9 +13,34 @@
 --
 --  Applications
 --
+
 INSERT INTO msbms_syst_data.syst_applications
     ( internal_name, display_name, syst_description )
 VALUES
-    ( 'test_app_2', 'Test App Two', 'Test App Two Description' )
+    ( 'test_app_2', 'Test App 2', 'Test App Two Description' )
      ,
-    ( 'test_app_1', 'Test App One', 'Test App One Description' );
+    ( 'test_app_1', 'Test App 1', 'Test App One Description' );
+
+--
+--  Owners
+--
+
+
+INSERT INTO msbms_syst_data.syst_owners
+    ( internal_name, display_name, owner_state_id )
+VALUES
+    ( 'owner_4', 'Owner 4 Inactive', ( SELECT id
+                                         FROM msbms_syst_data.syst_enum_items
+                                         WHERE internal_name = 'owner_states_sysdef_inactive' ) )
+     ,
+    ( 'owner_3', 'Owner 3 Active', ( SELECT id
+                                         FROM msbms_syst_data.syst_enum_items
+                                         WHERE internal_name = 'owner_states_sysdef_active' ) )
+     ,
+    ( 'owner_1', 'Owner 1 Active', ( SELECT id
+                                       FROM msbms_syst_data.syst_enum_items
+                                       WHERE internal_name = 'owner_states_sysdef_active' ) )
+     ,
+    ( 'owner_2', 'Owner 2 Inactive', ( SELECT id
+                                          FROM msbms_syst_data.syst_enum_items
+                                          WHERE internal_name = 'owner_states_sysdef_inactive' ) );
