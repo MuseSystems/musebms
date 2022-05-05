6e7be1b09cc5b2542f015169851fa49d6d9830f2
Steven C. Buttgereit <github.com@musesystems.com>
2022-05-05 16:23:13 -0700

    Add SystOwners functionality
    This commit adds the functionality for managing Owners to the
    msbms_syst_instance_mgr module.  This includes the implementation, API,
    tests, and docs.

---
+++ components/system/msbms_syst_instance_mgr/lib/impl/owners.ex
@@ -0,0 +1,189 @@
+# Source File: owners.ex
+# Location:    components/system/msbms_syst_instance_mgr/lib/impl/owners.ex
+# Project:     msbms
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https: //muse.systems
+
+defmodule MsbmsSystInstanceMgr.Impl.Owners do
+  import Ecto.Query
+
+  alias MsbmsSystInstanceMgr.Data
+  alias MsbmsSystInstanceMgr.Types
+
+  require Logger
+
+  @moduledoc false
+
+  ######
+  #
+  # Logic for managing and accessing SystOwners data.
+  #
+  ######
+
+  @spec list_owners(
+          Keyword.t(
+            state_functional_types: list(Types.owner_state_functional_types()) | [],
+            sort: boolean()
+          )
+        ) ::
+          {:ok, list(Data.SystOwners.t())} | {:error, MsbmsSystError.t()}
+  def list_owners(opts_given) do
+    opts_default = [state_functional_types: [], sort: false]
+
+    opts = Keyword.merge(opts_given, opts_default, fn _k, v1, _v2 -> v1 end)
+
+    from(o in Data.SystOwners,
+      join: s in assoc(o, :owner_state),
+      join: f in assoc(s, :functional_type),
+      select: %{
+        id: o.id,
+        internal_name: o.internal_name,
+        display_name: o.display_name,
+        state_id: s.id,
+        state_display_name: s.display_name,
+        state_external_name: s.external_name,
+        state_functional_type_id: f.id,
+        state_functional_type: f.internal_name
+      }
+    )
+    |> maybe_add_owner_display_name_sort(opts[:sort])
+    |> maybe_add_owner_state_filter(opts[:state_functional_types])
+    |> MsbmsSystDatastore.all()
+    |> then(&{:ok, &1})
+  rescue
+    error ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+
+      {
+        :error,
+        %MsbmsSystError{
+          code: :undefined_error,
+          message: "Failure retrieving owners list.",
+          cause: error
+        }
+      }
+  end
+
+  defp maybe_add_owner_state_filter(query, [_ | _] = functional_types) do
+    functional_types
+    |> Enum.map(&Atom.to_string/1)
+    |> then(&from([o, s, f] in query, where: f.internal_name in ^&1))
+  end
+
+  defp maybe_add_owner_state_filter(query, _functional_types), do: query
+
+  defp maybe_add_owner_display_name_sort(query, true),
+    do: from(o in query, order_by: o.display_name)
+
+  defp maybe_add_owner_display_name_sort(query, false), do: query
+
+  @spec create_owner(Types.owner_name(), String.t(), Ecto.UUID.t()) ::
+          {:ok, Data.SystOwners.t()} | {:error, MsbmsSystError.t()}
+  def create_owner(owner_name, owner_display_name, owner_state_id) do
+    %Data.SystOwners{}
+    |> Data.SystOwners.changeset(%{
+      internal_name: owner_name,
+      display_name: owner_display_name,
+      owner_state_id: owner_state_id
+    })
+    |> MsbmsSystDatastore.insert!(returning: true)
+    |> then(&{:ok, &1})
+  rescue
+    error ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+
+      {
+        :error,
+        %MsbmsSystError{
+          code: :undefined_error,
+          message: "Failure creating new owner.",
+          cause: error
+        }
+      }
+  end
+
+  @spec set_owner_values(Ecto.UUID.t(), Types.owner_params()) ::
+          {:ok, Data.SystOwners.t()} | {:error, MsbmsSystError.t()}
+  def set_owner_values(owner_id, owner_params) do
+    MsbmsSystDatastore.get!(Data.SystOwners, owner_id)
+    |> Data.SystOwners.changeset(owner_params)
+    |> MsbmsSystDatastore.update!(returning: true)
+    |> then(&{:ok, &1})
+  rescue
+    error ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+
+      {
+        :error,
+        %MsbmsSystError{
+          code: :undefined_error,
+          message: "Failure updating existing owner.",
+          cause: error
+        }
+      }
+  end
+
+  @spec purge_owner(Ecto.UUID.t()) ::
+          {:ok, {non_neg_integer(), nil | [term()]}} | {:error, MsbmsSystError.t()}
+  def purge_owner(owner_id) do
+    from(o in Data.SystOwners,
+      join: s in assoc(o, :owner_state),
+      join: f in assoc(s, :functional_type),
+      where: o.id == ^owner_id and f.internal_name == "owner_states_purge_eligible"
+    )
+    |> MsbmsSystDatastore.delete_all(returning: true)
+    |> maybe_owner_purged()
+    |> then(&{:ok, &1})
+  rescue
+    error ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+
+      {
+        :error,
+        %MsbmsSystError{
+          code: :undefined_error,
+          message: "Failure purging an existing owner.",
+          cause: error
+        }
+      }
+  end
+
+  defp maybe_owner_purged({1, _rows} = delete_result), do: delete_result
+
+  defp maybe_owner_purged(delete_result) do
+    raise MsbmsSystError,
+      code: :database_error,
+      message: "Failure to delete single owner.",
+      cause: delete_result
+  end
+
+  @spec purge_all_eligible_owners() ::
+          {:ok, {non_neg_integer(), nil | [term()]}} | {:error, MsbmsSystError.t()}
+  def purge_all_eligible_owners() do
+    from(o in Data.SystOwners,
+      join: s in assoc(o, :owner_state),
+      join: f in assoc(s, :functional_type),
+      where: f.internal_name == "owner_states_purge_eligible"
+    )
+    |> MsbmsSystDatastore.delete_all(returning: true)
+    |> then(&{:ok, &1})
+  rescue
+    error ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+
+      {
+        :error,
+        %MsbmsSystError{
+          code: :undefined_error,
+          message: "Failure purging eligible owners.",
+          cause: error
+        }
+      }
+  end
+end
--- components/system/msbms_syst_instance_mgr/test/msbms_syst_instance_mgr_test.exs
+++ components/system/msbms_syst_instance_mgr/test/msbms_syst_instance_mgr_test.exs
@@ -65,16 +65,20 @@ defmodule MsbmsSystInstanceMgrTest do
   #  correct here.
 
   setup_all do
-    setup_test_environment()
+    setup_testing_database()
 
-    on_exit(&cleanup_test_environment/0)
+    on_exit(&cleanup_testing_database/0)
+  end
+
+  setup_all do
+    setup_enums_service()
   end
 
   setup %{} do
     [datastore_context: MsbmsSystDatastore.set_datastore_context(@datastore_context_name)]
   end
 
-  defp setup_test_environment do
+  defp setup_testing_database do
     :ok = build_migrations()
 
     datastore_options = @datastore_options
@@ -93,7 +97,7 @@ defmodule MsbmsSystInstanceMgrTest do
     {:ok, _, _} = MsbmsSystDatastore.start_datastore(datastore_options)
   end
 
-  defp cleanup_test_environment do
+  defp cleanup_testing_database do
     datastore_options = @datastore_options
     :ok = MsbmsSystDatastore.drop_datastore(datastore_options)
     File.rm_rf!("priv/database/cmp_msbms_syst_instance_mgr")
@@ -109,11 +113,209 @@ defmodule MsbmsSystInstanceMgrTest do
     ])
   end
 
+  defp setup_enums_service do
+    service_child_spec = %{
+      id: MsbmsSystInstanceMgrTestingEnumService,
+      start: {MsbmsSystEnums, :start_link, [{:instance_mgr, @datastore_context_name}]}
+    }
+
+    start_supervised!(service_child_spec)
+
+    [
+      owner_states: MsbmsSystEnums.get_enum_items(:instance_mgr, "owner_states"),
+      instance_states: MsbmsSystEnums.get_enum_items(:instance_mgr, "instance_states"),
+      instance_types: MsbmsSystEnums.get_enum_items(:instance_mgr, "instance_types")
+    ]
+  end
+
   doctest MsbmsSystInstanceMgr
 
   test "Can List Applications" do
     assert {:ok, apps} = MsbmsSystInstanceMgr.list_applications()
     assert 2 = length(apps)
     assert %MsbmsSystInstanceMgr.Data.SystApplications{internal_name: "test_app_1"} = hd(apps)
+  end
+
+  test "Can List All Owners" do
+    assert {:ok, owners} = MsbmsSystInstanceMgr.list_owners()
+    assert 4 <= length(owners)
+  end
+
+  test "Can List Sorted Owners" do
+    assert {:ok, owners} = MsbmsSystInstanceMgr.list_owners(sort: true)
+    assert 4 <= length(owners)
+    assert %{internal_name: "owner_1"} = hd(owners)
+  end
+
+  test "Can List Owners by Status" do
+    assert {:ok, owners} =
+             MsbmsSystInstanceMgr.list_owners(state_functional_types: [:owner_states_active])
+
+    assert 2 <= length(owners)
+  end
+
+  test "Can List Sorted Owners by Status" do
+    assert {:ok, owners} =
+             MsbmsSystInstanceMgr.list_owners(
+               sort: true,
+               state_functional_types: [:owner_states_inactive]
+             )
+
+    assert 2 <= length(owners)
+    assert %{internal_name: "owner_2"} = hd(owners)
+  end
+
+  test "Can Create New Owner" do
+    default_owner_state = MsbmsSystEnums.get_default_enum_item(:instance_mgr, "owner_states")
+
+    assert {:ok, %MsbmsSystInstanceMgr.Data.SystOwners{internal_name: "owner_create_1"}} =
+             MsbmsSystInstanceMgr.create_owner(
+               "owner_create_1",
+               "Owner Create Test",
+               default_owner_state.id
+             )
+  end
+
+  test "Can Change Existing Owner Internal Name" do
+    change = %{internal_name: "changed_owner_create_2"}
+
+    default_owner_state = MsbmsSystEnums.get_default_enum_item(:instance_mgr, "owner_states")
+
+    {:ok, test_owner} =
+      MsbmsSystInstanceMgr.create_owner(
+        "owner_create_2",
+        "Owner Change Internal Name Test",
+        default_owner_state.id
+      )
+
+    assert {:ok, %MsbmsSystInstanceMgr.Data.SystOwners{internal_name: "changed_owner_create_2"}} =
+             MsbmsSystInstanceMgr.set_owner_values(test_owner.id, change)
+  end
+
+  test "Can Change Existing Owner Display Name" do
+    change = %{display_name: "Owner Change Display Name Test Is Changed"}
+
+    default_owner_state = MsbmsSystEnums.get_default_enum_item(:instance_mgr, "owner_states")
+
+    {:ok, test_owner} =
+      MsbmsSystInstanceMgr.create_owner(
+        "owner_create_3",
+        "Owner Change Display Name Test",
+        default_owner_state.id
+      )
+
+    assert {:ok,
+            %MsbmsSystInstanceMgr.Data.SystOwners{
+              display_name: "Owner Change Display Name Test Is Changed"
+            }} = MsbmsSystInstanceMgr.set_owner_values(test_owner.id, change)
+  end
+
+  test "Can Change Existing Owner Owner State", context do
+    default_owner_state = MsbmsSystEnums.get_default_enum_item(:instance_mgr, "owner_states")
+
+    new_state =
+      MsbmsSystEnums.get_default_enum_item(:instance_mgr, "owner_states",
+        functional_type_name: "owner_states_inactive"
+      )
+
+    change = %{owner_state_id: new_state.id}
+
+    {:ok, test_owner} =
+      MsbmsSystInstanceMgr.create_owner(
+        "owner_create_4",
+        "Owner Change Owner State Test",
+        default_owner_state.id
+      )
+
+    assert {:ok,
+            %MsbmsSystInstanceMgr.Data.SystOwners{
+              owner_state_id: updated_id
+            }} = MsbmsSystInstanceMgr.set_owner_values(test_owner.id, change)
+
+    assert new_state.id == updated_id
+  end
+
+  test "Can Change Existing Owner Multiple Values" do
+    default_owner_state = MsbmsSystEnums.get_default_enum_item(:instance_mgr, "owner_states")
+
+    new_state =
+      MsbmsSystEnums.get_default_enum_item(:instance_mgr, "owner_states",
+        functional_type_name: "owner_states_inactive"
+      )
+
+    change = %{
+      owner_state_id: new_state.id,
+      internal_name: "owner_create_5_changed",
+      display_name: "Owner Change Owner Multi Test Changed"
+    }
+
+    {:ok, test_owner} =
+      MsbmsSystInstanceMgr.create_owner(
+        "owner_create_5",
+        "Owner Change Owner Multi Test",
+        default_owner_state.id
+      )
+
+    assert {:ok, updated_owner} = MsbmsSystInstanceMgr.set_owner_values(test_owner.id, change)
+
+    assert new_state.id == updated_owner.owner_state_id
+    assert "owner_create_5_changed" == updated_owner.internal_name
+    assert "Owner Change Owner Multi Test Changed" == updated_owner.display_name
+  end
+
+  test "Can Purge a Single Purge Eligible Owner" do
+    default_owner_state =
+      MsbmsSystEnums.get_default_enum_item(:instance_mgr, "owner_states",
+        functional_type_name: "owner_states_purge_eligible"
+      )
+
+    {:ok, test_owner} =
+      MsbmsSystInstanceMgr.create_owner(
+        "owner_purge_1",
+        "Owner Purge Single Owner Test",
+        default_owner_state.id
+      )
+
+    assert {:ok, {1, _rows}} = MsbmsSystInstanceMgr.purge_owner(test_owner.id)
+  end
+
+  test "Cannot Purge a Single Ineligible Owner" do
+    default_owner_state = MsbmsSystEnums.get_default_enum_item(:instance_mgr, "owner_states")
+
+    {:ok, test_owner} =
+      MsbmsSystInstanceMgr.create_owner(
+        "owner_purge_2",
+        "Owner No Purge Single Owner Test",
+        default_owner_state.id
+      )
+
+    assert {:error, _reason} = MsbmsSystInstanceMgr.purge_owner(test_owner.id)
+  end
+
+  test "Can Purge a All Purge Eligible Owners" do
+    default_owner_state =
+      MsbmsSystEnums.get_default_enum_item(:instance_mgr, "owner_states",
+        functional_type_name: "owner_states_purge_eligible"
+      )
+
+    {:ok, _} =
+      MsbmsSystInstanceMgr.create_owner(
+        "owner_purge_3",
+        "Owner Purge All Eligible Owners 1",
+        default_owner_state.id
+      )
+
+    {:ok, _} =
+      MsbmsSystInstanceMgr.create_owner(
+        "owner_purge_4",
+        "Owner Purge All Eligible Owners 2",
+        default_owner_state.id
+      )
+
+    assert {:ok, {2, _rows}} = MsbmsSystInstanceMgr.purge_all_eligible_owners()
+
+    # A second call should result in no deletions, but no error.  This test could
+    # cause race conditions... keep an eye on if that concern is real.
+    assert {:ok, {0, _rows}} = MsbmsSystInstanceMgr.purge_all_eligible_owners()
   end
 end
