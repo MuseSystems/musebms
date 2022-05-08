108542675a3ed7a3361b262cab571f696ea54901
Steven C. Buttgereit <github.com@musesystems.com>
2022-05-08 07:54:09 -0700

    Add into documentation for MsbmsSystInstanceMgr

--- components/system/msbms_syst_instance_mgr/lib/msbms_syst_instance_mgr.ex
+++ components/system/msbms_syst_instance_mgr/lib/msbms_syst_instance_mgr.ex
@@ -4,7 +4,13 @@ defmodule MsbmsSystInstanceMgr do
   alias MsbmsSystInstanceMgr.Types
 
   @moduledoc """
-  Documentation for `MsbmsSystInstanceMgr`.
+  Functionality allowing for the management of application Instances.
+
+  "Instances" are instances of running application environments.  Instances are
+  established to host the application for different purposes, such as for
+  running the application for production, training, and testing purposes; or as
+  a means to implement multi-tenancy where each tenant application environment
+  is an Instance.
   """
 
   @doc """
