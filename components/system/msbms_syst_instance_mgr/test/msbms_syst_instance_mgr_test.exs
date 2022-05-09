c8f2f1ba0e4135f7795e01a1ca8d8d62a6067c96
Steven C. Buttgereit <github.com@musesystems.com>
2022-05-09 16:43:48 -0700

    Add more Instance testing

--- components/system/msbms_syst_instance_mgr/test/msbms_syst_instance_mgr_test.exs
+++ components/system/msbms_syst_instance_mgr/test/msbms_syst_instance_mgr_test.exs
@@ -409,4 +409,65 @@ defmodule MsbmsSystInstanceMgrTest do
              }
            end)
   end
+
+  test "Can Retrieve Single Instance" do
+    assert {:ok, %MsbmsSystInstanceMgr.Data.SystInstances{internal_name: "test_instance_1"}} =
+             MsbmsSystInstanceMgr.get_instance_by_name("test_instance_1")
+  end
+
+  test "Can List Instance Types" do
+    assert {:ok, instance_types_list} = MsbmsSystInstanceMgr.list_instance_types(:instance_mgr)
+  end
+
+  test "Can create Instance Type" do
+    new_instance_type = %{
+      internal_name: "can_create_instance_type",
+      display_name: "Can Create Instance Type",
+      enum_default: true,
+      user_description: "Can Create Instance Type Description",
+      external_name: "Can Create Instance Type",
+      user_options: %{
+        datastore_contexts: [
+          %{
+            id: :test_datastore_context_1,
+            db_pool_size: 3
+          },
+          %{
+            id: :test_datastore_context_1,
+            db_pool_size: 3
+          }
+        ]
+      }
+    }
+
+    assert {:ok, %MsbmsSystEnums.Data.SystEnumItems{internal_name: "can_create_instance_type"}} =
+             MsbmsSystInstanceMgr.create_instance_type(:instance_mgr, new_instance_type)
+  end
+
+  test "Can Get Instance by Instance Internal Name" do
+    assert {:ok, %MsbmsSystEnums.Data.SystEnumItems{internal_name: "instance_types_big_instance"}} =
+             MsbmsSystInstanceMgr.get_instance_type_by_name(
+               :instance_mgr,
+               "instance_types_big_instance"
+             )
+  end
+
+  test "Can Set Instance Type Values" do
+    instance_type_changes = %{
+      external_name: "Standard Instance (Changed)",
+      display_name: "Instance Types / Std. Instance (Changed)"
+    }
+
+    assert {:ok,
+            %MsbmsSystEnums.Data.SystEnumItems{
+              internal_name: "instance_types_std_instance",
+              external_name: "Standard Instance (Changed)",
+              display_name: "Instance Types / Std. Instance (Changed)"
+            }} =
+             MsbmsSystInstanceMgr.set_instance_type_values(
+               :instance_mgr,
+               "instance_types_std_instance",
+               instance_type_changes
+             )
+  end
 end
