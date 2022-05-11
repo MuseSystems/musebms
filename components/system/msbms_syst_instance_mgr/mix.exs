3c44180325cc4c588dbf18ce41862fa828a3acc2
Steven C. Buttgereit <github.com@musesystems.com>
2022-05-11 16:29:24 -0700

    Add MsbmsSystInstanceMgr support for new testing
    These changes were forgotten in a previous commit where the testing
    setup was refactored.

--- components/system/msbms_syst_instance_mgr/mix.exs
+++ components/system/msbms_syst_instance_mgr/mix.exs
@@ -33,7 +33,8 @@ defmodule MsbmsSystInstanceMgr.MixProject do
       deps: @deps,
       build_embedded: in_production,
       start_permanent: in_production,
-      dialyzer: @dialyzer_opts
+      dialyzer: @dialyzer_opts,
+      elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
 
@@ -44,4 +45,8 @@ defmodule MsbmsSystInstanceMgr.MixProject do
       ]
     ]
   end
+
+  defp elixirc_paths(:test), do: elixirc_paths() ++ ["test/support"]
+  defp elixirc_paths(_), do: elixirc_paths()
+  defp elixirc_paths(), do: ["lib"]
 end
