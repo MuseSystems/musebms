ce1d01e437a900a6418f2e79231ddcb175414b0b
Steven C. Buttgereit <github.com@musesystems.com>
2022-05-21 11:37:33 -0700

    Add instance type functional types to init data
    Instance types now require a functional type.  The intent of the new
    functional types mostly about filtering which database servers are
    available for use with a given instance; database server entries in the
    options TOML file designate which instance types (or "instance classes")
    are supported by the server, if any.

--- database/cmp_msbms_syst_instance_mgr/seed_data/initialize_enum_instance_types.eex.sql
+++ database/cmp_msbms_syst_instance_mgr/seed_data/initialize_enum_instance_types.eex.sql
@@ -14,24 +14,46 @@ DO
 $INIT_ENUM$
 BEGIN
 
-PERFORM
-    msbms_syst_priv.initialize_enum(
-        p_enum_def => $INIT_ENUM_INSTANCE_TYPES$
+    PERFORM msbms_syst_priv.initialize_enum(
+        p_enum_def =>
+            $INIT_ENUM_INSTANCE_TYPES$
+            {
+              "internal_name": "instance_types",
+              "display_name": "Instance Types",
+              "syst_description": "Defines the available kinds of instances and specifies their capabilities.  This typing works both for simple informational categorization as well as functional concerns within the application.",
+              "syst_defined": true,
+              "user_maintainable": true,
+              "default_syst_options": null,
+              "default_user_options": null,
+              "functional_types": [
                 {
-                  "internal_name": "instance_types",
-                  "display_name": "Instance Types",
-                  "syst_description": "Defines the available kinds of instances and specifies their capabilities.  This typing works both for simple informational categorization as well as functional concerns within the application.",
-                  "syst_defined": true,
-                  "user_maintainable": true,
-                  "default_syst_options": {
-                    "app_db_context_pool_size": 10,
-                    "api_db_context_pool_size": 10
-                    },
-                  "default_user_options": null,
-                  "functional_types": [],
-                  "enum_items": []
+                  "internal_name": "instance_types_primary",
+                  "display_name": "Instance Types / Primary",
+                  "external_name": "Primary",
+                  "syst_description": "General purpose instance type which includes instances designated as 'production'.  In most cases, this is the only instance functional type that is required."
+                },
+                {
+                  "internal_name": "instance_types_linked",
+                  "display_name": "Instance Types / Linked",
+                  "external_name": "Linked",
+                  "syst_description": "Designates a class of instances which are the children of a parent instance.  Typically these are non-production instances which are copies of their parent instance."
+                },
+                {
+                  "internal_name": "instance_types_demo",
+                  "display_name": "Instance Types / Demo",
+                  "external_name": "Demo",
+                  "syst_description": "Instances which support application demonstration purposes.  Most installations will not require this type to be used."
+                },
+                {
+                  "internal_name": "instance_types_reserved",
+                  "display_name": "Instance Types / Reserved",
+                  "external_name": "Reserved",
+                  "syst_description": "Other special use instances which don't fall under the other categories.  Typically there is no need to use this class of instance type."
                 }
-                    $INIT_ENUM_INSTANCE_TYPES$::jsonb);
+              ],
+              "enum_items": []
+            }
+            $INIT_ENUM_INSTANCE_TYPES$::jsonb );
 
 END;
 $INIT_ENUM$;
