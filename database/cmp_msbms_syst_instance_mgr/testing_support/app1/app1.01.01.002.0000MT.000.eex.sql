fae5453029a700b7de0eaf06904234447cb818a8
Steven C. Buttgereit <github.com@musesystems.com>
2022-07-12 12:54:51 -0700

    Add MsbmsSystInstanceMgr integration test support
    This commit refactors test data and testing support/helpers to be able
    to handle separate unit and integration test runs.  Here the unit test
    definition means tests concentrating on testing individual API functions
    (asynchronous/random ordering) and integration testing means to test an
    end-to-end process involving persistent, long running services and
    information evolution.

---
+++ database/cmp_msbms_syst_instance_mgr/testing_support/app1/app1.01.01.002.0000MT.000.eex.sql
@@ -0,0 +1,32 @@
+-- File:        01.01.002.0000MT.000.eex.sql
+-- Location:    database\cmp_msbms_syst_instance_mgr\testing_support\app1\01.01.002.0000MT.000.eex.sql
+-- Project:     Muse Business Management System
+--
+-- Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+-- This file may include content copyrighted and licensed from third parties.
+--
+-- See the LICENSE file in the project root for license terms and conditions.
+-- See the NOTICE file in the project root for copyright ownership information.
+--
+-- muse.information@musesystems.com  :: https://muse.systems
+
+DO
+$MIGRATION$
+    BEGIN
+        INSERT INTO testing.test_header
+            ( test_value )
+        VALUES
+            ( '<%= app1_owner %>' )
+             ,
+            ( '<%= app1_appusr %>' )
+             ,
+            ( '<%= app1_apiusr %>' );
+
+        INSERT INTO testing.test_detail
+            ( test_header_id, test_detail_value )
+        SELECT
+            id
+          , 'A test detail value for header: ' || id
+        FROM testing.test_header;
+    END;
+$MIGRATION$;
