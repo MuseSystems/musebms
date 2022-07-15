456a0b3d370f5b5ad5f40dd46fd197e05b6a391e
Steven C. Buttgereit <github.com@musesystems.com>
2022-07-14 19:05:20 -0700

    Remove more Dialyzer bad :race_conditions configs

--- components/system/msbms_syst_instance_mgr/mix.exs
+++ components/system/msbms_syst_instance_mgr/mix.exs
@@ -19,7 +19,7 @@ defmodule MsbmsSystInstanceMgr.MixProject do
   ]
 
   @dialyzer_opts [
-    flags: ["-Wunmatched_returns", :error_handling, :race_conditions, :underspecs],
+    flags: ["-Wunmatched_returns", :error_handling, :underspecs],
     plt_add_apps: [:mix]
   ]
 
