2d60f53d5ae9d7d62a824c3c5e6b3a48e558d1e0
Steven C. Buttgereit <github.com@musesystems.com>
2022-07-16 05:14:29 -0700

    Fix incorrect typespecs in MsbmsSystDatastore
    These came in while I was experimenting with using Gradient instead of
    Dialyzer.

--- components/system/msbms_syst_datastore/lib/datastore.ex
+++ components/system/msbms_syst_datastore/lib/datastore.ex
@@ -66,6 +66,6 @@ defmodule MsbmsSystDatastore.Datastore do
     Datastore and Datastore Context related connection options.  See
     `t:MsbmsSystDatastore.Types.datastore_options/0` for more.
   """
-  @spec start_link(Keyword.t()) :: Supervisor.on_start()
+  @spec start_link(Keyword.t()) :: Supervisor.on_start_child()
   defdelegate start_link(opts \\ []), to: Runtime.Datastore, as: :start_link_datastore
 end
