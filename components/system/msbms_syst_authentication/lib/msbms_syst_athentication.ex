5b27f8e49c42dbecbba64f018d06860a740aa8ec
Steven C. Buttgereit <scb@musesystems.com>
2022-07-20 11:56:38 -0700

    Add component MsbmsSystAuthentication
    This is "a bit more than skeleton" start for the MsbmsSystAuthentication
    module.  This has the dependencies and setup and the mix.lock file
    built.  Otherwise, still a very early days build.

---
+++ components/system/msbms_syst_authentication/lib/msbms_syst_athentication.ex
@@ -0,0 +1,18 @@
+defmodule MsbmsSystAthentication do
+  @moduledoc """
+  Documentation for `MsbmsSystAthentication`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MsbmsSystAthentication.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ components/system/msbms_syst_authentication/test/msbms_syst_athentication_test.exs
@@ -0,0 +1,8 @@
+defmodule MsbmsSystAthenticationTest do
+  use ExUnit.Case
+  doctest MsbmsSystAthentication
+
+  test "greets the world" do
+    assert MsbmsSystAthentication.hello() == :world
+  end
+end
