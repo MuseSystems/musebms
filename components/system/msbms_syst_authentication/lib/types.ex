a860bbf5e34a7196dcab017c7846e1dc68b1329a
Steven C. Buttgereit <scb@musesystems.com>
2022-07-27 13:13:16 -0700

    Add MsbmsSystAuthentication support Types def.
    This isn't complete, but sufficient for now while the Ecto schema
    modules are being built.

---
+++ components/system/msbms_syst_authentication/lib/types.ex
@@ -0,0 +1,37 @@
+# Source File: types.ex
+# Location:    components/system/msbms_syst_authentication/lib/types.ex
+# Project:     msbms
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https: //muse.systems
+
+defmodule MsbmsSystAuthentication.Types do
+  #
+  # Note that the ordering of typespecs here is alphabetical.
+  #
+
+  @moduledoc """
+  Types used by the Authentication module.
+  """
+
+  @type access_account_id() :: Ecto.UUID.t()
+
+  @type access_account_name() :: String.t()
+
+  @type access_account_state_id() :: MsbmsSystEnums.Types.enum_item_id()
+
+  @type access_account_state_name() :: MsbmsSystEnums.Types.enum_item_name()
+
+  @type credential_type_id() :: MsbmsSystEnums.Types.enum_item_id()
+
+  @type credential_type_name() :: MsbmsSystEnums.Types.enum_item_name()
+
+  @type identity_id() :: Ecto.UUID.t()
+
+  @type identity_type_id() :: MsbmsSystEnums.Types.enum_item_id()
+end
