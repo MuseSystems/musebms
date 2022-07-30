f28a67b925afbb04057c62dae967057671df0f4f
Steven C. Buttgereit <github.com@musesystems.com>
2022-07-30 09:27:35 -0700

    Add get_access_account_id_by_name func and support
    In MsbmsSystAuthentication, add the initial supporting files for the
    Access Account API and start with a lookup function which will be used
    by the syst_access_account_instance_assoc name resolution functions.

--- components/system/msbms_syst_authentication/lib/msbms_syst_authentication.ex
+++ components/system/msbms_syst_authentication/lib/msbms_syst_authentication.ex
@@ -1,18 +1,34 @@
 defmodule MsbmsSystAuthentication do
+  alias MsbmsSystAuthentication.Data
+  alias MsbmsSystAuthentication.Impl
+  alias MsbmsSystAuthentication.Types
+
   @moduledoc """
-  Documentation for `MsbmsSystAuthentication`.
+  API for the management of system user authentication.
+
+  Authentication is a global concern allowing users to be either bound to 
+  specific Application Instances and Owners or to be independent and valid for
+  a number of Applications and Instances.
   """
 
+  @doc section: :access_account_data
   @doc """
-  Hello world.
+  Looks up an Access Account record ID by its internal name.
+
+  ## Parameters
+
+    * `access_account_name` - the internal name of the record for which to 
+    retrieve the record ID.
 
   ## Examples
 
-      iex> MsbmsSystAuthentication.hello()
-      :world
-
+    iex> {:ok, access_account_id}  = 
+    ...>   MsbmsSystAuthentication.get_access_account_id_by_name("accnt1")
+    iex> is_binary(access_account_id)
+    true   
   """
-  def hello do
-    :world
-  end
+
+  @spec get_access_account_id_by_name(Types.access_account_name()) ::
+          {:ok, Types.access_account_id()} | {:error, MsbmsSystError.t()}
+  defdelegate get_access_account_id_by_name(access_account_name), to: Impl.AccessAccount
 end
