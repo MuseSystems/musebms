c2d92bd35c50c540d1a7dce196b42aec797cd259
Steven C. Buttgereit <github.com@musesystems.com>
2022-08-07 09:20:18 -0700

    Skeleton MsbmsSystMnesia Elixir project

---
+++ components/system/msbms_syst_mnesia/lib/msbms_syst_mnesia.ex
@@ -0,0 +1,18 @@
+defmodule MsbmsSystMnesia do
+  @moduledoc """
+  Documentation for `MsbmsSystMnesia`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MsbmsSystMnesia.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
