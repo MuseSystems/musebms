0216a8e2ee6eca40e49bf4b7a58f18fe2542f4d9
Steven C. Buttgereit <github.com@musesystems.com>
2022-08-07 07:31:45 -0700

    Skeleton MsbmsSystRateLimiter Elixir project

---
+++ components/system/msbms_syst_rate_limiter/test/msbms_syst_rate_limiter_test.exs
@@ -0,0 +1,8 @@
+defmodule MsbmsSystRateLimiterTest do
+  use ExUnit.Case
+  doctest MsbmsSystRateLimiter
+
+  test "greets the world" do
+    assert MsbmsSystRateLimiter.hello() == :world
+  end
+end
