540b2a8caf69054d030fc0bd2168875885796038
Steven C. Buttgereit <github.com@musesystems.com>
2022-08-07 08:12:31 -0700

    Add MsbmsSystRateLimiter compile config & deps

--- components/system/msbms_syst_rate_limiter/mix.exs
+++ components/system/msbms_syst_rate_limiter/mix.exs
@@ -1,28 +1,48 @@
 defmodule MsbmsSystRateLimiter.MixProject do
   use Mix.Project
 
+  @name :msbms_syst_rate_limiter
+  @version "0.1.0"
+
+  @deps [
+    # Third Party Dependencies
+    {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
+    {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+    {:ex_doc, "~> 0.20", only: :dev, runtime: false},
+    {:hammer_backend_mnesia, "~> 0.5"},
+    {:hammer, "~> 6.0"},
+
+    # Muse Systems Business Management System Components
+    {:msbms_syst_error, path: "../msbms_syst_error"},
+    {:msbms_syst_utils, path: "../msbms_syst_utils"}
+  ]
+
+  @dialyzer_opts [
+    flags: ["-Wunmatched_returns", :error_handling, :underspecs],
+    plt_add_apps: [:mix]
+  ]
+
+  # ------------------------------------------------------------
+
   def project do
+    in_production = Mix.env() == :prod
+
     [
-      app: :msbms_syst_rate_limiter,
-      version: "0.1.0",
+      app: @name,
+      version: @version,
       elixir: "~> 1.13",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: @deps,
+      build_embedded: in_production,
+      start_permanent: in_production,
+      dialyzer: @dialyzer_opts
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      extra_applications: [
+        :logger
+      ]
     ]
   end
 end
