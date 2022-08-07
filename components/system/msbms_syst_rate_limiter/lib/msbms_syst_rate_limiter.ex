1b56ebee7a66b04056e6ed91dfeb2b43d6c856d6
Steven C. Buttgereit <github.com@musesystems.com>
2022-08-07 07:31:45 -0700

    Skeleton MsbmsSystRateLimiter Elixir project

---
+++ components/system/msbms_syst_rate_limiter/lib/msbms_syst_rate_limiter.ex
@@ -0,0 +1,18 @@
+defmodule MsbmsSystRateLimiter do
+  @moduledoc """
+  Documentation for `MsbmsSystRateLimiter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MsbmsSystRateLimiter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
