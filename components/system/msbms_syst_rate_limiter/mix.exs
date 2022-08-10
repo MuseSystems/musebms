7692addb575b6edb4cebe1d496a62f66a29d1e43
Steven C. Buttgereit <github.com@musesystems.com>
2022-08-10 04:57:35 -0700

    Refactor MsbmsSystRateLimiter start & config
    Initially we were following the Hammer instructions pretty faithfully,
    but if we want to wrap that library and make it completely opaque
    outside of the MsbmsSystRateLimiter module, then not depending on this
    config makes sense.
    
      - Make it so the runtime doesn't start the Hammer application.  We'll
        do this (though there is argument for the consumer of the
        MsbmsSystRateLimiter app to do this via start_link calls.)
    
      - Remove the Hammer configuration from config.exs.  We'll set Hammer's
        configuration explicitly when we start our application.
    
      - Add an Application runtime module to handle starting up Hammer with
        its expected configuration.

--- components/system/msbms_syst_rate_limiter/mix.exs
+++ components/system/msbms_syst_rate_limiter/mix.exs
@@ -9,8 +9,8 @@ defmodule MsbmsSystRateLimiter.MixProject do
     {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
     {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
     {:ex_doc, "~> 0.20", only: :dev, runtime: false},
-    {:hammer_backend_mnesia, "~> 0.5"},
-    {:hammer, "~> 6.0"},
+    {:hammer_backend_mnesia, "~> 0.5", runtime: false},
+    {:hammer, "~> 6.0", runtime: false},
 
     # Muse Systems Business Management System Components
     {:msbms_syst_error, path: "../msbms_syst_error"},
@@ -41,8 +41,10 @@ defmodule MsbmsSystRateLimiter.MixProject do
   def application do
     [
       extra_applications: [
-        :logger
-      ]
+        :logger,
+        :mnesia
+      ],
+      mod: {MsbmsSystRateLimiter.Runtime.Application, []}
     ]
   end
 end
