7e576c3e6d2eb7e3f72f58a0a114c235b143a2c4
Steven C. Buttgereit <github.com@musesystems.com>
2022-08-15 17:04:10 -0700

    Add MsbmsSystAuthentication instance network rules

---
+++ database/cmp_msbms_syst_authentication/msbms_syst_data/syst_instance_network_rules/trig_a_iu_syst_instance_network_rule_ordering.eex.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE FUNCTION msbms_syst_data.trig_a_iu_syst_instance_network_rule_ordering()
+RETURNS trigger AS
+$BODY$
+
+-- File:        trig_a_iu_syst_instance_network_rule_ordering.eex.sql
+-- Location:    musebms/database/cmp_msbms_syst_authentication/msbms_syst_data/syst_instance_network_rules/trig_a_iu_syst_instance_network_rule_ordering.eex.sql
+-- Project:     Muse Systems Business Management System
+--
+-- Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+-- This file may include content copyrighted and licensed from third parties.
+--
+-- See the LICENSE file in the project root for license terms and conditions.
+-- See the NOTICE file in the project root for copyright ownership information.
+--
+-- muse.information@musesystems.com :: https://muse.systems
+
+BEGIN
+
+    UPDATE msbms_syst_data.syst_instance_network_rules
+    SET ordering = ordering + 1
+    WHERE instance_id = new.instance_id AND ordering = new.ordering AND id != new.id;
+
+    RETURN null;
+
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE;
+
+ALTER FUNCTION msbms_syst_data.trig_a_iu_syst_instance_network_rule_ordering()
+    OWNER TO <%= msbms_owner %>;
+
+REVOKE EXECUTE ON FUNCTION msbms_syst_data.trig_a_iu_syst_instance_network_rule_ordering() FROM public;
+GRANT EXECUTE ON FUNCTION msbms_syst_data.trig_a_iu_syst_instance_network_rule_ordering() TO <%= msbms_owner %>;
+
+COMMENT ON FUNCTION msbms_syst_data.trig_a_iu_syst_instance_network_rule_ordering() IS
+$DOC$Ensures that the ordering of network rules is maintained and that ordering
+values are not duplicated.$DOC$;
