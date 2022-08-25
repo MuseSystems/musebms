6961aed66e3a679c9c6b6e7a736ba4b46abbfaeb
Steven C. Buttgereit <github.com@musesystems.com>
2022-08-25 14:20:36 -0700

    Add new tool project MsbmsBuildDocs
    This is an escript project to help create our documentation.

---
+++ tools/build_tools/msbms_build_docs/lib/msbms_build_docs.ex
@@ -0,0 +1,18 @@
+defmodule MsbmsBuildDocs do
+  @moduledoc """
+  Documentation for `MsbmsBuildDocs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MsbmsBuildDocs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ tools/build_tools/msbms_build_docs/test/msbms_build_docs_test.exs
@@ -0,0 +1,8 @@
+defmodule MsbmsBuildDocsTest do
+  use ExUnit.Case
+  doctest MsbmsBuildDocs
+
+  test "greets the world" do
+    assert MsbmsBuildDocs.hello() == :world
+  end
+end
