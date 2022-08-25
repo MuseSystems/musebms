86fd346cf25a8c8c27751be2f55ad7e8310dd933
Steven C. Buttgereit <github.com@musesystems.com>
2022-08-25 05:24:04 -0700

    Add AccessAccount Impl, API, tests, and support
    This fills out the basic initial functionality of SystAccessAccount
    management.  There's probably more to be done, but we'll not really have
    a good handle on that until we start using the component.

--- components/system/msbms_syst_authentication/lib/msbms_syst_authentication.ex
+++ components/system/msbms_syst_authentication/lib/msbms_syst_authentication.ex
@@ -13,6 +13,35 @@ defmodule MsbmsSystAuthentication do
 
   @doc section: :access_account_data
   @doc """
+  Create a new Access Account.
+
+  ## Parameters
+
+    * `access_account_params` - a map of required and optional parameters which
+    define the new Access Account record.  Required attributes in this map are:
+    `internal_name`, `external_name`, `access_account_state_id`, and
+    `allow_global_logins`.
+
+  ## Example
+
+      iex> state = MsbmsSystEnums.get_default_enum_item("access_account_states")
+      iex> {:ok, %MsbmsSystAuthentication.Data.SystAccessAccounts{}} =
+      ...>   MsbmsSystAuthentication.create_access_account(
+      ...>     %{
+      ...>       internal_name: "example_create_accnt",
+      ...>       external_name: "Create Access Account Example",
+      ...>       access_account_state_id: state.id,
+      ...>       allow_global_logins: true
+      ...>      }
+      ...>    )
+  """
+
+  @spec create_access_account(Types.access_account_params()) ::
+          {:ok, Data.SystAccessAccounts.t()} | {:error, MsbmsSystError.t()}
+  defdelegate create_access_account(access_account_params), to: Impl.AccessAccount
+
+  @doc section: :access_account_data
+  @doc """
   Looks up an Access Account record ID by its internal name.
 
   ## Parameters
@@ -31,4 +60,87 @@ defmodule MsbmsSystAuthentication do
   @spec get_access_account_id_by_name(Types.access_account_name()) ::
           {:ok, Types.access_account_id()} | {:error, MsbmsSystError.t()}
   defdelegate get_access_account_id_by_name(access_account_name), to: Impl.AccessAccount
+
+  @doc section: :access_account_data
+  @doc """
+  Retrieves a fully populated Access Account record as found by internal name.
+
+  'Fully populated' in this context means that the related Account Account State
+  and related functional type data is also retrieved along with the actual
+  Access Account data.
+
+  ## Parameters
+
+    * `access_account_name` - the internal name of the Access Account record to
+    retrieve.
+
+  ## Example
+
+      iex> {
+      ...>   :ok,
+      ...>   %MsbmsSystAuthentication.Data.SystAccessAccounts{internal_name: "example_accnt"}
+      ...> } =
+      ...>   MsbmsSystAuthentication.get_access_account_by_name("example_accnt")
+  """
+
+  @spec get_access_account_by_name(Types.access_account_name()) ::
+          Data.SystAccessAccounts.t() | {:error, MsbmsSystError.t()}
+  defdelegate get_access_account_by_name(access_account_name), to: Impl.AccessAccount
+
+  @doc section: :access_account_data
+  @doc """
+  Updates the maintainable fields of a given Access Account record.
+
+  ## Parameters
+
+    * `access_account` - either the record ID value of the Access Account to
+    update or is the complete `MsbmsSystAccessAccount.Data.SystAccessAccounts`
+    struct representing the before-update state of the Access Account record.
+
+    * `access_account_params` - a map containing those attributes to be changed
+    along with their new values.
+
+  ## Example
+
+      iex> {:ok, target_access_account} =
+      ...>   MsbmsSystAuthentication.get_access_account_by_name("example_accnt")
+      iex> {:ok, updated_access_account} =
+      ...>   MsbmsSystAuthentication.update_access_account(
+      ...>     target_access_account,
+      ...>     %{external_name: "Updated Example Account Name"}
+      ...>   )
+      iex> %MsbmsSystAuthentication.Data.SystAccessAccounts{
+      ...>   external_name: "Updated Example Account Name"
+      ...> } = updated_access_account
+  """
+
+  @spec update_access_account(
+          Types.access_account_id() | Data.SystAccessAccounts.t(),
+          Types.access_account_params()
+        ) ::
+          {:ok, Data.SystAccessAccounts.t()} | {:error, MsbmsSystError.t()}
+  defdelegate update_access_account(access_account, access_account_params), to: Impl.AccessAccount
+
+  @doc section: :access_account_data
+  @doc """
+  Purges the requested Access Account if the Access Account State is of
+  a purge eligible functional type.
+
+  ## Parameters
+
+    * `access_account` - is either the record ID of the Access Account to purge
+    or the populated `MsbmsSystAccessAccount.Data.SystAccessAccounts` struct
+    representing the record to purge.
+
+  ## Example
+
+      iex> {:ok, target_access_account} =
+      ...>   MsbmsSystAuthentication.get_access_account_by_name("example_purge_accnt")
+      iex> MsbmsSystAuthentication.purge_access_account(target_access_account)
+      :ok
+  """
+
+  @spec purge_access_account(Types.access_account_id() | Data.SystAccessAccounts.t()) ::
+          :ok | {:error, MsbmsSystError.t()}
+  defdelegate purge_access_account(access_account), to: Impl.AccessAccount
 end
