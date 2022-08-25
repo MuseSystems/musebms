136be2ceaaad701ce1386b0139499e7b4f5d0728
Steven C. Buttgereit <github.com@musesystems.com>
2022-08-25 05:24:04 -0700

    Add AccessAccount Impl, API, tests, and support
    This fills out the basic initial functionality of SystAccessAccount
    management.  There's probably more to be done, but we'll not really have
    a good handle on that until we start using the component.

--- components/system/msbms_syst_authentication/lib/impl/access_account.ex
+++ components/system/msbms_syst_authentication/lib/impl/access_account.ex
@@ -12,7 +12,6 @@
 
 defmodule MsbmsSystAuthentication.Impl.AccessAccount do
   import Ecto.Query
-  import MsbmsSystUtils
 
   alias MsbmsSystAuthentication.Data
   alias MsbmsSystAuthentication.Types
@@ -20,6 +19,69 @@ defmodule MsbmsSystAuthentication.Impl.AccessAccount do
   require Logger
 
   @moduledoc false
+
+  @spec create_access_account(Types.access_account_params()) ::
+          {:ok, Data.SystAccessAccounts.t()} | {:error, MsbmsSystError.t()}
+  def create_access_account(access_account_params) do
+    access_account_params
+    |> Data.SystAccessAccounts.insert_changeset()
+    |> MsbmsSystDatastore.insert!(returning: true)
+    |> then(&{:ok, &1})
+  rescue
+    error ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+
+      {
+        :error,
+        %MsbmsSystError{
+          code: :undefined_error,
+          message: "Failure creating Access Account.",
+          cause: error
+        }
+      }
+  end
+
+  @spec update_access_account(
+          Types.access_account_id() | Data.SystAccessAccounts.t(),
+          Types.access_account_params()
+        ) ::
+          {:ok, Data.SystAccessAccounts.t()} | {:error, MsbmsSystError.t()}
+  def update_access_account(access_account_id, access_account_params)
+      when is_binary(access_account_id) do
+    MsbmsSystDatastore.get!(Data.SystAccessAccounts, access_account_id)
+    |> update_access_account(access_account_params)
+  rescue
+    error ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+
+      {
+        :error,
+        %MsbmsSystError{
+          code: :undefined_error,
+          message: "Failure updating Access Account by ID.",
+          cause: error
+        }
+      }
+  end
+
+  def update_access_account(%Data.SystAccessAccounts{} = access_account, access_account_params) do
+    access_account
+    |> Data.SystAccessAccounts.update_changeset(access_account_params)
+    |> MsbmsSystDatastore.update!(returning: true)
+    |> then(&{:ok, &1})
+  rescue
+    error ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+
+      {
+        :error,
+        %MsbmsSystError{
+          code: :undefined_error,
+          message: "Failure updating Access Account.",
+          cause: error
+        }
+      }
+  end
 
   @spec get_access_account_id_by_name(Types.access_account_name()) ::
           {:ok, Types.access_account_id()} | {:error, MsbmsSystError.t()}
@@ -43,4 +105,81 @@ defmodule MsbmsSystAuthentication.Impl.AccessAccount do
         }
       }
   end
+
+  @spec get_access_account_by_name(Types.access_account_name()) ::
+          Data.SystAccessAccounts.t() | {:error, MsbmsSystError.t()}
+  def get_access_account_by_name(access_account_name) do
+    from(
+      aa in Data.SystAccessAccounts,
+      join: aas in assoc(aa, :access_account_state),
+      join: aasft in assoc(aas, :functional_type),
+      where: aa.internal_name == ^access_account_name,
+      preload: [access_account_state: {aas, functional_type: aasft}]
+    )
+    |> MsbmsSystDatastore.one!()
+    |> then(&{:ok, &1})
+  rescue
+    error ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+
+      {
+        :error,
+        %MsbmsSystError{
+          code: :undefined_error,
+          message: "Failure retrieving Access Account data.",
+          cause: error
+        }
+      }
+  end
+
+  @spec purge_access_account(Types.access_account_id() | Data.SystAccessAccounts.t()) ::
+          :ok | {:error, MsbmsSystError.t()}
+  def purge_access_account(access_account_id) when is_binary(access_account_id) do
+    from(
+      aa in Data.SystAccessAccounts,
+      join: aas in assoc(aa, :access_account_state),
+      join: aasft in assoc(aas, :functional_type),
+      where: aa.id == ^access_account_id,
+      preload: [access_account_state: {aas, functional_type: aasft}]
+    )
+    |> MsbmsSystDatastore.one!()
+    |> purge_access_account()
+  end
+
+  def purge_access_account(
+        %Data.SystAccessAccounts{
+          access_account_state: %MsbmsSystEnums.Data.SystEnumItems{
+            functional_type: %MsbmsSystEnums.Data.SystEnumFunctionalTypes{
+              internal_name: functional_type
+            }
+          }
+        } = access_account
+      ) do
+    case functional_type do
+      "access_account_states_purge_eligible" ->
+        MsbmsSystDatastore.delete!(access_account)
+        :ok
+
+      _ ->
+        raise MsbmsSystError,
+          code: :invalid_parameter,
+          message: "Invalid Access Account State Functional Type for purge.",
+          cause: %{parameters: [functional_type: functional_type]}
+    end
+  rescue
+    error ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+
+      {
+        :error,
+        %MsbmsSystError{
+          code: :undefined_error,
+          message: "Failure purging Access Account.",
+          cause: error
+        }
+      }
+  end
+
+  def purge_access_account(%Data.SystAccessAccounts{id: access_account_id}),
+    do: purge_access_account(access_account_id)
 end
--- database/cmp_msbms_syst_authentication/testing_support/test_data.eex.sql
+++ database/cmp_msbms_syst_authentication/testing_support/test_data.eex.sql
@@ -55,7 +55,282 @@ $AUTHENTICATION_TESTING_INIT$
             ( '10.123.123.5'::inet );
 
         ------------------------------------------------------------------------
-        -- All Access / Example User Test Data
+        -- Password Rules
+        ------------------------------------------------------------------------
+
+        INSERT INTO msbms_syst_data.syst_owner_password_rules
+            ( owner_id
+            , password_length
+            , max_age
+            , require_upper_case
+            , require_lower_case
+            , require_numbers
+            , require_symbols
+            , disallow_recently_used
+            , disallow_known_compromised
+            , require_mfa
+            , allowed_mfa_types )
+        VALUES
+            ( (SELECT id FROM msbms_syst_data.syst_owners WHERE internal_name = 'owner1')
+            , int4range( 6, 1024, '[]' )
+            , interval '0 days'
+            , 0
+            , 0
+            , 0
+            , 0
+            , 0
+            , FALSE
+            , FALSE
+            , ARRAY['credential_types_secondary_totp']::text[] );
+
+        INSERT INTO msbms_syst_data.syst_owner_password_rules
+            ( owner_id
+            , password_length
+            , max_age
+            , require_upper_case
+            , require_lower_case
+            , require_numbers
+            , require_symbols
+            , disallow_recently_used
+            , disallow_known_compromised
+            , require_mfa
+            , allowed_mfa_types )
+        VALUES
+            ( (SELECT id FROM msbms_syst_data.syst_owners WHERE internal_name = 'owner2')
+            , int4range( 12, 64, '[]' )
+            , interval '10 days'
+            , 2
+            , 2
+            , 2
+            , 2
+            , 2
+            , FALSE
+            , TRUE
+            , ARRAY['credential_types_secondary_totp']::text[] );
+
+        INSERT INTO msbms_syst_data.syst_owner_password_rules
+            ( owner_id
+            , password_length
+            , max_age
+            , require_upper_case
+            , require_lower_case
+            , require_numbers
+            , require_symbols
+            , disallow_recently_used
+            , disallow_known_compromised
+            , require_mfa
+            , allowed_mfa_types )
+        VALUES
+            ( (SELECT id FROM msbms_syst_data.syst_owners WHERE internal_name = 'owner3')
+            , int4range( 8, 512, '[]' )
+            , interval '0 days'
+            , 0
+            , 0
+            , 0
+            , 0
+            , 0
+            , TRUE
+            , FALSE
+            , ARRAY['credential_types_secondary_totp']::text[] );
+
+        INSERT INTO msbms_syst_data.syst_owner_password_rules
+            ( owner_id
+            , password_length
+            , max_age
+            , require_upper_case
+            , require_lower_case
+            , require_numbers
+            , require_symbols
+            , disallow_recently_used
+            , disallow_known_compromised
+            , require_mfa
+            , allowed_mfa_types )
+        VALUES
+            ( (SELECT id FROM msbms_syst_data.syst_owners WHERE internal_name = 'owner4')
+            , int4range( 8, 512, '[]' )
+            , interval '0 days'
+            , 0
+            , 0
+            , 0
+            , 0
+            , 0
+            , TRUE
+            , FALSE
+            , ARRAY['credential_types_secondary_totp']::text[] );
+
+        ------------------------------------------------------------------------
+        -- Network Rule Creation
+        ------------------------------------------------------------------------
+
+        INSERT INTO msbms_syst_data.syst_global_network_rules
+        (
+          template_rule
+        , ordering
+        , functional_type
+        , ip_host_or_network
+        , ip_host_range_lower
+        , ip_host_range_upper
+        )
+        VALUES
+        (
+          TRUE,
+          1,
+          'deny',
+          '10.124.124.1',
+          NULL,
+          NULL
+        ),
+        (
+          TRUE,
+          2,
+          'allow',
+          '10.124.124.0/24',
+          NULL,
+          NULL
+        ),
+        (
+          TRUE,
+          3,
+          'allow',
+          NULL,
+          '10.123.123.1',
+          '10.123.123.254'
+        ),
+        (
+          FALSE,
+          1,
+          'deny',
+          '10.124.124.2',
+          NULL,
+          NULL
+        ),
+        (
+          FALSE,
+          2,
+          'allow',
+          '10.125.125.0/24',
+          NULL,
+          NULL
+        ),
+        (
+          FALSE,
+          3,
+          'allow',
+          NULL,
+          '10.123.123.5',
+          '10.123.123.254'
+        );
+
+        INSERT INTO msbms_syst_data.syst_owner_network_rules
+        (
+          owner_id
+        , ordering
+        , functional_type
+        , ip_host_or_network
+        , ip_host_range_lower
+        , ip_host_range_upper
+        )
+        VALUES
+        (
+          (SELECT id FROM msbms_syst_data.syst_owners WHERE internal_name = 'owner1'),
+          1,
+          'deny',
+          '10.124.124.1',
+          NULL,
+          NULL
+        ),
+        (
+          (SELECT id FROM msbms_syst_data.syst_owners WHERE internal_name = 'owner1'),
+          2,
+          'allow',
+          '10.124.124.0/24',
+          NULL,
+          NULL
+        ),
+        (
+          (SELECT id FROM msbms_syst_data.syst_owners WHERE internal_name = 'owner1'),
+          3,
+          'allow',
+          NULL,
+          '10.123.123.1',
+          '10.123.123.254'
+        );
+
+        INSERT INTO msbms_syst_data.syst_instance_network_rules
+        (
+          instance_id
+        , ordering
+        , functional_type
+        , ip_host_or_network
+        , ip_host_range_lower
+        , ip_host_range_upper
+        )
+        SELECT
+            i.id
+            ,1
+            ,'allow'
+            ,'10.126.126.1'
+            ,NULL
+            ,NULL
+        FROM msbms_syst_data.syst_instances i;
+
+        INSERT INTO msbms_syst_data.syst_instance_network_rules
+        (
+          instance_id
+        , ordering
+        , functional_type
+        , ip_host_or_network
+        , ip_host_range_lower
+        , ip_host_range_upper
+        )
+        SELECT
+            i.id
+            ,2
+            ,'deny'
+            ,'10.126.126.2'
+            ,NULL
+            ,NULL
+        FROM msbms_syst_data.syst_instances i;
+
+        INSERT INTO msbms_syst_data.syst_instance_network_rules
+        (
+          instance_id
+        , ordering
+        , functional_type
+        , ip_host_or_network
+        , ip_host_range_lower
+        , ip_host_range_upper
+        )
+        SELECT
+            i.id
+            ,3
+            ,'allow'
+            ,'10.126.126.0/24'
+            ,NULL
+            ,NULL
+        FROM msbms_syst_data.syst_instances i;
+
+        INSERT INTO msbms_syst_data.syst_instance_network_rules
+        (
+          instance_id
+        , ordering
+        , functional_type
+        , ip_host_or_network
+        , ip_host_range_lower
+        , ip_host_range_upper
+        )
+        SELECT
+            i.id
+            ,4
+            ,'deny'
+            ,NULL
+            ,'10.127.127.1'
+            ,'10.128.128.254'
+        FROM msbms_syst_data.syst_instances i;
+
+
+        ------------------------------------------------------------------------
+        -- Access Account Creation
         ------------------------------------------------------------------------
 
         INSERT INTO msbms_syst_data.syst_access_accounts
@@ -84,6 +359,53 @@ $AUTHENTICATION_TESTING_INIT$
                     id
                 FROM msbms_syst_data.syst_enum_items
                 WHERE internal_name = 'access_account_states_sysdef_active' ) )
+             ,
+            ( 'update_test_accnt'
+            , 'Access Account Update Test Account'
+            , ( SELECT id
+                FROM msbms_syst_data.syst_owners
+                WHERE internal_name = 'owner1' )
+            , FALSE
+            , ( SELECT
+                    id
+                FROM msbms_syst_data.syst_enum_items
+                WHERE internal_name = 'access_account_states_sysdef_active' ) )
+
+             ,
+            ( 'bad_update_test_accnt'
+            , 'Bad Access Account Update Test Account'
+            , ( SELECT id
+                FROM msbms_syst_data.syst_owners
+                WHERE internal_name = 'owner1' )
+            , FALSE
+            , ( SELECT
+                    id
+                FROM msbms_syst_data.syst_enum_items
+                WHERE internal_name = 'access_account_states_sysdef_active' ) )
+
+
+             ,
+            ( 'purge_test_accnt'
+            , 'Purge Access Account Test Account'
+            , ( SELECT id
+                FROM msbms_syst_data.syst_owners
+                WHERE internal_name = 'owner1' )
+            , FALSE
+            , ( SELECT
+                    id
+                FROM msbms_syst_data.syst_enum_items
+                WHERE internal_name = 'access_account_states_sysdef_purge_eligible' ) )
+             ,
+            ( 'example_purge_accnt'
+            , 'Example Purge Account'
+            , ( SELECT id
+                FROM msbms_syst_data.syst_owners
+                WHERE internal_name = 'owner1' )
+            , FALSE
+            , ( SELECT
+                    id
+                FROM msbms_syst_data.syst_enum_items
+                WHERE internal_name = 'access_account_states_sysdef_purge_eligible' ) )
              ,
             ( 'example_accnt'
             , 'Example Account'
@@ -146,6 +468,10 @@ $AUTHENTICATION_TESTING_INIT$
               cte.internal_name = 'credential_types'
           AND aa.internal_name IN ( 'unowned_all_access', 'example_accnt' );
 
+        ------------------------------------------------------------------------
+        -- Identity Creation
+        ------------------------------------------------------------------------
+
         INSERT INTO msbms_syst_data.syst_identities
             ( access_account_id
             , identity_type_id
@@ -186,6 +512,149 @@ $AUTHENTICATION_TESTING_INIT$
           , TRUE
         FROM msbms_syst_data.syst_access_accounts aa;
 
+        INSERT INTO msbms_syst_data.syst_identities
+            ( access_account_id
+            , identity_type_id
+            , account_identifier
+            , validated
+            , validation_requested
+            , validation_expires
+            , primary_contact )
+        SELECT
+            aa.id
+          , ( SELECT id
+              FROM msbms_syst_data.syst_enum_items
+              WHERE internal_name = 'identity_types_sysdef_api' )
+          , msbms_syst_priv.get_random_string(16)
+          , now( ) - INTERVAL '15 days'
+          , now( ) - INTERVAL '20 days'
+          , now( ) - INTERVAL '10 days'
+          , TRUE
+        FROM msbms_syst_data.syst_access_accounts aa;
+
+        ------------------------------------------------------------------------
+        -- Credential Creation
+        ------------------------------------------------------------------------
+
+        INSERT INTO msbms_syst_data.syst_credentials
+            ( access_account_id, credential_type_id, credential_data, last_updated )
+        VALUES
+            ( ( SELECT id
+                FROM msbms_syst_data.syst_access_accounts
+                WHERE internal_name = 'unowned_all_access' )
+            , ( SELECT
+                    id
+                FROM msbms_syst_data.syst_enum_items
+                WHERE internal_name = 'credential_types_sysdef_password' )
+            -- unowned.all.access.test.password
+            , '$argon2id$v=19$m=65536,t=8,p=2$WuXR7/MzTrdNzsYUaX/ssg$DZaMwZLHDX5z38tNck4vFIuGeZHOwfXxS87BonqETFc'
+            , now( ) - INTERVAL '1 day' );
+
+        INSERT INTO msbms_syst_data.syst_credentials
+            ( access_account_id, credential_type_id, credential_data, last_updated )
+        VALUES
+            ( ( SELECT id
+                FROM msbms_syst_data.syst_access_accounts
+                WHERE internal_name = 'owned_all_access' )
+            , ( SELECT
+                    id
+                FROM msbms_syst_data.syst_enum_items
+                WHERE internal_name = 'credential_types_sysdef_password' )
+            -- owned.all.access.test.password
+            , '$argon2id$v=19$m=65536,t=8,p=2$E3Fo2aZ0ujVew+zFzVmQLg$HmOFV+om6gLAIp6rM8MFtqYpv8vPb8xWeY3RIjynxVw'
+            , now( ) - INTERVAL '1 day' );
+
+        INSERT INTO msbms_syst_data.syst_credentials
+            ( access_account_id, credential_type_id, credential_data, last_updated )
+        VALUES
+            ( ( SELECT id
+                FROM msbms_syst_data.syst_access_accounts
+                WHERE internal_name = 'example_accnt' )
+            , ( SELECT
+                    id
+                FROM msbms_syst_data.syst_enum_items
+                WHERE internal_name = 'credential_types_sysdef_password' )
+            -- example.test.password
+            , '$argon2id$v=19$m=65536,t=8,p=2$0n4sdMLjE7IeQNmR1YfFgg$BzujLUJVkKVvAH6LA8yKuqtGRd00qZN5iQTgskFHhxY'
+            , now( ) - INTERVAL '1 day' );
+
+        INSERT INTO msbms_syst_data.syst_credentials
+            ( access_account_id
+            , credential_type_id
+            , credential_for_identity_id
+            , credential_data
+            , last_updated )
+        VALUES
+            ( ( SELECT id
+                FROM msbms_syst_data.syst_access_accounts
+                WHERE internal_name = 'unowned_all_access' )
+            , ( SELECT
+                    id
+                FROM msbms_syst_data.syst_enum_items
+                WHERE internal_name = 'credential_types_sysdef_token_api' )
+            , ( SELECT
+                    i.id
+                FROM msbms_syst_data.syst_identities i
+                    JOIN msbms_syst_data.syst_access_accounts aa ON aa.id = i.access_account_id
+                    JOIN msbms_syst_data.syst_enum_items ei ON ei.id = i.identity_type_id
+                WHERE
+                      aa.internal_name = 'unowned_all_access'
+                  AND ei.internal_name = 'identity_types_sysdef_api' )
+              -- TY7N2G9TADG6ADA80FFI4F24PTNYKAQGRG0LYK4FHNJ67NPS
+            , '$argon2id$v=19$m=65536,t=8,p=2$etwRUSNGUlv7SK7d3WCg5A$E1t6u7fI599HdoRJB8zXeEqoF/UdhL+lG7UfyAw3O28'
+            , now( ) - INTERVAL '1 day' );
+
+        INSERT INTO msbms_syst_data.syst_credentials
+            ( access_account_id
+            , credential_type_id
+            , credential_for_identity_id
+            , credential_data
+            , last_updated )
+        VALUES
+            ( ( SELECT id
+                FROM msbms_syst_data.syst_access_accounts
+                WHERE internal_name = 'owned_all_access' )
+            , ( SELECT
+                    id
+                FROM msbms_syst_data.syst_enum_items
+                WHERE internal_name = 'credential_types_sysdef_token_api' )
+            , ( SELECT
+                    i.id
+                FROM msbms_syst_data.syst_identities i
+                    JOIN msbms_syst_data.syst_access_accounts aa ON aa.id = i.access_account_id
+                    JOIN msbms_syst_data.syst_enum_items ei ON ei.id = i.identity_type_id
+                WHERE
+                      aa.internal_name = 'owned_all_access'
+                  AND ei.internal_name = 'identity_types_sysdef_api' )
+              -- QNXWXLSYLB8O3PHMSLOEU9Y1WZF4PIIPUQREXSRRYLVBMPU2
+            , '$argon2id$v=19$m=65536,t=8,p=2$HVjIPnRUzdzwubYxKJ6ZZg$vy4aDFg1hUBpMPm5S4/Bfs3Q7VUM725w9zIhcwUeXvQ'
+            , now( ) - INTERVAL '1 day' );
+
+        INSERT INTO msbms_syst_data.syst_credentials
+            ( access_account_id
+            , credential_type_id
+            , credential_for_identity_id
+            , credential_data
+            , last_updated )
+        VALUES
+            ( ( SELECT id
+                FROM msbms_syst_data.syst_access_accounts
+                WHERE internal_name = 'example_accnt' )
+            , ( SELECT
+                    id
+                FROM msbms_syst_data.syst_enum_items
+                WHERE internal_name = 'credential_types_sysdef_token_api' )
+            , ( SELECT
+                    i.id
+                FROM msbms_syst_data.syst_identities i
+                    JOIN msbms_syst_data.syst_access_accounts aa ON aa.id = i.access_account_id
+                    JOIN msbms_syst_data.syst_enum_items ei ON ei.id = i.identity_type_id
+                WHERE
+                      aa.internal_name = 'example_accnt'
+                  AND ei.internal_name = 'identity_types_sysdef_api' )
+              -- J7RJY39PCI2CJ2P6BWSRA98XFG5SAZ0PIMTELMGLCC3X5PYD
+            , '$argon2id$v=19$m=65536,t=8,p=2$IMiQN/YlkA+g20mMgsKV1w$/WP/6UFwnxo46oR4nN6/5MqncIu4JAsnRyCnkoiLS/I'
+            , now( ) - INTERVAL '1 day' );
 
 
     END;
