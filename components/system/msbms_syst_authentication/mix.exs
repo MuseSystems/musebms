58b7820a680580f076e28b4c9cee51e4bee89e81
Steven C. Buttgereit <github.com@musesystems.com>
2022-09-04 07:56:13 -0700

    Add MsbmsSystAuthentication DevSupport
    For interactive development, it's beneficial to use the testing related
    database setup to build/start the database and services so that
    iex -S mix can be used.
    
    Create a near copy of the TestingSupport and TestHelper functions which
    create the database based on testing seed data and start the services,
    including the Enum services, so that full interactive development is
    a possibility.
    
    This is a "good enough" implementation of the ability.  The next steps
    would be to create a module which can just be included into dev builds
    as a dependency, but which can use a configuration file to for the
    one or two items which are specific to the component being developed.
    Finally, de-dupe the code so that the testing support used the same
    functionality to bootstrap the test run environments.  All later...

--- components/system/msbms_syst_authentication/mix.exs
+++ components/system/msbms_syst_authentication/mix.exs
@@ -96,6 +96,7 @@ defmodule MsbmsSystAuthentication.MixProject do
   end
 
   defp elixirc_paths(:test), do: elixirc_paths() ++ ["test/support"]
+  defp elixirc_paths(:dev), do: elixirc_paths() ++ ["dev_support"]
   defp elixirc_paths(_), do: elixirc_paths()
   defp elixirc_paths(), do: ["lib"]
 end
