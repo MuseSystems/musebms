376446f61d06ca394d2b4e3450a9d5a42b403e3b
Steven C. Buttgereit <github.com@musesystems.com>
2022-09-04 18:35:18 -0700

    Add standard file header to component mix.exs
    Across all Elixir projects.

--- components/system/msbms_syst_options/mix.exs
+++ components/system/msbms_syst_options/mix.exs
@@ -1,3 +1,15 @@
+# Source File: mix.exs
+# Location:    musebms/components/system/msbms_syst_options/mix.exs
+# Project:     Muse Systems Business Management System
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https://muse.systems
+
 defmodule MsbmsSystOptions.MixProject do
   use Mix.Project
 
