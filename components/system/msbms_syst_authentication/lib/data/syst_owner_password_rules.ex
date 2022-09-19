71859bba8199aaeb9f0f7c63b9e8743401cc5370
Steven C. Buttgereit <github.com@musesystems.com>
2022-09-19 04:23:18 -0700

    Refactor password rule validators
    The SystGlobalPasswordRules & SystOwnerPasswordRules validators existed
    prior to this commit, but were not complete or exposed through the API
    module.
    
      * Expose the validators to API modules
    
      * Clean up the validator modules including removing unnecessary calls
        and misc. bug fixes
    
      * Add absolute constraints on the password length values

--- components/system/msbms_syst_authentication/lib/data/syst_owner_password_rules.ex
+++ components/system/msbms_syst_authentication/lib/data/syst_owner_password_rules.ex
@@ -13,6 +13,8 @@
 defmodule MsbmsSystAuthentication.Data.SystOwnerPasswordRules do
   use MsbmsSystDatastore.Schema
 
+  alias MsbmsSystAuthentication.Data.Validators
+
   alias MsbmsSystDatastore.DbTypes
 
   @moduledoc """
@@ -72,4 +74,13 @@ defmodule MsbmsSystAuthentication.Data.SystOwnerPasswordRules do
 
     belongs_to(:owner, MsbmsSystInstanceMgr.Data.SystOwners)
   end
+
+  @spec insert_changeset(Types.password_rule_params()) ::
+          Ecto.Changeset.t()
+  defdelegate insert_changeset(insert_params), to: Validators.SystOwnerPasswordRules
+
+  @spec update_changeset(Data.SystOwnerPasswordRules.t(), Types.password_rule_params()) ::
+          Ecto.Changeset.t()
+  defdelegate update_changeset(owner_password_rule, update_params),
+    to: Validators.SystOwnerPasswordRules
 end
--- components/system/msbms_syst_authentication/lib/data/validators/syst_global_password_rules.ex
+++ components/system/msbms_syst_authentication/lib/data/validators/syst_global_password_rules.ex
@@ -16,7 +16,19 @@ defmodule MsbmsSystAuthentication.Data.Validators.SystGlobalPasswordRules do
   alias MsbmsSystAuthentication.Data
   alias MsbmsSystAuthentication.Types
 
+  alias MsbmsSystDatastore.DbTypes
+
   @moduledoc false
+
+  # The min and min/max password length constraints are derived from
+  # NIST SP800-63B.  The absolute max constraint is an arbitrarily chosen value.
+  #
+  # TODO: Consider making these compile time configs since they are defined in
+  #       multiple modules.
+
+  @min_password_length 8
+  @min_max_password_length 64
+  @max_password_length 1024
 
   @spec update_changeset(Data.SystGlobalPasswordRules.t(), Types.password_rule_params()) ::
           Ecto.Changeset.t()
@@ -46,6 +58,57 @@ defmodule MsbmsSystAuthentication.Data.Validators.SystGlobalPasswordRules do
       :require_mfa,
       :allowed_mfa_types
     ])
+    |> validate_password_length_min()
+    |> validate_password_length_mix_max()
+    |> validate_password_length_max()
     |> optimistic_lock(:diag_row_version)
+  end
+
+  defp validate_password_length_min(changeset) do
+    password_length = get_field(changeset, :password_length)
+
+    case DbTypes.Range.bounds_compare(@min_password_length, password_length) do
+      %{lower_comparison: :gt} ->
+        add_error(
+          changeset,
+          :password_length,
+          "Password minimum length must be at least #{@min_password_length} characters."
+        )
+
+      _ ->
+        changeset
+    end
+  end
+
+  defp validate_password_length_mix_max(changeset) do
+    password_length = get_field(changeset, :password_length)
+
+    case DbTypes.Range.bounds_compare(@min_max_password_length, password_length) do
+      %{upper_comparison: :gt} ->
+        add_error(
+          changeset,
+          :password_length,
+          "Password min. max. length must be at least #{@min_max_password_length} characters."
+        )
+
+      _ ->
+        changeset
+    end
+  end
+
+  defp validate_password_length_max(changeset) do
+    password_length = get_field(changeset, :password_length)
+
+    case DbTypes.Range.bounds_compare(@max_password_length, password_length) do
+      %{upper_comparison: :lt} ->
+        add_error(
+          changeset,
+          :password_length,
+          "Password max length must be no more than #{@max_password_length} characters."
+        )
+
+      _ ->
+        changeset
+    end
   end
 end
