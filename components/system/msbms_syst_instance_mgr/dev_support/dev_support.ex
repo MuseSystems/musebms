e65c50986581e75a675ad044b67eed643f7d8c4c
Steven C. Buttgereit <github.com@musesystems.com>
2022-09-24 05:34:20 -0700

    Add MsbmsSystInstanceMgr.DevSupport return matches
    Per Dialyzer.  Also remove extraneous set_datastore_context call while
    we're here.  It shouldn't be needed as none of the intervening calls
    after the first set_datastore_context call change the set context.  I
    think this was a cut/paste error since DevSupport code originated from
    testing support code.

--- components/system/msbms_syst_instance_mgr/dev_support/dev_support.ex
+++ components/system/msbms_syst_instance_mgr/dev_support/dev_support.ex
@@ -59,9 +59,9 @@ defmodule DevSupport do
   }
 
   def start_dev_environment(db_kind \\ :unit_testing) do
-    setup_database(db_kind)
+    _ = setup_database(db_kind)
 
-    MsbmsSystDatastore.set_datastore_context(get_datastore_context_id())
+    _ = MsbmsSystDatastore.set_datastore_context(get_datastore_context_id())
 
     enum_service_spec = %{
       id: MsbmsDevEnumService,
@@ -76,13 +76,12 @@ defmodule DevSupport do
       {DynamicSupervisor, strategy: :one_for_one, name: Msbms.DevSupervisor}
     ]
 
-    Supervisor.start_link(children, strategy: :one_for_one)
+    _ = Supervisor.start_link(children, strategy: :one_for_one)
     Logger.configure(level: :info)
 
-    DynamicSupervisor.start_child(Msbms.DevSupervisor, enum_service_spec)
+    _ = DynamicSupervisor.start_child(Msbms.DevSupervisor, enum_service_spec)
 
-    MsbmsSystDatastore.set_datastore_context(get_datastore_context_id())
-    MsbmsSystEnums.put_enums_service(:msbms_dev_enum_service)
+    _ = MsbmsSystEnums.put_enums_service(:msbms_dev_enum_service)
   end
 
   def stop_dev_environment(), do: cleanup_database()
