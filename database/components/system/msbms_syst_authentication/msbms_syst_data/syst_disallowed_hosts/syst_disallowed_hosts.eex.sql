1d77d92d43d9ae485e5c1ea1be842f65539a5093
Steven C. Buttgereit <github.com@musesystems.com>
2022-10-01 05:13:49 -0700

    Reword syst_disallowed_hosts table comment
    The documentation was tweaked slightly in the corresponding Elixir
    module.  Port that change back to the table SQL and respace accordingly.

--- database/components/system/msbms_syst_authentication/msbms_syst_data/syst_disallowed_hosts/syst_disallowed_hosts.eex.sql
+++ database/components/system/msbms_syst_authentication/msbms_syst_data/syst_disallowed_hosts/syst_disallowed_hosts.eex.sql
@@ -52,13 +52,13 @@ CREATE TRIGGER z99_trig_b_iu_set_diagnostic_columns
 
 COMMENT ON
     TABLE msbms_syst_data.syst_disallowed_hosts IS
-$DOC$A listing of IP addresses which are not allowed to attempt authentication.  This
-registry differs from the syst_*_network_rules tables in that IP addresses here
-are registered as the result of automatic system heuristics whereas the network
-rules are direct expressions of system administrator intent.  The timing between
-these two mechanisms is also different in that records in this table are
-evaluated prior to an authentication attempt and most network rules are
-processed in the authentication attempt sequence.$DOC$;
+$DOC$A simple listing of "banned" IP address which are not allowed to authenticate
+their users to the system.  This registry differs from the syst_*_network_rules
+tables in that IP addresses here are registered as the result of automatic
+system heuristics whereas the network rules are direct expressions of system
+administrator intent.  The timing between these two mechanisms is also different
+in that records in this table are evaluated prior to an authentication attempt
+and most network rules are processed in the authentication attempt sequence.$DOC$;
 
 COMMENT ON
     COLUMN msbms_syst_data.syst_disallowed_hosts.id IS
