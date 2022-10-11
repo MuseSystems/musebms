ca749254219cb26689f1c63ae5d4a564a9b790d9
Steven C. Buttgereit <github.com@musesystems.com>
2022-10-11 06:00:58 -0700

    Add SystDisallowedHosts to MsbmsSystAuthentication
    This is the data representation of the msbms_syst.syst_disallowed_hosts
    which contains those hosts which are banned by the system.

---
+++ components/system/msbms_syst_authentication/lib/data/validators/syst_disallowed_hosts.ex
@@ -0,0 +1,27 @@
+# Source File: syst_disallowed_hosts.ex
+# Location:    musebms/components/system/msbms_syst_authentication/lib/data/validators/syst_disallowed_hosts.ex
+# Project:     Muse Systems Business Management System
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https://muse.systems
+
+defmodule MsbmsSystAuthentication.Data.Validators.SystDisallowedHosts do
+  import Ecto.Changeset
+
+  alias MsbmsSystAuthentication.Data
+  alias MsbmsSystDatastore.DbTypes
+
+  @moduledoc false
+
+  @spec insert_changeset(DbTypes.Inet.t()) :: Ecto.Changeset.t()
+  def insert_changeset(host_address) do
+    %Data.SystDisallowedHosts{}
+    |> cast(%{host_address: host_address}, [:host_address])
+    |> validate_required([:host_address])
+  end
+end
