568709d18760bbe4aeef6680c2378687f7f5ad96
Steven C. Buttgereit <github.com@musesystems.com>
2022-10-20 20:10:34 -0700

    Add Impl.DisallowedPasswords and related refactor
      - Add new implementation module for Disallowed Passwords.
    
      - Rename private Credential.Password.check_compromised_pwd/1 to
        check_disallowed_pwd
    
      - Call the new Impl.DisallowedPassword.password_disallowed/1 function
        rather than having looking function in Credential.Password.
    
      - Add tests for new module

---
+++ components/system/msbms_syst_authentication/test/disallowed_password_test.ex
@@ -0,0 +1,44 @@
+# Source File: disallowed_password_test.ex
+# Location:    musebms/components/system/msbms_syst_authentication/test/disallowed_password_test.ex
+# Project:     Muse Systems Business Management System
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https://muse.systems
+
+defmodule DisallowedPasswordTest do
+  use AuthenticationTestCase, async: true
+
+  import Ecto.Query
+
+  alias MsbmsSystAuthentication.Data
+  alias MsbmsSystAuthentication.Impl
+
+  test "Can add Disallowed Password" do
+    assert :ok =
+             Impl.DisallowedPasswords.add_disallowed_password("can_add_disallowed_password_test")
+
+    # The attempt to add a duplicate entry should still return :ok even though
+    # at the database level the insert is ignored (ON CONFLICT/DO NOTHING).
+
+    assert :ok =
+             Impl.DisallowedPasswords.add_disallowed_password("can_add_disallowed_password_test")
+  end
+
+  test "Can test if password is disallowed" do
+    assert {:ok, true} = Impl.DisallowedPasswords.password_disallowed("Disallowed!Test#02")
+    assert {:ok, false} = Impl.DisallowedPasswords.password_disallowed("NotDisallowed!Test#02")
+  end
+
+  test "Can delete Disallowed Password once" do
+    assert {:ok, :deleted} =
+             Impl.DisallowedPasswords.delete_disallowed_password("DeleteDisallowed!Test#02")
+
+    assert {:ok, :no_record} =
+             Impl.DisallowedPasswords.delete_disallowed_password("DeleteDisallowed!Test#02")
+  end
+end
