5954d72e25acc3981d8ed1eedffbef881a84663a
Steven C. Buttgereit <github.com@musesystems.com>
2022-10-29 13:28:18 -0700

    Add DbTypes.Inet conversion to/from net_address
    The net_address library (module `IP`) adds network addresses that look
    very much like standard Erlang IP addresses, though this begins to
    differ once you get into representing subnets.
    
    This commit adds conversion to and from the DbType.Inet which the
    database changesets are expecting.  It's difficult to know right now
    which style will be easier to work with in client code, so the idea
    right now is that it shouldn't matter much which is chosen.
    
    Also committed here is a fix for the DbTypes.Inet to Postgrex.INET
    conversion functions.  I didn't read the typespecs closely enough and 
    assumed a more Erlangy representation for the netmask when, in fact, the
    desired representation was just an integer count of the bits in the 
    mask.

--- components/system/msbms_syst_datastore/test/db_types_inet_test.exs
+++ components/system/msbms_syst_datastore/test/db_types_inet_test.exs
@@ -13,27 +13,42 @@
 defmodule DbTypesInetTest do
   use ExUnit.Case, async: true
 
+  import IP, only: [sigil_i: 2]
+
   alias MsbmsSystDatastore.DbTypes
 
   test "Can convert DbTypes.Inet to Postgrex.INET" do
     assert %Postgrex.INET{address: {10, 1, 1, 10}} =
              DbTypes.Inet.to_postgrex_inet(%DbTypes.Inet{address: {10, 1, 1, 10}, netmask: nil})
 
-    assert %Postgrex.INET{address: {10, 1, 1, 0}, netmask: {255, 255, 255, 0}} =
-             DbTypes.Inet.to_postgrex_inet(%DbTypes.Inet{
-               address: {10, 1, 1, 0},
-               netmask: {255, 255, 255, 0}
-             })
+    assert %Postgrex.INET{address: {10, 1, 1, 0}, netmask: 24} =
+             DbTypes.Inet.to_postgrex_inet(%DbTypes.Inet{address: {10, 1, 1, 0}, netmask: 24})
   end
 
   test "Can convert Postgrex.INET to DbTypes.Inet" do
     assert %DbTypes.Inet{address: {10, 1, 1, 10}} =
              DbTypes.Inet.from_postgrex_inet(%Postgrex.INET{address: {10, 1, 1, 10}, netmask: nil})
 
-    assert %DbTypes.Inet{address: {10, 1, 1, 0}, netmask: {255, 255, 255, 0}} =
-             DbTypes.Inet.from_postgrex_inet(%Postgrex.INET{
-               address: {10, 1, 1, 0},
-               netmask: {255, 255, 255, 0}
-             })
+    assert %DbTypes.Inet{address: {10, 1, 1, 0}, netmask: 24} =
+             DbTypes.Inet.from_postgrex_inet(%Postgrex.INET{address: {10, 1, 1, 0}, netmask: 24})
+  end
+
+  test "Can convert DbTypes.Inet to IP.addr & IP.Subnet" do
+    assert ~i"10.231.231.111" =
+             DbTypes.Inet.to_net_address(%DbTypes.Inet{address: {10, 231, 231, 111}})
+
+    assert ~i"10.231.231.0/24" =
+             DbTypes.Inet.to_net_address(%DbTypes.Inet{address: {10, 231, 231, 0}, netmask: 24})
+  end
+
+  test "Can convert IP.addr & IP.Subnet to DbTypes.Inet" do
+    assert %DbTypes.Inet{address: {10, 231, 231, 111}} =
+             DbTypes.Inet.from_net_address(~i"10.231.231.111")
+
+    assert %DbTypes.Inet{address: {10, 231, 231, 0}, netmask: 24} =
+             DbTypes.Inet.from_net_address(~i"10.231.231.0/24")
+
+    assert %DbTypes.Inet{address: {10, 1, 1, 0}, netmask: 24} =
+             DbTypes.Inet.from_net_address(~i"10.1.1.0/24")
   end
 end
