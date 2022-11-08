344a2037d46032962d4bcc256ffa4d2cd983cbc5
Steven C. Buttgereit <github.com@musesystems.com>
2022-11-08 06:13:21 -0800

    Add MsbmsSystAuthentication.Impl.ExtendedMgmtLogic
    This module carries the combined logic functions for managing
    authenticators.
    
    In this commit the module is not complete, but contains functionality
    for Email/Password, Validation Tokens, and Recovery Tokens.

---
+++ components/system/msbms_syst_authentication/lib/impl/extended_mgmt_logic.ex
@@ -0,0 +1,291 @@
+# Source File: extended_mgmt_logic.ex
+# Location:    musebms/components/system/msbms_syst_authentication/lib/impl/extended_mgmt_logic.ex
+# Project:     Muse Systems Business Management System
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https://muse.systems
+
+defmodule MsbmsSystAuthentication.Impl.ExtendedMgmtLogic do
+  alias MsbmsSystAuthentication.Data
+  alias MsbmsSystAuthentication.Impl
+  alias MsbmsSystAuthentication.Types
+
+  require Logger
+
+  @moduledoc false
+
+  # ============================================================================
+  #
+  # Email / Password Management
+  #
+  # ============================================================================
+
+  @spec create_authenticator_email_password(
+          Types.access_account_id(),
+          Types.account_identifier(),
+          Types.credential(),
+          Keyword.t()
+        ) ::
+          {:ok, Types.authenticator_result()}
+          | {:error, MsbmsSystError.t() | Exception.t()}
+  def create_authenticator_email_password(access_account_id, email_addr, plaintext_pwd, opts) do
+    opts = MsbmsSystUtils.resolve_options(opts, create_validated: false, validation_token: nil)
+
+    authenticator_func = fn ->
+      with {:ok, email_identity} <-
+             Impl.Identity.Email.create_identity(access_account_id, email_addr, opts),
+           :ok <-
+             Impl.Credential.Password.set_credential(access_account_id, plaintext_pwd, opts),
+           {:ok, validator_result} <-
+             maybe_create_email_validator(opts[:create_validated], email_identity, opts) do
+        Map.merge(validator_result, %{
+          access_account_id: access_account_id,
+          account_identifier: email_identity.account_identifier
+        })
+      else
+        {:invalid_credential, _} = cred_violations ->
+          MsbmsSystDatastore.rollback(cred_violations)
+
+        error ->
+          MsbmsSystDatastore.rollback(%MsbmsSystError{
+            code: :undefined_error,
+            message: "Failed creating Email/Password Authenticator.",
+            cause: error
+          })
+      end
+    end
+
+    MsbmsSystDatastore.transaction(authenticator_func)
+  rescue
+    error ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+
+      {
+        :error,
+        %MsbmsSystError{
+          code: :undefined_error,
+          message: "Exception while creating Email/Password Authenticator.",
+          cause: error
+        }
+      }
+  end
+
+  defp maybe_create_email_validator(true = _create_validated, _email_identity, _opts),
+    do: {:ok, %{}}
+
+  defp maybe_create_email_validator(false = _create_validated, email_identity, opts),
+    do: create_validator(email_identity, opts)
+
+  # ============================================================================
+  #
+  # Validation Token Management
+  #
+  # ============================================================================
+
+  @spec request_identity_validation(Types.identity_id() | Data.SystIdentities.t(), Keyword.t()) ::
+          {:ok, Types.authenticator_result()} | {:error, MsbmsSystError.t() | Exception.t()}
+  def request_identity_validation(target_identity_id, opts) when is_binary(target_identity_id) do
+    target_identity_id
+    |> Impl.Identity.get_identity_record()
+    |> request_identity_validation(opts)
+  rescue
+    error ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+
+      {
+        :error,
+        %MsbmsSystError{
+          code: :undefined_error,
+          message: "Failure requesting Identity validation by Identity ID.",
+          cause: error
+        }
+      }
+  end
+
+  def request_identity_validation(%Data.SystIdentities{} = target_identity, opts) do
+    with {:ok, validator} <- create_validator(target_identity, opts) do
+      validator
+      |> Map.put(:access_account_id, target_identity.access_account_id)
+      |> then(&{:ok, &1})
+    else
+      error ->
+        raise MsbmsSystError,
+          code: :undefined_error,
+          message: "Failure requesting Identity Validation.",
+          cause: error
+    end
+  rescue
+    error in [MsbmsSystError] ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+      {:error, error}
+
+    error ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+
+      {:error,
+       %MsbmsSystError{
+         code: :undefined_error,
+         message: "Error creating Validation Authenticator",
+         cause: error
+       }}
+  end
+
+  defp create_validator(identity, opts) do
+    with {:ok, validation_identity} <-
+           Impl.Identity.Validation.request_identity_validation(identity, opts),
+         {:ok, validation_credential} <-
+           Impl.Credential.Validation.set_credential(
+             validation_identity.access_account_id,
+             validation_identity.id,
+             opts[:validation_token],
+             opts
+           ) do
+      {:ok,
+       %{
+         validation_identifier: validation_identity.account_identifier,
+         validation_credential: validation_credential
+       }}
+    else
+      error ->
+        {:error,
+         %MsbmsSystError{
+           code: :undefined_error,
+           message: "Error creating Validation Authenticator",
+           cause: error
+         }}
+    end
+  end
+
+  # There's a good argument that revoke_validator_for_identity_id/1 belongs in
+  # `Impl.Identity.Validation` since we're not combining cross-module
+  # functionality.  The sole rationale for placing it here is based on our
+  # starting with an Identity ID for a different kind of Identity.  To a lesser
+  # degree we're also combining more primitive functions from the private API
+  # defined in `Impl.Identity.Validation` into a more appropriate public API
+  # composite function.
+
+  @spec revoke_validator_for_identity_id(Types.identity_id()) ::
+          {:ok, :deleted | :not_found} | {:error, MsbmsSystError.t() | Exception.t()}
+  def revoke_validator_for_identity_id(target_identity_id) do
+    with {:ok, %Data.SystIdentities{} = validation_identity} <-
+           Impl.Identity.Validation.get_validation_identity_for_identity_id(target_identity_id),
+         {:ok, %Data.SystIdentities{}} <-
+           Impl.Identity.Validation.revoke_identity_validation(validation_identity) do
+      {:ok, :deleted}
+    else
+      {:ok, nil} ->
+        {:ok, :not_found}
+
+      error ->
+        {:error,
+         %MsbmsSystError{
+           code: :undefined_error,
+           message: "Failed processing Identity validation revocation.",
+           cause: error
+         }}
+    end
+  rescue
+    error ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+
+      {
+        :error,
+        %MsbmsSystError{
+          code: :undefined_error,
+          message: "Failure revoking Identity validation.",
+          cause: error
+        }
+      }
+  end
+
+  # ============================================================================
+  #
+  # Recovery Token Management
+  #
+  # ============================================================================
+
+  @spec request_password_recovery(Types.access_account_id(), Keyword.t()) ::
+          {:ok, Types.authenticator_result()} | {:error, MsbmsSystError.t() | Exception.t()}
+  def request_password_recovery(access_account_id, opts) do
+    with {:ok, recovery_identity} <-
+           Impl.Identity.Recovery.request_credential_recovery(access_account_id, opts),
+         {:ok, recovery_credential} <-
+           Impl.Credential.Recovery.set_credential(
+             recovery_identity.access_account_id,
+             recovery_identity.id,
+             opts[:recovery_token],
+             opts
+           ) do
+      {:ok,
+       %{
+         access_account_id: recovery_identity.access_account_id,
+         account_identifier: recovery_identity.account_identifier,
+         credential: recovery_credential
+       }}
+    end
+  rescue
+    error in [MsbmsSystError] ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+      {:error, error}
+
+    error ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+
+      {:error,
+       %MsbmsSystError{
+         code: :undefined_error,
+         message: "Error creating Recovery Authenticator",
+         cause: error
+       }}
+  end
+
+  @spec revoke_password_recovery(Types.access_account_id() | Data.SystIdentities.t()) ::
+          {:ok, :deleted | :not_found} | {:error, MsbmsSystError.t() | Exception.t()}
+  def revoke_password_recovery(access_account_id) when is_binary(access_account_id) do
+    with {:ok, %Data.SystIdentities{} = recovery_identity} <-
+           Impl.Identity.Recovery.get_recovery_identity_for_access_account_id(access_account_id),
+         :ok <- Impl.Identity.Recovery.revoke_credential_recovery(recovery_identity) do
+      {:ok, :deleted}
+    else
+      {:ok, nil} ->
+        {:ok, :not_found}
+
+      error ->
+        {:error,
+         %MsbmsSystError{
+           code: :undefined_error,
+           message: "Failed processing Identity recovery revocation.",
+           cause: error
+         }}
+    end
+  rescue
+    error ->
+      Logger.error(Exception.format(:error, error, __STACKTRACE__))
+
+      {
+        :error,
+        %MsbmsSystError{
+          code: :undefined_error,
+          message: "Failure revoking Identity recovery.",
+          cause: error
+        }
+      }
+  end
+
+  # ============================================================================
+  #
+  # API Token Management
+  #
+  # ============================================================================
+
+  # ============================================================================
+  #
+  # Account Code Management
+  #
+  # ============================================================================
+end
