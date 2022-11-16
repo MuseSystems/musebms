3b7964a33642aff51abb922468410932c0431c53
Steven C. Buttgereit <github.com@musesystems.com>
2022-11-16 13:23:35 -0800

    Refactor scratch/workfile for new module names

--- tools/workfiles/elixir_rebuild_cmds.fish
+++ tools/workfiles/elixir_rebuild_cmds.fish
@@ -1,29 +1,36 @@
 cd ~/source/products/musebms
-cd components/system/msbms_syst_error
+cd components/system/mscmp_syst_error
 rm -Rf _build; rm -Rf deps
 mix deps.clean --all; mix deps.update --all; mix deps.get
 cd ../../../
-cd components/system/msbms_syst_utils
+cd components/system/mscmp_syst_utils
 rm -Rf _build; rm -Rf deps
 mix deps.clean --all; mix deps.update --all; mix deps.get
 cd ../../../
-cd components/system/msbms_syst_options
+cd components/system/mscmp_syst_options
+rm -Rf _build; rm -Rf deps
+mix deps.clean --all; mix deps.update --all; mix deps.get
+cd components/system/mscmp_syst_limiter
 rm -Rf _build; rm -Rf deps
 mix deps.clean --all; mix deps.update --all; mix deps.get
 cd ../../../
-cd components/system/msbms_syst_datastore
+cd components/system/mscmp_syst_db
 rm -Rf _build; rm -Rf deps
 mix deps.clean --all; mix deps.update --all; mix deps.get
 cd ../../../
-cd components/system/msbms_syst_settings
+cd components/system/mscmp_syst_settings
 rm -Rf _build; rm -Rf deps
 mix deps.clean --all; mix deps.update --all; mix deps.get
 cd ../../../
-cd components/system/msbms_syst_enums
+cd components/system/mscmp_syst_enums
 rm -Rf _build; rm -Rf deps
 mix deps.clean --all; mix deps.update --all; mix deps.get
 cd ../../../
-cd components/system/msbms_syst_instance_mgr
+cd components/system/mscmp_syst_instance
+rm -Rf _build; rm -Rf deps
+mix deps.clean --all; mix deps.update --all; mix deps.get
+cd ../../../
+cd components/system/mscmp_syst_authn
 rm -Rf _build; rm -Rf deps
 mix deps.clean --all; mix deps.update --all; mix deps.get
 cd ../../../
@@ -36,39 +43,39 @@ cd ~/source/products/musebms
 
 
 cd ~/source/products/musebms
-cd components/system/msbms_syst_error
+cd components/system/mscmp_syst_error
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile; mix dialyzer.clean; mix dialyzer --plt
 cd ../../../
-cd components/system/msbms_syst_utils
+cd components/system/mscmp_syst_utils
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile; mix dialyzer.clean; mix dialyzer --plt
 cd ../../../
-cd components/system/msbms_syst_options
+cd components/system/mscmp_syst_options
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile; mix dialyzer.clean; mix dialyzer --plt
 cd ../../../
-cd components/system/msbms_syst_rate_limiter
+cd components/system/mscmp_syst_limiter
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile; mix dialyzer.clean; mix dialyzer --plt
 cd ../../../
-cd components/system/msbms_syst_datastore
+cd components/system/mscmp_syst_db
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile; mix dialyzer.clean; mix dialyzer --plt
 cd ../../../
-cd components/system/msbms_syst_settings
+cd components/system/mscmp_syst_settings
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile; mix dialyzer.clean; mix dialyzer --plt
 cd ../../../
-cd components/system/msbms_syst_enums
+cd components/system/mscmp_syst_enums
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile; mix dialyzer.clean; mix dialyzer --plt
 cd ../../../
-cd components/system/msbms_syst_instance_mgr
+cd components/system/mscmp_syst_instance
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile; mix dialyzer.clean; mix dialyzer --plt
 cd ../../../
-cd components/system/msbms_syst_authentication
+cd components/system/mscmp_syst_authn
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile; mix dialyzer.clean; mix dialyzer --plt
 cd ../../../
@@ -80,35 +87,39 @@ cd ~/source/products/musebms
 ===========================
 
 cd ~/source/products/musebms
-cd components/system/msbms_syst_error
+cd components/system/mscmp_syst_error
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.get; mix compile; mix dialyzer --plt &
 cd ../../../
-cd components/system/msbms_syst_utils
+cd components/system/mscmp_syst_utils
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.get; mix compile; mix dialyzer --plt &
 cd ../../../
-cd components/system/msbms_syst_options
+cd components/system/mscmp_syst_options
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.get; mix compile; mix dialyzer --plt &
 cd ../../../
-cd components/system/msbms_syst_rate_limiter
+cd components/system/mscmp_syst_limiter
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.get; mix compile; mix dialyzer --plt &
 cd ../../../
-cd components/system/msbms_syst_datastore
+cd components/system/mscmp_syst_db
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.get; mix compile; mix dialyzer --plt &
 cd ../../../
-cd components/system/msbms_syst_settings
+cd components/system/mscmp_syst_settings
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.get; mix compile; mix dialyzer --plt &
 cd ../../../
-cd components/system/msbms_syst_enums
+cd components/system/mscmp_syst_enums
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.get; mix compile; mix dialyzer --plt &
 cd ../../../
-cd components/system/msbms_syst_instance_mgr
+cd components/system/mscmp_syst_instance
+rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
+mix deps.clean --all; mix deps.get; mix compile; mix dialyzer --plt &
+cd ../../../
+cd components/system/mscmp_syst_authn
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.get; mix compile; mix dialyzer --plt &
 cd ../../../
@@ -121,39 +132,39 @@ cd ~/source/products/musebms
 
 
 cd ~/source/products/musebms
-cd components/system/msbms_syst_error
+cd components/system/mscmp_syst_error
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile
 cd ../../../
-cd components/system/msbms_syst_utils
+cd components/system/mscmp_syst_utils
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile
 cd ../../../
-cd components/system/msbms_syst_options
+cd components/system/mscmp_syst_options
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile
 cd ../../../
-cd components/system/msbms_syst_rate_limiter
+cd components/system/mscmp_syst_limiter
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile
 cd ../../../
-cd components/system/msbms_syst_datastore
+cd components/system/mscmp_syst_db
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile
 cd ../../../
-cd components/system/msbms_syst_settings
+cd components/system/mscmp_syst_settings
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile
 cd ../../../
-cd components/system/msbms_syst_enums
+cd components/system/mscmp_syst_enums
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile
 cd ../../../
-cd components/system/msbms_syst_instance_mgr
+cd components/system/mscmp_syst_instance
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile
 cd ../../../
-cd components/system/msbms_syst_authentication
+cd components/system/mscmp_syst_authn
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile
 cd ../../../
@@ -161,28 +172,28 @@ cd application/msbms
 rm -Rf _build; rm -Rf .elixir_ls; rm -Rf deps
 mix deps.clean --all; mix deps.unlock --all; mix deps.get; mix compile
 cd ~/source/products/musebms
-cd components/system/msbms_syst_error
+cd components/system/mscmp_syst_error
 mix dialyzer.clean; mix dialyzer --plt
 cd ../../../
-cd components/system/msbms_syst_options
+cd components/system/mscmp_syst_options
 mix dialyzer.clean; mix dialyzer --plt
 cd ../../../
-cd components/system/msbms_syst_rate_limiter
+cd components/system/mscmp_syst_limiter
 mix dialyzer.clean; mix dialyzer --plt
 cd ../../../
-cd components/system/msbms_syst_datastore
+cd components/system/mscmp_syst_db
 mix dialyzer.clean; mix dialyzer --plt
 cd ../../../
-cd components/system/msbms_syst_settings
+cd components/system/mscmp_syst_settings
 mix dialyzer.clean; mix dialyzer --plt
 cd ../../../
-cd components/system/msbms_syst_enums
+cd components/system/mscmp_syst_enums
 mix dialyzer.clean; mix dialyzer --plt
 cd ../../../
-cd components/system/msbms_syst_instance_mgr
+cd components/system/mscmp_syst_instance
 mix dialyzer.clean; mix dialyzer --plt
 cd ../../../
-cd components/system/msbms_syst_authentication
+cd components/system/mscmp_syst_authn
 mix dialyzer.clean; mix dialyzer --plt
 cd ../../../
 cd application/msbms
