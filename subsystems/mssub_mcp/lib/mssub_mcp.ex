c075763ac947999accb006006739440c58e54b8b
Steven C. Buttgereit <github.com@musesystems.com>
2022-11-18 06:42:40 -0800

    Add subsystem MssubMcp skeleton
    This is the clean mix new --sup Elixir project mssub_mcp.

---
+++ subsystems/mssub_mcp/lib/mssub_mcp.ex
@@ -0,0 +1,18 @@
+defmodule MssubMcp do
+  @moduledoc """
+  Documentation for `MssubMcp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MssubMcp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
