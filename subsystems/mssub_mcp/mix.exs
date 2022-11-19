e8b7038cb1ee8d971f265090e3d687d14efeccbf
Steven C. Buttgereit <github.com@musesystems.com>
2022-11-19 04:41:19 -0800

    Refactor MssubMcp for our basic structure
    This is a minor refactor.  Basically getting our directory structure and
    some boilerplate in config, credo, and mix.exs.  Far from final, but a
    start.

--- subsystems/mssub_mcp/mix.exs
+++ subsystems/mssub_mcp/mix.exs
@@ -1,29 +1,92 @@
+# Source File: mix.exs
+# Location:    musebms/subsystems/mssub_mcp/mix.exs
+# Project:     Muse Systems Business Management System
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https://muse.systems
+
 defmodule MssubMcp.MixProject do
   use Mix.Project
 
+  @name :mssub_mcp
+  @version "0.1.0"
+
+  @deps [
+    # Third Party Dependencies
+    {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
+    {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+    {:ex_doc, "~> 0.20", only: :dev, runtime: false},
+    {:gradient, github: "esl/gradient", only: [:dev], runtime: false},
+
+    # Muse Systems Business Management System Components
+    {:mscmp_syst_utils, path: "../../components/system/mscmp_syst_utils"},
+    {:mscmp_syst_error, path: "../../components/system/mscmp_syst_error"}
+  ]
+
+  @dialyzer_opts [
+    flags: ["-Wunmatched_returns", :error_handling, :underspecs],
+    plt_add_apps: [:mix]
+  ]
+
+  # ------------------------------------------------------------
+
   def project do
+    in_production = Mix.env() == :prod
+
     [
-      app: :mssub_mcp,
-      version: "0.1.0",
+      app: @name,
+      version: @version,
       elixir: "~> 1.14",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: @deps,
+      build_embedded: in_production,
+      start_permanent: in_production,
+      dialyzer: @dialyzer_opts,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      docs: [
+        name: "MssubMcp",
+        nest_modules_by_prefix: [MscmpSystAuthn.Data],
+        main: "MssubMcp",
+        output: "../../../documentation/technical/app_server/mscmp_syst_authn",
+        deps: [
+          mscmp_syst_db: "../../documentation/technical/app_server/mscmp_syst_db",
+          mscmp_syst_error: "../../documentation/technical/app_server/mscmp_syst_error",
+          mscmp_syst_utils: "../../documentation/technical/app_server/mscmp_syst_utils",
+          mscmp_syst_limiter: "../../documentation/technical/app_server/mscmp_syst_limiter",
+          mscmp_syst_enums: "../../documentation/technical/app_server/mscmp_syst_enums",
+          mscmp_syst_options: "../../documentation/technical/app_server/mscmp_syst_options",
+          mscmp_syst_instance: "../../documentation/technical/app_server/mscmp_syst_instance",
+          mscmp_syst_authn: "../../documentation/technical/app_server/mscmp_syst_authn"
+        ],
+        groups_for_functions: [Runtime: &(&1[:section] == :service_management)],
+        groups_for_modules: [
+          API: [MssubMcp],
+          Data: [],
+          "Supporting Types": []
+        ]
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [
+        :logger,
+        :crypto,
+        :mnesia
+      ],
       mod: {MssubMcp.Application, []}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
-  end
+  # ++ ["test/support"]
+  defp elixirc_paths(:test), do: elixirc_paths()
+  # ++ ["dev_support"]
+  defp elixirc_paths(:dev), do: elixirc_paths()
+  defp elixirc_paths(_), do: elixirc_paths()
+  defp elixirc_paths(), do: ["lib"]
 end
