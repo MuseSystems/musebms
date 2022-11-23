da951a37af3a4ed72f3954539775e661b1774cf2
Steven C. Buttgereit <github.com@musesystems.com>
2022-11-22 19:01:17 -0800

    Add MssubMcp Enums, Settings, & Rate Limiter setup
    My feeling is that these are a bit too naive in implementation, but for
    now it's working.  Let's keep it this way for now and we can address if
    it becomes an issue.

--- subsystems/mssub_mcp/lib/runtime/application.ex
+++ subsystems/mssub_mcp/lib/runtime/application.ex
@@ -10,10 +10,33 @@ defmodule MssubMcp.Application do
   def start(_type, args) do
     opts = MscmpSystUtils.resolve_options(args[:opts], datastore_name: @datastore_supervisor_name)
 
+    setup_rate_limiter()
+
+    enum_service_spec = %{
+      id: MssubMcpEnumsService,
+      start: {MscmpSystEnums, :start_link, [{:mssub_mcp_enums_service, :mssub_mcp_app_access}]}
+    }
+
+    settings_service_spec = %{
+      id: MssubMcpSettingsService,
+      start:
+        {MscmpSystSettings, :start_link, [{:mssub_mcp_settings_service, :mssub_mcp_app_access}]}
+    }
+
     mcp_service_childredn = [
-      MssubMcp.Runtime.Datastore.child_spec(opts)
+      MssubMcp.Runtime.Datastore.child_spec(opts),
+      enum_service_spec,
+      settings_service_spec
     ]
 
     Supervisor.start_link(mcp_service_childredn, strategy: :one_for_one, name: @supervisor_name)
+  end
+
+  defp setup_rate_limiter() do
+    :mnesia.stop()
+    :mnesia.create_schema([node()])
+    :mnesia.start()
+
+    MscmpSystLimiter.init_rate_limiter()
   end
 end
