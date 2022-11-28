9d8de7e47fe55b2e570fd4c46da2ffe95354bfff
Steven C. Buttgereit <github.com@musesystems.com>
2022-11-28 06:15:03 -0800

    Add MssubMcp starting Instance Types

---
+++ database/subsystems/mssub_mcp/gen_seed_data/initialize_enum_instance_types.eex.sql
@@ -0,0 +1,34 @@
+-- File:        initialize_enum_instance_types.eex.sql
+-- Location:    musebms/database/subsystems/mssub_mcp/gen_seed_data/initialize_enum_instance_types.eex.sql
+-- Project:     Muse Systems Business Management System
+--
+-- Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+-- This file may include content copyrighted and licensed from third parties.
+--
+-- See the LICENSE file in the project root for license terms and conditions.
+-- See the NOTICE file in the project root for copyright ownership information.
+--
+-- muse.information@musesystems.com :: https://muse.systems
+
+INSERT INTO ms_syst_data.syst_enum_items
+    ( internal_name
+    , display_name
+    , external_name
+    , enum_id
+    , enum_default
+    , syst_defined
+    , user_maintainable
+    , syst_description
+    , sort_order )
+VALUES
+    ( 'instance_types_sysdef_standard'
+    , 'Instance Types / Standard'
+    , 'Standard'
+    , ( SELECT id
+        FROM ms_syst_data.syst_enums
+        WHERE internal_name = 'instance_types' )
+    , TRUE
+    , TRUE
+    , TRUE
+    , 'A simple type representing the most typical kind of Instance.'
+    , 1 );
