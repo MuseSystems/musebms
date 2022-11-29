a528973ecb5ad4134216be5557fadb2a23c17c45
Steven C. Buttgereit <github.com@musesystems.com>
2022-11-29 14:42:04 -0800

    Fix MssubMcp.Runtime.Application Dialyzer warning
    Overall this is probably a code improvement above just making Dialyzer
    quiet.  We're more explicitly handling outcomes in a lot of this.

--- subsystems/mssub_mcp/lib/runtime/application.ex
+++ subsystems/mssub_mcp/lib/runtime/application.ex
@@ -26,45 +26,58 @@ defmodule MssubMcp.Runtime.Application do
   def start(_type, args) do
     opts = MscmpSystUtils.resolve_options(args[:opts], datastore_name: @datastore_supervisor_name)
 
-    setup_rate_limiter()
+    case setup_rate_limiter() do
+      {:ok, _} ->
+        enum_service_spec = %{
+          id: MssubMcpEnumsService,
+          start:
+            {MscmpSystEnums, :start_link,
+             [
+               {Application.get_env(:mssub_mcp, :enums_service_name, @default_enums_service_name),
+                :mssub_mcp_app_access}
+             ]}
+        }
 
-    enum_service_spec = %{
-      id: MssubMcpEnumsService,
-      start:
-        {MscmpSystEnums, :start_link,
-         [
-           {Application.get_env(:mssub_mcp, :enums_service_name, @default_enums_service_name),
-            :mssub_mcp_app_access}
-         ]}
-    }
+        settings_service_spec = %{
+          id: MssubMcpSettingsService,
+          start:
+            {MscmpSystSettings, :start_link,
+             [
+               {Application.get_env(
+                  :mssub_mcp,
+                  :settings_service_name,
+                  @default_settings_service_name
+                ), :mssub_mcp_app_access}
+             ]}
+        }
 
-    settings_service_spec = %{
-      id: MssubMcpSettingsService,
-      start:
-        {MscmpSystSettings, :start_link,
-         [
-           {Application.get_env(
-              :mssub_mcp,
-              :settings_service_name,
-              @default_settings_service_name
-            ), :mssub_mcp_app_access}
-         ]}
-    }
+        mcp_service_children = [
+          MssubMcp.Runtime.Datastore.child_spec(opts),
+          enum_service_spec,
+          settings_service_spec
+        ]
 
-    mcp_service_children = [
-      MssubMcp.Runtime.Datastore.child_spec(opts),
-      enum_service_spec,
-      settings_service_spec
-    ]
+        Supervisor.start_link(mcp_service_children, strategy: :one_for_one, name: @supervisor_name)
 
-    Supervisor.start_link(mcp_service_children, strategy: :one_for_one, name: @supervisor_name)
+      error ->
+        raise MscmpSystError,
+          code: :undefined_error,
+          message: "Unable to start MCP Subsystem",
+          cause: error
+    end
   end
 
   defp setup_rate_limiter do
-    :mnesia.stop()
-    :mnesia.create_schema([node()])
-    :mnesia.start()
-
-    MscmpSystLimiter.init_rate_limiter()
+    with :stopped <- :mnesia.stop(),
+         :ok <- :mnesia.create_schema([node()]),
+         :ok <- :mnesia.start() do
+      MscmpSystLimiter.init_rate_limiter()
+    else
+      error ->
+        raise MscmpSystError,
+          code: :undefined_error,
+          message: "Unable to setup Mnesia for MscmpSystLimiter.",
+          cause: error
+    end
   end
 end
