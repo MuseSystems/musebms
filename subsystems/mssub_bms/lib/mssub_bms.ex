19e5396c61d3801abfca1e6b0fce3ea397aaacf4
Steven C. Buttgereit <github.com@musesystems.com>
2022-12-02 04:27:07 -0800

    Add baseline MssubBms Elixir project
    While not exactly a clean skeleton start, it's only that plus the
    typical first step things to do on a new project.
    
    This does also include a first cut MssubBms database migration.  It's
    clear that this is a long way off from what the final will be, but we
    need something to start testing with and establishing a decent baseline
    of "what is the app" DB schema may as well start here.

---
+++ subsystems/mssub_bms/lib/mssub_bms.ex
@@ -0,0 +1,18 @@
+defmodule MssubBms do
+  @moduledoc """
+  Documentation for `MssubBms`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MssubBms.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ subsystems/mssub_bms/test/mssub_bms_test.exs
@@ -0,0 +1,8 @@
+defmodule MssubBmsTest do
+  use ExUnit.Case
+  doctest MssubBms
+
+  test "greets the world" do
+    assert MssubBms.hello() == :world
+  end
+end
