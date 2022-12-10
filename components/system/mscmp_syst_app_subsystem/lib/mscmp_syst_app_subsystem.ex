8f4ca40ee19a3758c45d703028b54bf2af6eb9c4
Steven C. Buttgereit <github.com@musesystems.com>
2022-12-10 09:02:42 -0800

    Add component MscmpSystAppSubsystem
    This is a very small utility component currently targeted at defining a
    behaviour for Application Subsystem projects to implement.  This should
    allow a common set of services that MsappPlatform can count on for
    managing its child application subsystems.
    
    This is just barely more than an Elixir project skeleton with a few
    common starting configurations implemented.

---
+++ components/system/mscmp_syst_app_subsystem/lib/mscmp_syst_app_subsystem.ex
@@ -0,0 +1,18 @@
+defmodule MscmpSystAppSubsystem do
+  @moduledoc """
+  Documentation for `MscmpSystAppSubsystem`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MscmpSystAppSubsystem.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
