36d787e662920a9ea387f8a2f6b3a80868101cc9
Steven C. Buttgereit <github.com@musesystems.com>
2022-12-11 15:01:58 -0800

    Add app_spec Application Subsystem behaviour
    Adds an initial behaviour and supporting types to the component defining
    a way for the Application Subsystem to be identifiable to MsappPlatform.
    
    Also, since we're doing some documentation, do some clean-up from the
    starting files in this regard.

--- components/system/mscmp_syst_app_subsystem/lib/mscmp_syst_app_subsystem.ex
+++ components/system/mscmp_syst_app_subsystem/lib/mscmp_syst_app_subsystem.ex
@@ -1,18 +1,25 @@
+# Source File: mscmp_syst_app_subsystem.ex
+# Location:    musebms/components/system/mscmp_syst_app_subsystem/lib/mscmp_syst_app_subsystem.ex
+# Project:     Muse Systems Business Management System
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https://muse.systems
+
 defmodule MscmpSystAppSubsystem do
-  @moduledoc """
-  Documentation for `MscmpSystAppSubsystem`.
-  """
+  @external_resource "README.md"
+
+  @moduledoc File.read!(Path.join([__DIR__, "..", "README.md"]))
+
+  alias MscmpSystAppSubsystem.Types
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MscmpSystAppSubsystem.hello()
-      :world
-
+  Allows for the retrieval of required data with which to bootstrap the
+  Application Subsystem in the database.
   """
-  def hello do
-    :world
-  end
+  @callback app_spec() :: Types.application()
 end
