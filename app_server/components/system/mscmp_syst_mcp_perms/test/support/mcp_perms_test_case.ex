889e9ef59b663e442bd8b537b8af79c9fd5db2be
Steven C. Buttgereit <github.com@musesystems.com>
2022-12-28 17:41:13 -0800

    Add MscmpSystMcpPerms
    This is the component which integrates MscmpSystPerms with data from
    MscmpSystInstance and MscmpSystAuthn in the MssubMcp subsystem.
    
    This is a skeleton commit with a fair number of the boilerplate-ish
    elements completed; there can still be a couple things left to do in
    that regard for this commit.

---
+++ app_server/components/system/mscmp_syst_mcp_perms/test/support/mcp_perms_test_case.ex
@@ -0,0 +1,24 @@
+# Source File: mcp_perms_test_case.ex
+# Location:    musebms/app_server/components/system/mscmp_syst_mcp_perms/test/support/mcp_perms_test_case.ex
+# Project:     Muse Systems Business Management System
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https://muse.systems
+
+defmodule McpPermsTestCase do
+  @moduledoc false
+
+  use ExUnit.CaseTemplate
+
+  setup do
+    [
+      datastore_context:
+        MscmpSystDb.put_datastore_context(TestSupport.get_testing_datastore_context_id())
+    ]
+  end
+end
