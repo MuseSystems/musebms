6ab9066daee63f1d1c1e27f71619462f20ef61c5
Steven C. Buttgereit <github.com@musesystems.com>
2022-12-28 17:41:13 -0800

    Add MscmpSystMcpPerms
    This is the component which integrates MscmpSystPerms with data from
    MscmpSystInstance and MscmpSystAuthn in the MssubMcp subsystem.
    
    This is a skeleton commit with a fair number of the boilerplate-ish
    elements completed; there can still be a couple things left to do in
    that regard for this commit.

---
+++ app_server/components/system/mscmp_syst_mcp_perms/test/test_helper.exs
@@ -0,0 +1,40 @@
+# Source File: test_helper.exs
+# Location:    musebms/app_server/components/system/mscmp_syst_mcp_perms/test/test_helper.exs
+# Project:     Muse Systems Business Management System
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https://muse.systems
+
+#  This testing presumes that the database schema is tested separately and is
+#  for module testing purposes.
+
+test_kind =
+  if ExUnit.configuration() |> Keyword.get(:include) |> Enum.member?(:integration) do
+    ExUnit.configure(seed: 0)
+    :integration_testing
+  else
+    ExUnit.configure(exclude: [:integration])
+    :unit_testing
+  end
+
+TestSupport.setup_testing_database(test_kind)
+
+MscmpSystDb.put_datastore_context(TestSupport.get_testing_datastore_context_id())
+
+children = [
+  {DynamicSupervisor, strategy: :one_for_one, name: MscmpSystMcpPerms.TestingSupervisor}
+]
+
+Supervisor.start_link(children, strategy: :one_for_one)
+Logger.configure(level: :info)
+
+ExUnit.start()
+
+ExUnit.after_suite(fn _suite_result ->
+  TestSupport.cleanup_testing_database(test_kind)
+end)
