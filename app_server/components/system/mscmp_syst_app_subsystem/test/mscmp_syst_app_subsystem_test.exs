0bb283a2a4df6e63718a01c5e8ec67cc07d4b32f
Steven C. Buttgereit <github.com@musesystems.com>
2023-01-02 12:19:47 -0800

    Remove MscmpSystAppSubsystem
    This is not going to be used at this point since we've introduced the
    platform idea and better ideas about subsystems (amongst other ways of
    handing this stuff).

--- app_server/components/system/mscmp_syst_app_subsystem/test/mscmp_syst_app_subsystem_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule MscmpSystAppSubsystemTest do
-  use ExUnit.Case
-  doctest MscmpSystAppSubsystem
-
-  test "greets the world" do
-    assert MscmpSystAppSubsystem.hello() == :world
-  end
-end
