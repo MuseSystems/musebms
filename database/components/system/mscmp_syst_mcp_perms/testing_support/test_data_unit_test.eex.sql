6df75d7e047e336aa6d59503b461fea1e4f8754c
Steven C. Buttgereit <github.com@musesystems.com>
2023-01-02 09:39:25 -0800

    Add MscmpSystMcpPerms unit test data

--- database/components/system/mscmp_syst_mcp_perms/testing_support/test_data_unit_test.eex.sql
+++ database/components/system/mscmp_syst_mcp_perms/testing_support/test_data_unit_test.eex.sql
@@ -9,3 +9,15 @@
 -- See the NOTICE file in the project root for copyright ownership information.
 --
 -- muse.information@musesystems.com :: https://muse.systems
+
+INSERT INTO ms_syst_data.syst_access_account_perm_role_assigns
+    ( access_account_id, perm_role_id )
+VALUES
+    ( ( SELECT id FROM ms_syst_data.syst_access_accounts WHERE internal_name = 'unowned_all_access' )
+    , ( SELECT id FROM ms_syst_data.syst_perm_roles WHERE internal_name = 'mcp_login' ) )
+  ,
+    ( ( SELECT id FROM ms_syst_data.syst_access_accounts WHERE internal_name = 'unowned_all_access' )
+    , ( SELECT id FROM ms_syst_data.syst_perm_roles WHERE internal_name = 'global_login' ) )
+  ,
+    ( ( SELECT id FROM ms_syst_data.syst_access_accounts WHERE internal_name = 'owned_all_access' )
+    , ( SELECT id FROM ms_syst_data.syst_perm_roles WHERE internal_name = 'mcp_login' ) );
