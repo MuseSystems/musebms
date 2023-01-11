eed8e965b890f9ad97f07e54ac32e2cd3e66ce29
Steven C. Buttgereit <github.com@musesystems.com>
2023-01-11 09:54:02 -0800

    Refactor MsappMcp runtime directory references
    Previously, renaming the Application and Mailer modules to be in the
    `Runtime` hierarchy was committed.  This commit extends that new
    namespacing to places in configuration files where those modules are
    referenced.

--- app_server/platform/msplatform/config/runtime.exs
+++ app_server/platform/msplatform/config/runtime.exs
@@ -88,7 +88,7 @@ if config_env() == :prod do
   # Also, you may need to configure the Swoosh API client of your choice if you
   # are not using SMTP. Here is an example of the configuration:
   #
-  #     config :msapp_mcp, MsappMcp.Mailer,
+  #     config :msapp_mcp, MsappMcp.Runtime.Mailer,
   #       adapter: Swoosh.Adapters.Mailgun,
   #       api_key: System.get_env("MAILGUN_API_KEY"),
   #       domain: System.get_env("MAILGUN_DOMAIN")
