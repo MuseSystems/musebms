b4dff334d660c3a156b21a9336867b566a951413
Steven C. Buttgereit <github.com@musesystems.com>
2023-01-30 03:36:16 -0800

    Add load_disallowed_passwords/0 to MsappMcp
    This calls the MssubMcp routine for the load, but passes a source for
    the passwords as a stream.

---
+++ app_server/platform/msplatform/apps/msapp_mcp/lib/impl/bootstrap.ex
@@ -0,0 +1,26 @@
+# Source File: bootstrap.ex
+# Location:    musebms/app_server/platform/msplatform/apps/msapp_mcp/lib/impl/bootstrap.ex
+# Project:     Muse Systems Business Management System
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https://muse.systems
+
+defmodule MsappMcp.Impl.Bootstrap do
+  @moduledoc false
+
+  @spec load_disallowed_passwords() :: :ok | {:error, MscmpSystError.t()}
+  def load_disallowed_passwords() do
+    Path.join([
+      :code.priv_dir(:msapp_mcp),
+      "database",
+      "mcp_disallowed_passwords_starter_list.txt"
+    ])
+    |> File.stream!()
+    |> MssubMcp.load_disallowed_passwords(pg_format: true)
+  end
+end
