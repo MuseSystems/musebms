cecbbcabb15d633f307354003f845e2e6020b9a7
Steven C. Buttgereit <github@musesystems.com>
2023-07-28 08:53:13 -0700

    Add file header comments to router.ex

--- app_server/platform/msplatform/apps/msapp_mcp_web/lib/msapp_mcp_web/router.ex
+++ app_server/platform/msplatform/apps/msapp_mcp_web/lib/msapp_mcp_web/router.ex
@@ -1,3 +1,14 @@
+# Source File: router.ex
+# Location:    musebms/app_server/platform/msplatform/apps/msapp_mcp_web/lib/msapp_mcp_web/router.ex
+# Project:     Muse Systems Business Management System
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https://muse.systems
 defmodule MsappMcpWeb.Router do
   use MsappMcpWeb, :router
 
