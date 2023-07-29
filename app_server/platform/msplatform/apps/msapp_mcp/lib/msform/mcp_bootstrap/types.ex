1661804590777731dea50a13e5bd5d67145cccf7
Steven C. Buttgereit <github@musesystems.com>
2023-07-29 08:12:32 -0700

    Refactor parameter type in MCP Msforms modules
    With the AuthPasswordReset form, taking a look at how we're handling
    types for use with typespecs led me to conclude we needed to have some
    Msform related handling norms.
    
    For Msform based forms, we want to define the type of the parameters
    that can be passed into the Changeset generating functions and so we'll
    have each Msform module define its own types which will usually just be
    the `parameters` type for the module.  This will be in a `types.ex` file
    kept along side the other Msform module specific files.
    
    This commit refactors the existing Msform forms (McpBootstrap &
    AuthEmailPassword) to use the new standard, removing any overlapping
    vestigial code previously written for this purpose.
    
    An additional task in this commit is also to bring McpBootstrap up to
    standard by not using a Msform.McpBootstrap alias for internal
    references and just accept that `Msform.` isn't enough to warrant the
    alias... this will make McpBootstrap consistent will other modules now
    avoiding that self-referencing alias.

---
+++ app_server/platform/msplatform/apps/msapp_mcp/lib/msform/mcp_bootstrap/types.ex
@@ -0,0 +1,22 @@
+# Source File: types.ex
+# Location:    musebms/app_server/platform/msplatform/apps/msapp_mcp/lib/msform/mcp_bootstrap/types.ex
+# Project:     Muse Systems Business Management System
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https://muse.systems
+
+defmodule Msform.McpBootstrap.Types do
+  @type parameters() :: %{
+          optional(:owner_name) => String.t() | nil,
+          optional(:owner_display_name) => String.t() | nil,
+          optional(:admin_display_name) => String.t() | nil,
+          optional(:admin_identifier) => String.t() | nil,
+          optional(:admin_credential) => String.t() | nil,
+          optional(:admin_credential_verify) => String.t() | nil
+        }
+end
