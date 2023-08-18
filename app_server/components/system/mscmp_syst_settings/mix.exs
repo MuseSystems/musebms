7432c6fd93798c8d8454e1e456d548831178ce1f
Steven C. Buttgereit <github@musesystems.com>
2023-08-17 17:57:44 -0700

    Remove Elixir library gradient from all projects
    Most of the time it just doesn't work.  We've come to terms with
    dialyzer at this point (if perhaps not come to peace with it)... so just
    lose gradient for the time being.

--- app_server/components/system/mscmp_syst_settings/mix.exs
+++ app_server/components/system/mscmp_syst_settings/mix.exs
@@ -21,7 +21,6 @@ defmodule MscmpSystSettings.MixProject do
     {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
     {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
     {:ex_doc, "~> 0.20", only: :dev, runtime: false},
-    {:gradient, github: "esl/gradient", only: [:dev], runtime: false},
 
     # Muse Systems Business Management System Components
     {:mscmp_syst_error, path: "../mscmp_syst_error"},
