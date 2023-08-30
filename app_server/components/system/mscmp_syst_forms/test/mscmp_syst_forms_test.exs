9153c0bcfe7623211329efcc25592304193946d7
Steven C. Buttgereit <github@musesystems.com>
2023-08-30 05:40:17 -0700

    Create MscmpSystForms dummy test
    We're punting on MscmpSystForms testing for now.  There's a fair amount
    to unpack around successfully testing the web user interface (and its
    constituent logic) and that study hasn't been completed yet.
    
    Rather than hold everything up, we'll allow ourselves to defer creating
    tests here for now and revisit as our plans become better defined.  This
    will also allow us to develop a couple more features which will better
    expand our sense of user interface testing needs.

--- app_server/components/system/mscmp_syst_forms/test/mscmp_syst_forms_test.exs
+++ app_server/components/system/mscmp_syst_forms/test/mscmp_syst_forms_test.exs
@@ -2,7 +2,11 @@ defmodule MscmpSystFormsTest do
   use ExUnit.Case
   doctest MscmpSystForms
 
-  test "greets the world" do
-    assert MscmpSystForms.hello() == :world
+  # TODO: Naturally, this we're not testing anything with what's written here.
+  #       Currently studying the issue and making decisions on we can best test
+  #       components and their combined outputs.
+
+  test "Closes eyes and pretends to test something." do
+    assert true
   end
 end
