6486ea304a5ef4c011ca3be0f3c6c014d0bb238a
Steven C. Buttgereit <github@musesystems.com>
2023-09-14 03:40:05 -0700

    Refactor MscmpSystDb structs; files, docs
    This commit takes the following actions:
    
      - Break out the newly created structs from the types.ex file into
        their own individual files under lib/types/*.ex.  The combined file
        just had too much noise making it harder to find what even applied;
        this is especially true since we include type documentation.
    
      - Refactor the struct related documentation, especially those docs
        associated with the `t/0` type.  The prior approach wasn't helpful
        in text editors and hover-over texts.

---
+++ app_server/components/system/mscmp_syst_db/lib/types/bounds_compare_result.ex
@@ -0,0 +1,34 @@
+# Source File: bounds_compare_result.ex
+# Location:    musebms/app_server/components/system/mscmp_syst_db/lib/types/bounds_compare_result.ex
+# Project:     Muse Systems Business Management System
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https://muse.systems
+
+defmodule MscmpSystDb.Types.BoundsCompareResult do
+  @moduledoc """
+  The comparison operators for both the lower and upper bounds of a range type.
+
+  There are cases where normal comparisons are too coarse-grained to provide a
+  meaningful result when dealing with ranges.  In these cases you need the
+  detailed lower/upper comparison results.
+  """
+
+  @enforce_keys [:lower_comparison, :upper_comparison]
+  defstruct [:lower_comparison, :upper_comparison]
+
+  @typedoc """
+  The comparison operators for both the lower and upper bounds of a range type.
+
+  See `MscmpSystDb.Types.BoundsCompareResult` for more.
+  """
+  @type t :: %__MODULE__{
+          lower_comparison: MscmpSystDb.Types.db_type_comparison_operators(),
+          upper_comparison: MscmpSystDb.Types.db_type_comparison_operators()
+        }
+end
---
+++ app_server/components/system/mscmp_syst_db/lib/types/datastore_options.ex
@@ -0,0 +1,56 @@
+# Source File: datastore_options.ex
+# Location:    musebms/app_server/components/system/mscmp_syst_db/lib/types/datastore_options.ex
+# Project:     Muse Systems Business Management System
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https://muse.systems
+
+defmodule MscmpSystDb.Types.DatastoreOptions do
+  @moduledoc """
+  A struct defining the connection parameters to use to connect to a Datastore
+  backing database.
+  """
+
+  @enforce_keys [:database_name, :contexts, :db_server]
+  defstruct [:database_name, :datastore_code, :datastore_name, :contexts, :db_server]
+
+  @typedoc """
+  A struct defining the connection parameters to use to connect to a Datastore
+  backing database.
+
+  ## Attributes
+
+    * `:database_name` - The name of the database in the database server to
+      which the connection will be made.  Often times this value will be the
+      same as the String.t() form of the `:datastore_name` value.
+
+    * `:datastore_code` - Defines a Datastore specific salting value for use in
+      certain security and cryptographic related functions.
+
+    * `:datastore_name` - A name for use by the application to identify a given
+      Datastore.  This value will often time be the same as the `:database_name`
+      value, except as an atom() rather than a String.t().
+
+    * `:contexts` - A list of available Datastore Context values defining which
+      contexts are available for this Datastore.  See
+      `t:MscmpSystDb.Types.DatastoreContext.t/0` for more information concerning
+      the values of each entry in the list.
+
+    * `:db_server` - The database server information for the server where the
+      Datastore database resides.  See 't:MscmpSystDb.Types.DbServer.t/0' for
+      additional details.
+  """
+
+  @type t :: %__MODULE__{
+          database_name: String.t(),
+          datastore_code: String.t() | nil,
+          datastore_name: atom() | nil,
+          contexts: [MscmpSystDb.Types.DatastoreContext.t()] | [],
+          db_server: MscmpSystDb.Types.DbServer.t()
+        }
+end
