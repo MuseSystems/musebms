c7f0e91d7a4ede1ccc7622b66c2efa354e8d71fc
Steven C. Buttgereit <github@musesystems.com>
2023-09-14 03:47:28 -0700

    Refactor MscmpSystMcpPerms structs; files
    This commit takes the following actions:
    
      - Break out the newly created structs from the types.ex file into
        their own individual files under lib/types/*.ex.  The combined file
        just had too much noise making it harder to find what even applied;
        this is especially true since we include type documentation.

---
+++ app_server/components/system/mscmp_syst_mcp_perms/lib/types/access_account_perms_selector.ex
@@ -0,0 +1,73 @@
+# Source File: access_account_perms_selector.ex
+# Location:    musebms/app_server/components/system/mscmp_syst_mcp_perms/lib/types/access_account_perms_selector.ex
+# Project:     Muse Systems Business Management System
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https://muse.systems
+
+defmodule MscmpSystMcpPerms.Types.AccessAccountPermsSelector do
+  @moduledoc """
+  Defines the Access Account selector for globally applicable MCP permissions.
+  """
+
+  alias MscmpSystMcpPerms.Impl
+
+  @enforce_keys [:access_account_id]
+  defstruct access_account_id: nil
+
+  @typedoc """
+  Defines the Access Account selector for globally applicable MCP permissions.
+
+  ## Attributes
+
+    * `access_account_id` - the record ID of the Access Account which is the
+    subject of Permissions related actions.
+  """
+  @type t() :: %__MODULE__{
+          access_account_id: MscmpSystAuthn.Types.access_account_id()
+        }
+
+  defimpl MscmpSystPerms.Protocol do
+    @spec get_effective_perm_grants(
+            MscmpSystMcpPerms.Types.AccessAccountPermsSelector.t(),
+            Keyword.t()
+          ) ::
+            {:ok, MscmpSystPerms.Types.perm_grants()} | {:error, MscmpSystError.t()}
+    def get_effective_perm_grants(selector, opts \\ []),
+      do: Impl.AccessAccountPermRole.get_effective_perm_grants(selector, opts)
+
+    @spec list_perm_grants(MscmpSystMcpPerms.Types.AccessAccountPermsSelector.t(), Keyword.t()) ::
+            {:ok, [Msdata.SystPermRoles.t()]} | {:error, MscmpSystError.t()}
+    def list_perm_grants(selector, opts \\ []),
+      do: Impl.AccessAccountPermRole.list_perm_grants(selector, opts)
+
+    @spec list_perm_denials(
+            MscmpSystMcpPerms.Types.AccessAccountPermsSelector.t(),
+            Keyword.t()
+          ) ::
+            {:ok, [Msdata.SystPerms.t()] | []} | {:error, MscmpSystError.t()}
+    def list_perm_denials(selector, opts \\ []),
+      do: Impl.AccessAccountPermRole.list_perm_denials(selector, opts)
+
+    @spec grant_perm_role(
+            MscmpSystMcpPerms.Types.AccessAccountPermsSelector.t(),
+            MscmpSystPerms.Types.perm_role_id()
+          ) ::
+            :ok | {:error, MscmpSystError.t()}
+    def grant_perm_role(selector, perm_role_id),
+      do: Impl.AccessAccountPermRole.grant_perm_role(selector, perm_role_id)
+
+    @spec revoke_perm_role(
+            MscmpSystMcpPerms.Types.AccessAccountPermsSelector.t(),
+            MscmpSystPerms.Types.perm_role_id()
+          ) ::
+            {:ok, :deleted | :not_found} | {:error, MscmpSystError.t()}
+    def revoke_perm_role(selector, perm_role_id),
+      do: Impl.AccessAccountPermRole.revoke_perm_role(selector, perm_role_id)
+  end
+end
