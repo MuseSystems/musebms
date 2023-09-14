8afde2e9a968d0fee0b68aecf753c1759a201de5
Steven C. Buttgereit <github@musesystems.com>
2023-09-14 12:43:00 -0700

    Minor MscmpSystMcpPerms white space tweak

--- app_server/components/system/mscmp_syst_mcp_perms/mix.exs
+++ app_server/components/system/mscmp_syst_mcp_perms/mix.exs
@@ -21,6 +21,7 @@ defmodule MscmpSystMcpPerms.MixProject do
     {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
     {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
     {:ex_doc, "~> 0.20", only: :dev, runtime: false},
+
     # Muse Systems Business Management System Components
     {:mscmp_syst_error, path: "../mscmp_syst_error"},
     {:mscmp_syst_utils, path: "../mscmp_syst_utils"},
