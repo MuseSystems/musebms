3c68957289f0267aac90701fe7219d3e514fbd07
Steven C. Buttgereit <github@musesystems.com>
2023-10-10 17:40:09 -0700

    Add skeleton MscmpSystNetwork component to project
    This component is a fork of the net_address IP/MAC address utility
    library by Isaac Yonemoto:
    
    https://github.com/ityonemo/net_address
    
    We were originally depending on that library directory, but with more
    recent versions of Elixir we're getting compile time warnings.  I
    submitted a PR to resolve the most concerning warning, but after a month
    I've received no response so I'm assuming that the library is no longer
    maintained.
    
    The license of the library is a permissive license which should be
    compatible with our current project license (or any license we're
    currently contemplating).
    
    We'll take most of the code as the library works well, but we'll make
    that code consistent with the rest of our project.
    
    This commit

---
+++ app_server/components/system/mscmp_syst_network/mix.exs
@@ -0,0 +1,66 @@
+# Source File: mix.exs
+# Location:    musebms/app_server/components/system/mscmp_syst_network/mix.exs
+# Project:     Muse Systems Business Management System
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https://muse.systems
+
+defmodule MscmpSystNetwork.MixProject do
+  use Mix.Project
+
+  @name :mscmp_syst_network
+  @version "0.1.0"
+
+  @deps [
+    # Third Party Dependencies
+    {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
+    {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+    {:ex_doc, "~> 0.20", only: :dev, runtime: false}
+  ]
+
+  @dialyzer_opts [
+    flags: ["-Wunmatched_returns", :error_handling, :underspecs],
+    plt_add_apps: [:mix]
+  ]
+
+  # ------------------------------------------------------------
+
+  def project do
+    in_production = Mix.env() == :prod
+
+    [
+      app: @name,
+      version: @version,
+      elixir: "~> 1.15",
+      deps: @deps,
+      build_embedded: in_production,
+      start_permanent: in_production,
+      dialyzer: @dialyzer_opts,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      docs: [
+        name: "MscmpSystNetwork",
+        main: "MscmpSystNetwork",
+        output: "../../../../documentation/technical/app_server/mscmp_syst_network",
+        groups_for_functions: [],
+        groups_for_modules: [
+          API: [MscmpSystNetwork]
+        ]
+      ]
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:crypto]
+    ]
+  end
+
+  defp elixirc_paths(:test), do: elixirc_paths() ++ ["test/support"]
+  defp elixirc_paths(_), do: elixirc_paths()
+  defp elixirc_paths(), do: ["lib"]
+end
---
+++ app_server/components/system/mscmp_syst_network/test/mscmp_syst_network_test.exs
@@ -0,0 +1,8 @@
+defmodule MscmpSystNetworkTest do
+  use ExUnit.Case
+  doctest MscmpSystNetwork
+
+  test "greets the world" do
+    assert MscmpSystNetwork.hello() == :world
+  end
+end
