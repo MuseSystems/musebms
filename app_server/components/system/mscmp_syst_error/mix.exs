1c062deae4cd2768e5734e02abc1314f9ecf9319
Steven C. Buttgereit <github@musesystems.com>
2023-10-29 06:09:26 -0700

    Remove underspecs from Dialyzer configs
    We're getting errors in CI; some of this is because we added check
    options to the mix.exs Dialyzer configuration.  However, some of the
    now reported errors appear to be legitimately spurious: saying a
    function doesn't return `nil` when it actually does in some
    circumstances isn't helpful.  Normally, I'd want to disable the check
    for that function, but adding :no_underspecs for that function seems to
    be ignored whereas removing it globally removes the error.
    
    Ideally I'd like to have underspecs on, but what it's protecting against
    right now isn't enough to make it worth the trouble.

--- app_server/components/system/mscmp_syst_error/mix.exs
+++ app_server/components/system/mscmp_syst_error/mix.exs
@@ -26,7 +26,7 @@ defmodule MscmpSystError.MixProject do
   ]
 
   @dialyzer_opts [
-    flags: ["-Wunmatched_returns", :error_handling, :underspecs],
+    flags: ["-Wunmatched_returns", :error_handling],
     plt_add_apps: [:mix, :ex_unit],
     plt_core_path: "priv/plts",
     plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
--- app_server/components/system/mscmp_syst_forms/mix.exs
+++ app_server/components/system/mscmp_syst_forms/mix.exs
@@ -33,7 +33,7 @@ defmodule MscmpSystForms.MixProject do
   ]
 
   @dialyzer_opts [
-    flags: ["-Wunmatched_returns", :error_handling, :underspecs],
+    flags: ["-Wunmatched_returns", :error_handling],
     plt_add_apps: [:mix, :ex_unit],
     plt_core_path: "priv/plts",
     plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
