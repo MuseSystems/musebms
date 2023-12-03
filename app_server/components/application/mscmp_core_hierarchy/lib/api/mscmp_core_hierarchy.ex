b7f90315d047c1411a8296b0383cfb13e8ccab3a
Steven C. Buttgereit <github@musesystems.com>
2023-12-03 06:29:02 -0800

    Update MscmpCoreHierarchy README.md and moduledoc

--- app_server/components/application/mscmp_core_hierarchy/lib/api/mscmp_core_hierarchy.ex
+++ app_server/components/application/mscmp_core_hierarchy/lib/api/mscmp_core_hierarchy.ex
@@ -11,4 +11,9 @@
 # muse.information@musesystems.com :: https://muse.systems
 
 defmodule MscmpCoreHierarchy do
+  @external_resource "README.md"
+  @moduledoc Path.join([__DIR__, "..", "..", "README.md"])
+             |> File.read!()
+             |> String.split("<!-- MDOC !-->")
+             |> Enum.fetch!(1)
 end
