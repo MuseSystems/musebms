2d407d459bcd6c80cc70f759f55c486d07a9874a
Steven C. Buttgereit <github@musesystems.com>
2023-12-07 03:58:42 -0800

    Refactor McmpCoreHierarchy test seed data
    This splits the test data seeding scripts to facilitate the new, more
    formally defined divisions between unit, integration, and documentation
    testing.

--- database/components/application/mscmp_core_hierarchy/testing_support/test_data_integration_test.eex.sql
+++ database/components/application/mscmp_core_hierarchy/testing_support/test_data_integration_test.eex.sql
@@ -89,6 +89,7 @@ BEGIN
               , (ei ->> 'user_maintainable')::boolean       AS user_maintainable
               , ei ->> 'syst_description'                   AS syst_description
               , ei -> 'syst_options'                        AS syst_options
+              , ei ->> 'sort_order'                         AS sort_order
               , ei -> 'hierarchies'                         AS hierarchies
             FROM jsonb_array_elements( var_enum_func_type.enum_items ) ei
 
@@ -104,7 +105,8 @@ BEGIN
                 , syst_defined
                 , user_maintainable
                 , syst_description
-                , syst_options )
+                , syst_options
+                , sort_order )
             VALUES
                 ( var_enum_item.internal_name
                 , var_enum_item.display_name
@@ -116,7 +118,8 @@ BEGIN
                 , var_enum_item.syst_defined
                 , var_enum_item.user_maintainable
                 , var_enum_item.syst_description
-                , var_enum_item.syst_options )
+                , var_enum_item.syst_options
+                , var_enum_item.sort_order )
             RETURNING id INTO var_enum_item_id;
 
             << hierarchies_loop >>
