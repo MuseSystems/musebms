deff61b0d2a6a07d8cd9cc5f13221e792dc2d2b2
Steven C. Buttgereit <github@musesystems.com>
2023-12-10 06:53:08 -0800

    Add mscmp_syst_nav data required schema
    This completes the basic data structure of the MscmpSystNav Component
    at least insofar as the ms_syst_data structures are concerned.
    
      - Complete the syst_actions table with command oriented columns,
        adding some clearly appropriate indexes, and finishing comments.
    
      - Add function generate_command_tsvector to allow syst_action_groups
        and syst_actions tables which generate their `command_search`
        columns.  This could be done directly in the table definition, but
        the resulting CREATE TABLE is pretty ugly on the eyes, so just make
        a little IMMUTABLE function to keep things clean.  The few cycles
        spent on the extra function call shouldn't be a big deal.
    
      - Add syst_action_groups table.  This is basically implementing the
        command line navigation concept that there is a "Command" such as
        "new" or "open" (etc.) and that there are sub-commands which are
        those directly associated with the syst_actions records.  In cases
        where commandline-like interfaces are not appropriate, Action Groups
        simply offer an organizing principle.
    
      - Add a starting subplans TOML file to define Component migrations.

--- database/components/system/mscmp_syst_nav/ms_syst_data/syst_actions/syst_actions.eex.sql
+++ database/components/system/mscmp_syst_nav/ms_syst_data/syst_actions/syst_actions.eex.sql
@@ -32,6 +32,32 @@ CREATE TABLE ms_syst_data.syst_actions
         NOT NULL
         CONSTRAINT syst_actions_action_group_fk
             REFERENCES ms_syst_data.syst_action_groups ( id )
+    ,command
+        text
+    ,CONSTRAINT syst_action_group_actions_command_udx
+        UNIQUE NULLS DISTINCT (action_group_id, command)
+    ,command_config
+        regconfig
+    ,commands_aliases
+        text[]
+    ,command_search
+         tsvector
+         GENERATED ALWAYS AS
+             ( ms_syst_priv.generate_command_tsvector(
+                 p_config => command_config,
+                 p_command => command,
+                 p_command_aliases => commands_aliases ) )
+             STORED
+    ,CONSTRAINT syst_actions_command_validity_chk
+        CHECK
+            ( ( command IS NULL
+                    AND command_config IS NULL
+                    AND commands_aliases IS NULL
+                    AND command_search IS NULL) OR
+              ( command IS NOT NULL
+                    AND command_config IS NOT NULL
+                    AND commands_aliases IS NOT NULL
+                    AND command_search IS NOT NULL) )
     ,diag_timestamp_created
         timestamptz
         NOT NULL DEFAULT now( )
@@ -62,16 +88,103 @@ CREATE TRIGGER z99_trig_b_iu_set_diagnostic_columns
     BEFORE INSERT OR UPDATE ON ms_syst_data.syst_actions
     FOR EACH ROW EXECUTE PROCEDURE ms_syst_priv.trig_b_iu_set_diagnostic_columns();
 
+CREATE INDEX syst_actions_action_group_idx
+    ON ms_syst_data.syst_actions ( action_group_id );
+
+CREATE INDEX syst_actions_command_search_idx
+    ON ms_syst_data.syst_actions USING GIN ( command_search );
+
 COMMENT ON
     TABLE ms_syst_data.syst_actions IS
-$DOC$Defines actions which may be taken when a Menu Item is "selected" by a user.$DOC$;
+$DOC$Defines actions which may be taken when a Menu Item is "selected" or the defined
+Command is entered by a user.  Actions may be associated with a Menu Item, may
+define a Command, or both.  Actions which are not referenced by a Menu Item and
+do not define a Command for access will effectively not be active in the
+application as there will be no way to resolve/invoke the action.$DOC$;
 
 COMMENT ON
     COLUMN ms_syst_data.syst_actions.id IS
 $DOC$The record's primary key.  The definitive identifier of the record in the
 system.$DOC$;
 
+COMMENT ON
+    COLUMN ms_syst_data.syst_actions.internal_name IS
+$DOC$A candidate key useful for programmatic references to individual records.$DOC$;
 
+COMMENT ON
+    COLUMN ms_syst_data.syst_actions.display_name IS
+$DOC$A friendly name and candidate key for the record, suitable for use in user
+interactions$DOC$;
+
+COMMENT ON
+    COLUMN ms_syst_data.syst_actions.external_name IS
+$DOC$A non-unique/non-key value used to display to users and external parties where
+uniqueness is less of a concern than specific end user presentation.$DOC$;
+
+COMMENT ON
+    COLUMN ms_syst_data.syst_actions.action_group_id IS
+$DOC$If the Action is to be accessible via Command entry, it must define a parent
+Action Group which is done via the value in this column.  If the Action is not
+accessible via Commands, this value should be left `NULL`.$DOC$;
+
+COMMENT ON
+    COLUMN ms_syst_data.syst_actions.command IS
+$DOC$Provides the primary text "Command" used for identifying the Action.
+
+Commands associated with Actions are used for commandline-like input by
+application users to select the specific action to take assuming the Action
+Group has been resolved..  For example, the Command "po", when taken from the
+Action Group identified with Command "new", could indicate that the user wishes
+to create a new purchase order.
+
+When the value of this column is `NULL`, the Action is not searchable or
+available to the user using command line interfaces.  Such Actions exist if they
+are only invokable from menus or similar, non-commandline user interfaces.
+Note that if this value is `NULL` other `command_*` columns must also be `NULL`.
+
+`command` designated Commands have priority over similar "Command Aliases"
+defined in the `command_aliases` column.$DOC$;
+
+COMMENT ON
+    CONSTRAINT syst_action_group_actions_command_udx
+    ON ms_syst_data.syst_actions IS
+$DOC$Action Commands may be duplicated across Action Groups.  For example this
+allows the Action Groups "new" and "open" to both share Actions identified by
+the same Action Command "po" or "purchase order".  Action Commands must be
+unique within an Action Group to remain unambiguous.$DOC$;
+
+COMMENT ON
+    COLUMN ms_syst_data.syst_actions.command_config IS
+$DOC$Establishes the PostgresSQL text search configuration to use when parsing the
+Command strings.  The primary use of this column is to set the value of the
+generated column `command_search`, though establishing the appropriate
+configuration for use with the record may be useful elsewhere.
+
+The null/not null state of this column must match the null/not null state of the
+`command` column.$DOC$;
+
+COMMENT ON
+    COLUMN ms_syst_data.syst_actions.commands_aliases IS
+$DOC$An array of strings which designate alternate, possibly non-unique values which
+may be used in addition to the `command` value in identifying the record.  In
+searched results, `command_aliases` received a reduced priority vs. `command`
+values.
+
+The null/not null state of this column must match the null/not null state of the
+`command` column.$DOC$;
+
+COMMENT ON
+    COLUMN ms_syst_data.syst_actions.command_search IS
+$DOC$A generated column containing the PostgreSQL tsvector value used when
+resolving an Action Command.
+
+The null/not null state of this column must match the null/not null state of the
+`command` column.$DOC$;
+
+COMMENT ON
+    CONSTRAINT syst_actions_command_validity_chk
+    ON ms_syst_data.syst_actions IS
+$DOC$Enforces the rule that all `command*` columns share a null/not null state.$DOC$;
 
 COMMENT ON
     COLUMN ms_syst_data.syst_actions.diag_timestamp_created IS
