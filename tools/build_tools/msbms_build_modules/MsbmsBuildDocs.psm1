b8b2454ee677bca3159e31ebfbddc72bd188a7d0
Steven C. Buttgereit <github@musesystems.com>
2023-12-30 06:44:34 -0800

    Add PowerShell based build scripts
    This is a long outstanding task which was becoming more important to
    finish.  This is especially true now that we're going to generate
    database documentation for each Component individually.
    
    The new script supports common cross-project actions such as updating
    dependencies, building documentation, and similar actions which are
    Component-by-Component and boilerplate-ish in nature.
    
    Originally I started writing these scripts as Bash shells; our current
    development environment is expected to be Linux based and it would be
    the obvious first choice.  Having never written a real Bash script
    before, I started following this conventional logic: the scripting
    process itself convinced me otherwise.  In the end I landed on
    PowerShell.  I'd never written a PowerShell script before either; but
    the process was significantly more sane than what I found with Bash.
    
    The reason PowerShell won over alternatives like my beloved Fish Shell
    or something like Python or even Elixir scripts boils down to a few
    basic facts.  First, while development is expected to be primarily Linux
    driven, it's not intended to be exclusively Linux and there are cases
    where building on Windows is necessary (release builds) so whatever we
    scripting method we choose should ideally also be well supported on
    Windows.  Python and Elixir both have runtime environments that may
    not be present or may introduce runtime environment management issues
    that are needless complicated for simple scripts.  PowerShell is well
    supported on both Windows and Linux.  PowerShell, being a Microsoft
    product, should be acceptable for use in enterprises and such where tool
    selections could be controversial (yes, Elixir already has us off that
    trail, but lets not push every front).  It will be more likely that a
    Linux user will be more familiar with one-off installation of a tool
    like PowerShell than a Windows user trying to similarly go off well
    trodden paths.  Finally, PowerShell is a decent shell for this sort of
    thing; the biggest issue is that it's such a rethink of the paradigm
    that it's not substantially similar to anything that came before it...
    especially the role and naming of Cmdlets.
    
    These scripts are not examples of how best to write PowerShell; they
    work and attempt to organize the code, but I expect there are plenty
    bad and non-compliant with PowerShell standards and practices.

---
+++ tools/build_tools/msbms_build_modules/MsbmsBuildDocs.psm1
@@ -0,0 +1,131 @@
+# Source File: MsbmsBuildDocs.psm1
+# Location:    musebms/tools/build_tools/msbms_build_modules/MsbmsBuildDocs.psm1
+# Project:     Muse Systems Business Management System
+#
+# Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+# This file may include content copyrighted and licensed from third parties.
+#
+# See the LICENSE file in the project root for license terms and conditions.
+# See the NOTICE file in the project root for copyright ownership information.
+#
+# muse.information@musesystems.com :: https://muse.systems
+
+function Build-ElixirDocs {
+  param ( [string[]]$Components = @() )
+
+  $start_path = Get-Location
+
+  $elixir_docs_root = "documentation/technical/app_server"
+
+  $elixir_component_paths = @(
+    "app_server/components/system"
+    "app_server/components/application"
+    "app_server/subsystems"
+    "app_server/platform"
+  ) | Where-Object { Test-Path $_ -PathType Container }
+
+  foreach ( $component_path in $elixir_component_paths) {
+    $target_components =
+    Get-ChildItem $component_path -Directory |
+    Split-Path -Leaf |
+    Where-Object {
+        ( $null -eq $Components ) -or
+        ( @($Components).count -eq 0 ) -or
+        ( $Components -contains $_ )
+    }
+
+    foreach ( $current_component in $target_components) {
+      Write-Host ""
+      Write-Host "===========> ${current_component}: Start Elixir docs build.  ($(Get-Date -Format "o"))"
+
+      Remove-Item ( Join-Path $start_path $elixir_docs_root $current_component  ) -Recurse -Force
+
+      Push-Location -Path ( Join-Path $start_path $component_path $current_component  )
+
+      mix docs
+
+      Pop-Location
+
+      Write-Host "===========> ${current_component}: Finish Elixir docs build.  ($(Get-Date -Format "o"))"
+      Write-Host ""
+    }
+  }
+}
+
+function Build-DbDocs {
+  param ( [string[]]$Components = @() )
+
+  $start_path = Get-Location
+
+  $db_source_root = "database"
+
+  $db_docs_root = "documentation/technical/database"
+
+  $elixir_component_paths = @(
+    "app_server/components/system"
+    "app_server/components/application"
+    "app_server/subsystems"
+    "app_server/platform"
+  ) | Where-Object { Test-Path $_ -PathType Container }
+
+  foreach ( $component_path in $elixir_component_paths) {
+    $target_components =
+    Get-ChildItem $component_path -Directory |
+    Split-Path -Leaf |
+    Where-Object {
+        ( $null -eq $Components ) -or
+        ( @($Components).count -eq 0 ) -or
+        ( $Components -contains $_ )
+    }
+
+    foreach ( $current_component in $target_components) {
+      $component_buildplan_name = "buildplans.${current_component}_db_docs.toml"
+
+      $component_buildplan_path = Join-Path $start_path $db_source_root $component_buildplan_name
+
+      $component_docs_path = Join-Path $start_path $db_docs_root $current_component
+
+      if (! ( Test-Path -Path $component_buildplan_path -PathType Leaf ) ) { continue }
+
+      Write-Host ""
+      Write-Host "===========> ${current_component}: Start DB docs build.  ($(Get-Date -Format "o"))"
+
+      Remove-Item $component_docs_path -Recurse -Force
+
+      Push-Location -Path ( Join-Path $start_path $component_path $current_component  )
+
+      mix loaddb `
+        --build `
+        --clean `
+        --type "${current_component}_db_docs" `
+        --db-name "${current_component}" `
+        --source "$(Join-Path $start_path $db_source_root)"
+
+      java `
+        -jar "${start_path}/tools/build_tools/schemaspy/schemaspy-6.2.4.jar" `
+        -t pgsql11 `
+        -dp "${start_path}/tools/build_tools/schemaspy/postgresql-42.7.1.jar" `
+        -db "${current_component}" `
+        -host 127.0.0.1 `
+        -port 5432 `
+        -schemas "ms_appl,ms_appl_data,ms_appl_priv,ms_syst,ms_syst_data,ms_syst_db,ms_syst_priv,ms_user,ms_user_data,ms_user_priv" `
+        -norows `
+        -imageformat svg `
+        -nopages `
+        -noimplied `
+        -u "ms_documentation_role" `
+        -p "ms_documentation_password" `
+        -o "${component_docs_path}"
+
+      mix dropdb `
+        --clean `
+        --type "${current_component}_db_docs" `
+        --db-name "${current_component}"
+
+      Pop-Location
+
+      Write-Host "===========> ${current_component}: Finish DB docs build.  ($(Get-Date -Format "o"))"
+      Write-Host ""
+    }
+  }
+}
