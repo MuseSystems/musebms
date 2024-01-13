a1ad45687740663bec2edf9c572100269e26cfb9
Steven C. Buttgereit <github@musesystems.com>
2024-01-13 11:52:19 -0800

    Add a DataGrip supporting database load
    DataGrip works best when it has an introspected schema to work against.
    
    This commit adds a DataGrip specific database load based on the MssubMcp
    DB docs buildplan.
    
    In addition, we'll add MscmpSystHierarchy and MscmpSystNav to the be
    MssubMcp build.  They're not really ready for prime-time, but we're pre-
    release and it's probably better to have these committed than not at
    this stage.

---
+++ database/subsystems/mssub_mcp/privileges/mscmp_syst_nav.eex.sql
@@ -0,0 +1,14 @@
+-- File:        mscmp_syst_nav.eex.sql
+-- Location:    musebms/database/subsystems/mssub_mcp/privileges/mscmp_syst_nav.eex.sql
+-- Project:     Muse Systems Business Management System
+--
+-- Copyright © Lima Buttgereit Holdings LLC d/b/a Muse Systems
+-- This file may include content copyrighted and licensed from third parties.
+--
+-- See the LICENSE file in the project root for license terms and conditions.
+-- See the NOTICE file in the project root for copyright ownership information.
+--
+-- muse.information@musesystems.com :: https://muse.systems
+
+-- TODO: Actually put some perms here once we have objects to receive them.
+PERFORM 1;
