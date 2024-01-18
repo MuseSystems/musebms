45e0ad0b3f716bc41dca19eba43c68f03d5b195b
Steven C. Buttgereit <github@musesystems.com>
2024-01-18 06:04:52 -0800

    Refactor MscmpBrmEntity for new commenting std

--- database/components/application/mscmp_brm_entity/ms_appl_data/mstr_entities/mstr_entities.eex.sql
+++ database/components/application/mscmp_brm_entity/ms_appl_data/mstr_entities/mstr_entities.eex.sql
@@ -98,26 +98,50 @@ CREATE CONSTRAINT TRIGGER a50_trig_a_u_entity_states_enum_item_check
             ms_syst_priv.trig_a_iu_enum_item_check(
                 'entity_states', 'entity_state_id');
 
-COMMENT ON
-    TABLE ms_appl_data.mstr_entities IS
-$DOC$Master list of legal entities with whom the business interacts.  All legal
-entities are represented by this table, including the business using this
-application itself.  The information stored in this record represents general
-information about the entity that is broadly applicable in all contexts which
-might use the entity.$DOC$;
+DO
+$DOCUMENTATION$
+DECLARE
+    -- Table
+    var_comments_config ms_syst_priv.comments_config_table;
 
-COMMENT ON
-    COLUMN ms_appl_data.mstr_entities.id IS
-$DOC$The record's primary key.  The definitive identifier of the record in the
-system.$DOC$;
+    -- Columns
+    var_owning_entity_id ms_syst_priv.comments_config_table_column;
+    var_entity_type_id   ms_syst_priv.comments_config_table_column;
+    var_entity_state_id  ms_syst_priv.comments_config_table_column;
 
-COMMENT ON
-    COLUMN ms_appl_data.mstr_entities.owning_entity_id IS
+BEGIN
+
+    --
+    -- Table Config
+    --
+
+    var_comments_config.table_schema := 'ms_appl_data';
+    var_comments_config.table_name   := 'mstr_entities';
+
+    var_comments_config.description :=
+$DOC$Master list of legal entities with whom the business interacts.
+
+All legal entities are represented by this table, including the business using
+this application itself.$DOC$;
+
+    var_comments_config.general_usage :=
+$DOC$The information stored in this record represents general information about the
+entity that is broadly applicable in all contexts which might use the entity.$DOC$;
+
+    --
+    -- Column Configs
+    --
+
+    var_owning_entity_id.column_name := 'owning_entity_id';
+    var_owning_entity_id.description :=
 $DOC$Indicates which managing entity is the owning entity for purposes of default
-visibility and usage limitations.  This limitation primarily is evident in
-searches and lists.  Explicit assignment of rights to entities, persons,
-facilities, etc. that are not the owning entity is still possible and those
-rights will prevail over the default access rules for non-owning entities.
+visibility and usage limitations.$DOC$;
+    var_owning_entity_id.general_usage :=
+$DOC$The limited cases are primarily evident in searches and lists.
+
+Explicit assignment of rights to entities, persons, facilities, etc. that are
+not the owning entity is still possible and those rights will prevail over the
+default access rules for non-owning entities.
 
 There is a special case of owning entity where an entity is self owning.  There
 may only be a single entity that is self owning and that, by definition,
@@ -125,71 +149,35 @@ establishes that entity as the 'global entity'.  The global entity has global
 administrative rights as well as allows access to those persons, facilities, and
 entities that it owns effectively making them global to all other managed
 entities.  The global entity may be an actual business entity which essentially
-owns the MSBMS instance or it may a purely administrative construct for managing
+owns the MSBMS Instance or it may a purely administrative construct for managing
 the system.$DOC$;
 
-COMMENT ON
-    COLUMN ms_appl_data.mstr_entities.internal_name IS
-$DOC$A candidate key useful for programmatic references to individual records.$DOC$;
 
-COMMENT ON
-    COLUMN ms_appl_data.mstr_entities.display_name IS
-$DOC$A friendly name and candidate key for the record, suitable for use in user
-interfaces.$DOC$;
-
-COMMENT ON
-    COLUMN ms_appl_data.mstr_entities.external_name IS
-$DOC$A friendly name for externally facing uses such as in communications with the
-entity.  Note that this is not a key value and has no UNIQUE enforcement.$DOC$;
-
-COMMENT ON
-    COLUMN ms_appl_data.mstr_entities.entity_type_id IS
+    var_entity_type_id.column_name := 'entity_type_id';
+    var_entity_type_id.description :=
 $DOC$Defines the kind of entity that is being represented by the record and by
-extension the kinds of uses in which the entity may be used.  Application
-functionality is determined in part by the configuration of the selected type.$DOC$;
+extension the kinds of uses in which the entity may be used.$DOC$;
+    var_entity_type_id.general_usage :=
+$DOC$Application functionality is determined in part by the configuration of the
+selected type.$DOC$;
 
-COMMENT ON
-    COLUMN ms_appl_data.mstr_entities.entity_state_id IS
+    var_entity_state_id.column_name := 'entity_state_id';
+    var_entity_state_id.description :=
 $DOC$Establishes current state of the entity in the established lifecycle of
-entity records.  Certain application features and behaviors will depend on
-the configuration of the state value selected for a entity record.$DOC$;
+entity records.$DOC$;
+    var_entity_state_id.general_usage :=
+$DOC$Certain application features and behaviors will depend on the configuration
+of the state value selected for a entity record.$DOC$;
 
-COMMENT ON
-    COLUMN ms_appl_data.mstr_entities.diag_timestamp_created IS
-$DOC$The database server date/time when the transaction which created the record
-started.$DOC$;
 
-COMMENT ON
-    COLUMN ms_appl_data.mstr_entities.diag_role_created IS
-$DOC$The database role which created the record.$DOC$;
+    var_comments_config.columns :=
+        ARRAY [
+              var_owning_entity_id
+            , var_entity_type_id
+            , var_entity_state_id
+            ]::ms_syst_priv.comments_config_table_column[];
 
-COMMENT ON
-    COLUMN ms_appl_data.mstr_entities.diag_timestamp_modified IS
-$DOC$The database server date/time when the transaction which modified the record
-started.  This field will be the same as diag_timestamp_created for inserted
-records.$DOC$;
+    PERFORM ms_syst_priv.generate_comments_table( var_comments_config );
 
-COMMENT ON
-    COLUMN ms_appl_data.mstr_entities.diag_wallclock_modified IS
-$DOC$The database server date/time at the moment the record was actually modified.
-For long running transactions this time may be significantly later than the
-value of diag_timestamp_modified.$DOC$;
-
-COMMENT ON
-    COLUMN ms_appl_data.mstr_entities.diag_role_modified IS
-$DOC$The database role which modified the record.$DOC$;
-
-COMMENT ON
-    COLUMN ms_appl_data.mstr_entities.diag_row_version IS
-$DOC$The current version of the row.  The value here indicates how many actual
-data changes have been made to the row.  If an update of the row leaves all data
-fields the same, disregarding the updates to the diag_* columns, the row version
-is not updated, nor are any updates made to the other diag_* columns other than
-diag_update_count.$DOC$;
-
-COMMENT ON
-    COLUMN ms_appl_data.mstr_entities.diag_update_count IS
-$DOC$Records the number of times the record has been updated regardless as to if
-the update actually changed any data.  In this way needless or redundant record
-updates can be found.  This row starts at 0 and therefore may be the same as the
-diag_row_version - 1.$DOC$;
+END;
+$DOCUMENTATION$;
