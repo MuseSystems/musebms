a17d61de9ed0eadbf28b1b35f69e7f188bcc5fc7
Steven C. Buttgereit <github@musesystems.com>
2024-08-04 07:53:42 -0700

    Bump MscmpSystLimiter Elixir & Deps versions
    Elixir to 1.17.2/OTP-27 and update all dependency versions.

--- app_server/components/system/mscmp_syst_limiter/mix.exs
+++ app_server/components/system/mscmp_syst_limiter/mix.exs
@@ -18,10 +18,10 @@ defmodule MscmpSystLimiter.MixProject do
 
   @deps [
     # Third Party Dependencies
-    {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
-    {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
-    {:ex_doc, "~> 0.20", only: :dev, runtime: false},
-    {:hammer, "~> 6.0"},
+    {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+    {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+    {:ex_doc, "~> 0.31", only: :dev, runtime: false},
+    {:hammer, "~> 6.1"},
 
     # Muse Systems Business Management System Components
     {:mscmp_syst_error, path: "../mscmp_syst_error"},
@@ -43,7 +43,7 @@ defmodule MscmpSystLimiter.MixProject do
     [
       app: @name,
       version: @version,
-      elixir: "~> 1.15",
+      elixir: "~> 1.17",
       deps: @deps,
       build_embedded: in_production,
       start_permanent: in_production,
