f3e2dbf55e8f9db1d102c1804821bded89d5d963
Steven C. Buttgereit <github@musesystems.com>
2024-08-08 07:12:14 -0700

    Bump MscmpSystEnums Elixir & Deps versions
    Elixir to 1.17.2/OTP-27 and update all dependency versions.

--- app_server/components/system/mscmp_syst_enums/mix.exs
+++ app_server/components/system/mscmp_syst_enums/mix.exs
@@ -18,9 +18,9 @@ defmodule MscmpSystEnums.MixProject do
 
   @deps [
     # Third Party Dependencies
-    {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
-    {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
-    {:ex_doc, "~> 0.20", only: :dev, runtime: false},
+    {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+    {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+    {:ex_doc, "~> 0.31", only: :dev, runtime: false},
 
     # Muse Systems First Party Dependencies
     {:mscmp_syst_error, path: "../mscmp_syst_error"},
@@ -43,7 +43,7 @@ defmodule MscmpSystEnums.MixProject do
     [
       app: @name,
       version: @version,
-      elixir: "~> 1.15",
+      elixir: "~> 1.17",
       deps: @deps,
       build_embedded: in_production,
       start_permanent: in_production,
@@ -82,7 +82,10 @@ defmodule MscmpSystEnums.MixProject do
   def application do
     [
       extra_applications: [
-        :logger
+        :logger,
+        :observer,
+        :wx,
+        :runtime_tools
       ]
     ]
   end
