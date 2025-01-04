2a17190af0b9aca8233c0ff60dde18eb21b38d82
Steven C. Buttgereit <github@musesystems.com>
2025-01-04 04:55:54 -0800

    Add MscmpSystUtils Component project skeleton
    At this point it probably makes sense to condense the smaller Msutils
    components into a single component for maintenance ease.
    
    MscmpSystUtilsData can remain on its own since it has relatively heavier
    dependencies (Ecto).   At that ought to be our pattern: simple, low/no
    dependency utilities can go in the basic MscmpSytUtils package, but
    heavier utils can remain in their own Elixir projects as needed.
    
    Utilities in this new namespace will continue to be part of the Msutils
    namespace.
    
    In this commit we only have the files created by `mix new`.

---
+++ app_server/components/system/mscmp_syst_utils/lib/mscmp_syst_utils.ex
@@ -0,0 +1,18 @@
+defmodule MscmpSystUtils do
+  @moduledoc """
+  Documentation for `MscmpSystUtils`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MscmpSystUtils.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ app_server/components/system/mscmp_syst_utils/test/mscmp_syst_utils_test.exs
@@ -0,0 +1,8 @@
+defmodule MscmpSystUtilsTest do
+  use ExUnit.Case
+  doctest MscmpSystUtils
+
+  test "greets the world" do
+    assert MscmpSystUtils.hello() == :world
+  end
+end
