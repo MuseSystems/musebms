49d46c739cf39a4b203ff174078fd6fca443e443
Steven C. Buttgereit <github@musesystems.com>
2025-05-01 04:59:31 -0700

    Fix dependencies and bump dependency versions
    This just brings this subsystem up to our current standards, not hard to
    do when there isn't really any code.

--- app_server/subsystems/mssub_bms/mix.exs
+++ app_server/subsystems/mssub_bms/mix.exs
@@ -24,7 +24,7 @@ defmodule MssubBms.MixProject do
     {:nimble_options, "~> 1.0"},
 
     # Muse Systems Business Management System Components
-    {:mscmp_syst_utils_string, path: "../../components/system/mscmp_syst_utils_string"},
+    {:mscmp_syst_utils, path: "../../components/system/mscmp_syst_utils"},
     {:mscmp_syst_error, path: "../../components/system/mscmp_syst_error"},
     {:mscmp_syst_enums, path: "../../components/system/mscmp_syst_enums"},
     {:mscmp_syst_settings, path: "../../components/system/mscmp_syst_settings"}
@@ -45,7 +45,7 @@ defmodule MssubBms.MixProject do
     [
       app: @name,
       version: @version,
-      elixir: "~> 1.17",
+      elixir: "~> 1.18",
       deps: @deps,
       build_embedded: in_production,
       start_permanent: in_production,
@@ -58,7 +58,7 @@ defmodule MssubBms.MixProject do
         deps: [
           mscmp_syst_db: "../mscmp_syst_db",
           mscmp_syst_error: "../mscmp_syst_error",
-          mscmp_syst_utils_string: "../mscmp_syst_utils_string",
+          mscmp_syst_utils: "../mscmp_syst_utils",
           mscmp_syst_limiter: "../mscmp_syst_limiter",
           mscmp_syst_enums: "../mscmp_syst_enums",
           mscmp_syst_options: "../mscmp_syst_options",
